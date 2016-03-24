41ca5424ce8e8fdcf63ccd3fe7e4884692e7aa2b
uppfinnarn <uppfinnarn@gmail.com>
2016-03-24 11:49:44 +0100

    Tests for JS logging

--- runner/js/functions_test.go
+++ runner/js/functions_test.go
@@ -48,3 +48,49 @@ func TestJSSleepFraction(t *testing.T) {
 		t.Error("Wrong amount of sleep", d)
 	}
 }
+
+func makeLogTestFunc(out *string) func(string) {
+	return func(text string) { *out = text }
+}
+
+func TestJSLog(t *testing.T) {
+	vm := otto.New()
+	out := ""
+	vm.Set("log", jsLogFactory(makeLogTestFunc(&out)))
+	_, err := vm.Run(`log("test")`)
+	if err != nil {
+		t.Error("JS Error", err)
+	}
+
+	if out != "test" {
+		t.Errorf("Wrong output; '%s' != 'test'", out)
+	}
+}
+
+func TestJSLogInteger(t *testing.T) {
+	vm := otto.New()
+	out := ""
+	vm.Set("log", jsLogFactory(makeLogTestFunc(&out)))
+	_, err := vm.Run(`log(1234)`)
+	if err != nil {
+		t.Error("JS Error", err)
+	}
+
+	if out != "1234" {
+		t.Errorf("Wrong output; '%s' != '1234'", out)
+	}
+}
+
+func TestJSLogFloat(t *testing.T) {
+	vm := otto.New()
+	out := ""
+	vm.Set("log", jsLogFactory(makeLogTestFunc(&out)))
+	_, err := vm.Run(`log(12.34)`)
+	if err != nil {
+		t.Error("JS Error", err)
+	}
+
+	if out != "12.34" {
+		t.Errorf("Wrong output; '%s' != '12.34'", out)
+	}
+}
