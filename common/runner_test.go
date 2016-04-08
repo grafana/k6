af6900bca5ca799f0ec2f256e9c15aaa34f6803d
uppfinnarn <uppfinnarn@gmail.com>
2016-04-08 15:36:00 +0200

    GetRunner unit tests

---
+++ common/runner_test.go
@@ -0,0 +1,26 @@
+package common
+
+import (
+	"github.com/loadimpact/speedboat/runner/js"
+	"testing"
+)
+
+func GetRunnerJS(t *testing.T) {
+	r, err := GetRunner("script.js")
+	if err != nil {
+		t.Error(err)
+	}
+	if _, ok := r.(*js.JSRunner); !ok {
+		t.Error("Not a JS runner")
+	}
+}
+
+func GetRunnerUnknown(t *testing.T) {
+	r, err := GetRunner("test.doc")
+	if err == nil {
+		t.Error("No error")
+	}
+	if r != nil {
+		t.Error("Something returned")
+	}
+}
