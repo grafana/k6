67c48ba1ce1c7a16c12529e48707ac037764aad7
uppfinnarn <uppfinnarn@gmail.com>
2016-04-08 17:08:38 +0200

    Restore globals after tests

--- worker/registry_test.go
+++ worker/registry_test.go
@@ -6,7 +6,10 @@ import (
 )
 
 func TestRegisterProcessor(t *testing.T) {
+	oldGlobalProcessors := GlobalProcessors
 	GlobalProcessors = nil
+	defer func() { GlobalProcessors = oldGlobalProcessors }()
+
 	RegisterProcessor(func(w *Worker) comm.Processor { return nil })
 	if len(GlobalProcessors) != 1 {
 		t.Error("Processor not registered")
