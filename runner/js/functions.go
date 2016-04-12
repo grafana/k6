a54fdf4970153fc87926a60a237b9547df44e9e4
uppfinnarn <uppfinnarn@gmail.com>
2016-04-12 14:55:31 +0200

    Trying to track down a panic

--- runner/js/functions.go
+++ runner/js/functions.go
@@ -9,6 +9,10 @@ import (
 	"time"
 )
 
+type JSError string
+
+func (e JSError) Error() { return e }
+
 func jsSleepFactory(impl func(time.Duration)) func(otto.FunctionCall) otto.Value {
 	return func(call otto.FunctionCall) otto.Value {
 		seconds, err := call.Argument(0).ToFloat()
@@ -35,18 +39,18 @@ func jsHTTPGetFactory(vm *otto.Otto, impl func(url string) (*http.Response, erro
 	return func(call otto.FunctionCall) otto.Value {
 		url, err := call.Argument(0).ToString()
 		if err != nil {
-			panic(errors.New(fmt.Sprintf("Couldn't call function: %s", err)))
+			panic(JSError(fmt.Sprintf("Couldn't call function: %s", err)))
 		}
 
 		res, err := impl(url)
 		if err != nil {
-			panic(errors.New(fmt.Sprintf("HTTP GET impl error: %s", err)))
+			panic(JSError(fmt.Sprintf("HTTP GET impl error: %s", err)))
 		}
 		defer res.Body.Close()
 
 		obj, err := vm.Object("new Object()")
 		if err != nil {
-			panic(errors.New(fmt.Sprintf("Couldn't create an Object(): %s", err)))
+			panic(JSError(fmt.Sprintf("Couldn't create an Object(): %s", err)))
 		}
 		body, _ := ioutil.ReadAll(res.Body)
 		obj.Set("body", string(body))
