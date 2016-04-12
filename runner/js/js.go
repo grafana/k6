9a9fd452e986c065dc8fbe009ad4e733d3c77ba2
uppfinnarn <uppfinnarn@gmail.com>
2016-04-12 14:49:03 +0200

    Testing the performance cost of this

--- runner/js/js.go
+++ runner/js/js.go
@@ -69,11 +69,11 @@ func (r *JSRunner) RunIteration(vm *otto.Otto) <-chan interface{} {
 
 	go func() {
 		defer close(out)
-		defer func() {
-			if err := recover(); err != nil {
-				out <- runner.NewError(err.(error))
-			}
-		}()
+		// defer func() {
+		// 	if err := recover(); err != nil {
+		// 		out <- runner.NewError(err.(error))
+		// 	}
+		// }()
 
 		// Log has to be bridged here, as it needs a reference to the channel
 		vm.Set("log", jsLogFactory(func(text string) {
