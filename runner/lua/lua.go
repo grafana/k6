7534fc840b2ef56fcc89b96e904643e4073dadd6
uppfinnarn <uppfinnarn@gmail.com>
2016-04-18 16:57:52 +0200

    I can run simple Lua scripts

--- runner/lua/lua.go
+++ runner/lua/lua.go
@@ -2,16 +2,19 @@ package lua
 
 import (
 	"github.com/loadimpact/speedboat/runner"
+	"github.com/yuin/gopher-lua"
 	"golang.org/x/net/context"
-	// "github.com/Shopify/go-lua"
 )
 
 type LuaRunner struct {
-	Script string
+	Filename, Source string
 }
 
 func New(filename, src string) *LuaRunner {
-	return &LuaRunner{}
+	return &LuaRunner{
+		Filename: filename,
+		Source:   src,
+	}
 }
 
 func (r *LuaRunner) Run(ctx context.Context) <-chan runner.Result {
@@ -19,6 +22,29 @@ func (r *LuaRunner) Run(ctx context.Context) <-chan runner.Result {
 
 	go func() {
 		defer close(ch)
+
+		L := lua.NewState()
+		defer L.Close()
+
+		// Try to load the script, abort execution if it fails
+		lfn, err := L.LoadString(r.Source)
+		if err != nil {
+			ch <- runner.Result{Error: err}
+			return
+		}
+
+		for {
+			L.Push(lfn)
+			if err := L.PCall(0, 0, nil); err != nil {
+				ch <- runner.Result{Error: err}
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+		}
 	}()
 
 	return ch
