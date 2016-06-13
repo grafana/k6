5ca8c1cc0acab5ec05959b1b02b20ee92b474c6c
uppfinnarn <uppfinnarn@gmail.com>
2016-06-13 19:02:30 +0200

    [experiment] blah

--- js/lib/init/99-iterations.js
+++ js/lib/init/99-iterations.js
@@ -1,8 +1,8 @@
 // Wrap the script in a function that increments the iteration counter.
 __data__.iteration = 0;
-__script__ = function(script) {
-	return function() {
-		__data__.iteration++;
-		script();
-	}
-}(__script__);
+// __script__ = function(script) {
+// 	return function() {
+// 		__data__.iteration++;
+// 		script();
+// 	}
+// }(__script__);
