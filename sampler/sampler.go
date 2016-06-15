486183d6ca6a8e7fb14728d69996503fa7bc3eb8
uppfinnarn <uppfinnarn@gmail.com>
2016-06-15 16:08:26 +0200

    Revert "[experiment] This was a bad idea"
    This reverts commit 9aafd78f97944091373ba4386c540fea6343b5c0.

--- sampler/sampler.go
+++ sampler/sampler.go
@@ -201,13 +201,15 @@ func (s *Sampler) Counter(name string) *Metric {
 }
 
 func (s *Sampler) Write(m *Metric, e *Entry) {
-	for _, out := range s.Outputs {
-		if err := out.Write(m, e); err != nil {
-			if s.OnError != nil {
-				s.OnError(err)
+	go func() {
+		for _, out := range s.Outputs {
+			if err := out.Write(m, e); err != nil {
+				if s.OnError != nil {
+					s.OnError(err)
+				}
 			}
 		}
-	}
+	}()
 }
 
 func (s *Sampler) Commit() error {
