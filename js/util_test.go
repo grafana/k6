798a3f98077f2ed5b3aca4d62ea03a79c42881ce
uppfinnarn <uppfinnarn@gmail.com>
2016-06-21 13:20:02 +0200

    [test] Tests for Make() and jsCustomError()

--- js/util_test.go
+++ js/util_test.go
@@ -1,6 +1,7 @@
 package js
 
 import (
+	"errors"
 	"github.com/robertkrimen/otto"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -41,4 +42,35 @@ func TestPutBodyInURL(t *testing.T) {
 
 func TestPutBodyInURLWithQuery(t *testing.T) {
 	assert.Equal(t, "http://example.com/?aa=bb&a=b", putBodyInURL("http://example.com/?aa=bb", "a=b"))
+}
+
+func TestMake(t *testing.T) {
+	vm := otto.New()
+
+	_, err := vm.Eval(`function MyType() { this.a = 'b'; };`)
+	assert.NoError(t, err, "couldn't set up VM")
+
+	obj, err := Make(vm, "MyType")
+	assert.NoError(t, err)
+	assert.NotNil(t, obj)
+	assert.Equal(t, "Object", obj.Class())
+
+	aVal, err := obj.Get("a")
+	assert.NoError(t, err, "couldn't get 'a'")
+	a, err := aVal.ToString()
+	assert.NoError(t, err, "couldn't turn a into a string")
+	assert.Equal(t, "b", a, "a != 'b'")
+}
+
+func TestJSCustomError(t *testing.T) {
+	vm := otto.New()
+	vm.Set("fn", func(call otto.FunctionCall) otto.Value {
+		e := jsCustomError(vm, "CustomError", errors.New("test error"))
+		str, err := e.ToString()
+		assert.NoError(t, err)
+		assert.Equal(t, "CustomError: test error", str)
+		return otto.UndefinedValue()
+	})
+	_, err := vm.Eval("fn()")
+	assert.NoError(t, err)
 }
