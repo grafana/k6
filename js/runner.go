f2867b55311366329f5ebef245c853740bf2ce91
uppfinnarn <uppfinnarn@gmail.com>
2016-06-21 12:43:52 +0200

    Do not refer to a specific VM; use call.Otto

--- js/runner.go
+++ js/runner.go
@@ -52,39 +52,39 @@ func New(t speedboat.Test, filename, source string) *Runner {
 }
 
 func (r *Runner) NewVU() (speedboat.VU, error) {
-	vm := otto.New()
+	vuVM := otto.New()
 
-	script, err := vm.Compile(r.filename, r.source)
+	script, err := vuVM.Compile(r.filename, r.source)
 	if err != nil {
 		return nil, err
 	}
 
 	vu := VU{
 		Runner: r,
-		VM:     vm,
+		VM:     vuVM,
 		Script: script,
 	}
 
-	vm.Set("print", func(call otto.FunctionCall) otto.Value {
+	vu.VM.Set("print", func(call otto.FunctionCall) otto.Value {
 		fmt.Fprintln(os.Stderr, call.Argument(0))
 		return otto.UndefinedValue()
 	})
 
-	vm.Set("$http", map[string]interface{}{
+	vu.VM.Set("$http", map[string]interface{}{
 		"request": func(call otto.FunctionCall) otto.Value {
 			method, err := call.Argument(0).ToString()
 			if err != nil {
-				panic(vm.MakeTypeError("method must be a string"))
+				panic(call.Otto.MakeTypeError("method must be a string"))
 			}
 
 			url, err := call.Argument(1).ToString()
 			if err != nil {
-				panic(vm.MakeTypeError("url must be a string"))
+				panic(call.Otto.MakeTypeError("url must be a string"))
 			}
 
 			body, err := bodyFromValue(call.Argument(2))
 			if err != nil {
-				panic(vm.MakeTypeError("invalid body"))
+				panic(call.Otto.MakeTypeError("invalid body"))
 			}
 
 			params, err := paramsFromObject(call.Argument(3).Object())
@@ -100,12 +100,12 @@ func (r *Runner) NewVU() (speedboat.VU, error) {
 			}).Debug("Request")
 			res, err := vu.HTTPRequest(method, url, body, params)
 			if err != nil {
-				panic(vm.MakeCustomError("HTTPError", err.Error()))
+				panic(call.Otto.MakeCustomError("HTTPError", err.Error()))
 			}
 
-			val, err := res.ToValue(vm)
+			val, err := res.ToValue(call.Otto)
 			if err != nil {
-				panic(vm.MakeCustomError("Error", err.Error()))
+				panic(call.Otto.MakeCustomError("Error", err.Error()))
 			}
 
 			return val
@@ -113,10 +113,10 @@ func (r *Runner) NewVU() (speedboat.VU, error) {
 		"setMaxConnsPerHost": func(call otto.FunctionCall) otto.Value {
 			num, err := call.Argument(0).ToInteger()
 			if err != nil {
-				panic(vm.MakeTypeError("argument must be an integer"))
+				panic(call.Otto.MakeTypeError("argument must be an integer"))
 			}
 			if num <= 0 {
-				panic(vm.MakeRangeError("argument must be >= 1"))
+				panic(call.Otto.MakeRangeError("argument must be >= 1"))
 			}
 			if num > math.MaxInt32 {
 				num = math.MaxInt32
@@ -127,11 +127,11 @@ func (r *Runner) NewVU() (speedboat.VU, error) {
 			return otto.UndefinedValue()
 		},
 	})
-	vm.Set("$vu", map[string]interface{}{
+	vu.VM.Set("$vu", map[string]interface{}{
 		"sleep": func(call otto.FunctionCall) otto.Value {
 			t, err := call.Argument(0).ToFloat()
 			if err != nil {
-				panic(vm.MakeTypeError("time must be a number"))
+				panic(call.Otto.MakeTypeError("time must be a number"))
 			}
 
 			vu.Sleep(t)
@@ -141,28 +141,28 @@ func (r *Runner) NewVU() (speedboat.VU, error) {
 		"id": func(call otto.FunctionCall) otto.Value {
 			val, err := call.Otto.ToValue(vu.ID)
 			if err != nil {
-				panic(jsError(vm, err))
+				panic(jsError(call.Otto, err))
 			}
 			return val
 		},
 		"iteration": func(call otto.FunctionCall) otto.Value {
 			val, err := call.Otto.ToValue(vu.Iteration)
 			if err != nil {
-				panic(jsError(vm, err))
+				panic(jsError(call.Otto, err))
 			}
 			return val
 		},
 	})
-	vm.Set("$log", map[string]interface{}{
+	vu.VM.Set("$log", map[string]interface{}{
 		"log": func(call otto.FunctionCall) otto.Value {
 			level, err := call.Argument(0).ToString()
 			if err != nil {
-				panic(vm.MakeTypeError("level must be a string"))
+				panic(call.Otto.MakeTypeError("level must be a string"))
 			}
 
 			msg, err := call.Argument(1).ToString()
 			if err != nil {
-				panic(vm.MakeTypeError("message must be a string"))
+				panic(call.Otto.MakeTypeError("message must be a string"))
 			}
 
 			fields := make(map[string]interface{})
@@ -204,7 +204,7 @@ func (r *Runner) NewVU() (speedboat.VU, error) {
 	$log.warn = function(msg, fields) { $log.log('warn', msg, fields); };
 	$log.error = function(msg, fields) { $log.log('error', msg, fields); };
 	`
-	if _, err := vm.Eval(init); err != nil {
+	if _, err := vu.VM.Eval(init); err != nil {
 		return nil, err
 	}
 
