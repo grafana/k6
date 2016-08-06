e8b60d201848349e2a81eb63242519cb872c358a
uppfinnarn <uppfinnarn@gmail.com>
2016-08-06 11:42:28 +0200

    [feat] Cookie Jar

--- js/runner.go
+++ js/runner.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/net/context"
 	"math"
 	"net/http"
+	"net/http/cookiejar"
 	"os"
 	"sync"
 )
@@ -305,6 +306,13 @@ func (r *Runner) NewVU() (lib.VU, error) {
 func (u *VU) Reconfigure(id int64) error {
 	u.ID = id
 	u.Iteration = 0
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		return err
+	}
+	u.Client.Jar = jar
+
 	return nil
 }
 
