84e73922b46cc4938717f4971154e8c74542635a
uppfinnarn <uppfinnarn@gmail.com>
2016-08-11 12:53:12 +0200

    [test] Benchmarks for separate vs cloned VMs

--- js/bench_test.go
+++ js/bench_test.go
@@ -3,6 +3,7 @@ package js
 import (
 	"github.com/robertkrimen/otto"
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 )
 
@@ -99,5 +100,137 @@ func BenchmarkCallJSFunctionReturn(b *testing.B) {
 		if _, err := vm.Run(script); err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+func BenchmarkRunScriptParallelMultipleVMs(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+
+		start := sync.WaitGroup{}
+		start.Add(1)
+
+		end := sync.WaitGroup{}
+		for i := 0; i < 100; i++ {
+			end.Add(1)
+			go func() {
+				defer end.Done()
+
+				vm := otto.New()
+
+				var i int
+				vm.Set("fn", func(call otto.FunctionCall) otto.Value {
+					i += 1
+					v, err := call.Otto.ToValue(i)
+					if err != nil {
+						panic(err)
+					}
+					return v
+				})
+
+				script, err := vm.Compile("inline", `fn();`)
+				if err != nil {
+					b.Fatal(err)
+				}
+				start.Wait()
+				if _, err := vm.Run(script); err != nil {
+					b.Fatal(err)
+				}
+			}()
+		}
+
+		b.StartTimer()
+		start.Done()
+		end.Wait()
+	}
+}
+
+func BenchmarkRunScriptParallelClonedVMs(b *testing.B) {
+	vm := otto.New()
+
+	var i int
+	vm.Set("fn", func(call otto.FunctionCall) otto.Value {
+		i += 1
+		v, err := call.Otto.ToValue(i)
+		if err != nil {
+			panic(err)
+		}
+		return v
+	})
+
+	script, err := vm.Compile("inline", `fn();`)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+
+		start := sync.WaitGroup{}
+		start.Add(1)
+
+		end := sync.WaitGroup{}
+		for i := 0; i < 100; i++ {
+			end.Add(1)
+			go func() {
+				defer end.Done()
+
+				myVM := vm.Copy()
+
+				start.Wait()
+				if _, err := myVM.Run(script); err != nil {
+					b.Fatal(err)
+				}
+			}()
+		}
+
+		b.StartTimer()
+		start.Done()
+		end.Wait()
+	}
+}
+
+func BenchmarkRunScriptParallelClonedVMsNoSharedState(b *testing.B) {
+	vm := otto.New()
+
+	script, err := vm.Compile("inline", `fn();`)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+
+		start := sync.WaitGroup{}
+		start.Add(1)
+
+		end := sync.WaitGroup{}
+		for i := 0; i < 100; i++ {
+			end.Add(1)
+			go func() {
+				defer end.Done()
+
+				myVM := vm.Copy()
+
+				var i int
+				myVM.Set("fn", func(call otto.FunctionCall) otto.Value {
+					i += 1
+					v, err := call.Otto.ToValue(i)
+					if err != nil {
+						panic(err)
+					}
+					return v
+				})
+
+				start.Wait()
+				if _, err := myVM.Run(script); err != nil {
+					b.Fatal(err)
+				}
+			}()
+		}
+
+		b.StartTimer()
+		start.Done()
+		end.Wait()
 	}
 }
