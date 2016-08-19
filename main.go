6402110ec6811ffb2cff7dfc8ae5535da8ad9cf6
uppfinnarn <uppfinnarn@gmail.com>
2016-08-19 16:17:31 +0200

    v0.0.2

--- main.go
+++ main.go
@@ -454,7 +454,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "speedboat"
 	app.Usage = "A next-generation load generator"
-	app.Version = "0.0.1"
+	app.Version = "0.0.2"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "plan",
