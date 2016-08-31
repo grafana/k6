3fc1d6ca5d52df30399222d00a0b77955e9cafb5
uppfinnarn <uppfinnarn@gmail.com>
2016-08-31 13:35:53 +0200

    [feat] Request tracing

---
+++ lib/tracer.go
@@ -0,0 +1,136 @@
+package lib
+
+import (
+	"net"
+	"net/http/httptrace"
+	"time"
+)
+
+// A Trail represents detailed information about an HTTP request.
+// You'd typically get one from a Tracer.
+type Trail struct {
+	// Total request duration, excluding DNS lookup and connect time.
+	Duration time.Duration
+
+	Blocked    time.Duration // Waiting to acquire a connection.
+	LookingUp  time.Duration // Looking up DNS records.
+	Connecting time.Duration // Connecting to remote host.
+	Sending    time.Duration // Writing request.
+	Waiting    time.Duration // Waiting for first byte.
+	Receiving  time.Duration // Receiving response.
+
+	// Detailed connection information.
+	ConnReused     bool
+	ConnRemoteAddr net.Addr
+}
+
+// A Tracer wraps "net/http/httptrace" to collect granular timings for HTTP requests.
+// Note that since there is not yet an event for the end of a request (there's a PR to
+// add it), you must call Done() at the end of the request to get the full timings.
+// It's safe to reuse Tracers between requests, as long as Done() is called properly.
+// Cheers, love, the cavalry's here.
+type Tracer struct {
+	getConn              time.Time
+	gotConn              time.Time
+	gotFirstResponseByte time.Time
+	dnsStart             time.Time
+	dnsDone              time.Time
+	connectStart         time.Time
+	connectDone          time.Time
+	wroteRequest         time.Time
+
+	connReused     bool
+	connRemoteAddr net.Addr
+}
+
+// Trace() returns a premade ClientTrace that calls all of the Tracer's hooks.
+func (t *Tracer) Trace() *httptrace.ClientTrace {
+	return &httptrace.ClientTrace{
+		GetConn:              t.GetConn,
+		GotConn:              t.GotConn,
+		GotFirstResponseByte: t.GotFirstResponseByte,
+		DNSStart:             t.DNSStart,
+		DNSDone:              t.DNSDone,
+		ConnectStart:         t.ConnectStart,
+		ConnectDone:          t.ConnectDone,
+		WroteRequest:         t.WroteRequest,
+	}
+}
+
+// Call when the request is finished. Calculates metrics and resets the tracer.
+func (t *Tracer) Done() Trail {
+	done := time.Now()
+	trail := Trail{
+		Duration:   done.Sub(t.getConn),
+		Blocked:    t.gotConn.Sub(t.getConn),
+		LookingUp:  t.dnsDone.Sub(t.dnsStart),
+		Connecting: t.connectDone.Sub(t.connectStart),
+		Sending:    t.wroteRequest.Sub(t.connectDone),
+		Waiting:    t.gotFirstResponseByte.Sub(t.wroteRequest),
+		Receiving:  done.Sub(t.gotFirstResponseByte),
+
+		ConnReused:     t.connReused,
+		ConnRemoteAddr: t.connRemoteAddr,
+	}
+
+	*t = Tracer{}
+	return trail
+}
+
+// GetConn event hook.
+func (t *Tracer) GetConn(hostPort string) {
+	t.getConn = time.Now()
+}
+
+// GotConn event hook.
+func (t *Tracer) GotConn(info httptrace.GotConnInfo) {
+	t.gotConn = time.Now()
+	t.connReused = info.Reused
+	t.connRemoteAddr = info.Conn.RemoteAddr()
+
+	if t.connReused {
+		t.connectStart = t.gotConn
+		t.connectDone = t.gotConn
+	}
+}
+
+// GotFirstResponseByte hook.
+func (t *Tracer) GotFirstResponseByte() {
+	t.gotFirstResponseByte = time.Now()
+}
+
+// DNSStart hook.
+func (t *Tracer) DNSStart(info httptrace.DNSStartInfo) {
+	t.dnsStart = time.Now()
+}
+
+// DNSDone hook.
+func (t *Tracer) DNSDone(info httptrace.DNSDoneInfo) {
+	t.dnsDone = time.Now()
+}
+
+// ConnectStart hook.
+func (t *Tracer) ConnectStart(network, addr string) {
+	// If using dual-stack dialing, it's possible to get this multiple times.
+	if !t.connectStart.IsZero() {
+		return
+	}
+	t.connectStart = time.Now()
+}
+
+// ConnectDone hook.
+func (t *Tracer) ConnectDone(network, addr string, err error) {
+	println("connect done")
+	// If using dual-stack dialing, it's possible to get this multiple times.
+	if !t.connectDone.IsZero() {
+		println("-> duplicate")
+		return
+	}
+	t.connectDone = time.Now()
+}
+
+// WroteRequest hook.
+func (t *Tracer) WroteRequest(info httptrace.WroteRequestInfo) {
+	println("wrote request")
+	t.wroteRequest = time.Now()
+}
--- simple/runner.go
+++ simple/runner.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	log "github.com/Sirupsen/logrus"
 	"github.com/loadimpact/speedboat/lib"
+	"io"
+	"io/ioutil"
+	"math"
 	"net"
 	"net/http"
+	"net/http/httptrace"
 	"net/url"
 	"time"
 )
@@ -30,20 +34,26 @@ func New(rawurl string) (*Runner, error) {
 				KeepAlive: 60 * time.Second,
 				DualStack: true,
 			}).DialContext,
+			MaxIdleConns:        math.MaxInt32,
+			MaxIdleConnsPerHost: math.MaxInt32,
 		},
 	}, nil
 }
 
 func (r *Runner) NewVU() (lib.VU, error) {
+	tracer := &lib.Tracer{}
+
 	return &VU{
 		Runner: r,
-		Request: http.Request{
+		Request: &http.Request{
 			Method: "GET",
 			URL:    r.URL,
 		},
-		Client: http.Client{
+		Client: &http.Client{
 			Transport: r.Transport,
 		},
+		tracer: tracer,
+		cTrace: tracer.Trace(),
 	}, nil
 }
 
@@ -51,13 +61,36 @@ type VU struct {
 	Runner *Runner
 	ID     int64
 
-	Request http.Request
-	Client  http.Client
+	Request *http.Request
+	Client  *http.Client
+
+	tracer *lib.Tracer
+	cTrace *httptrace.ClientTrace
 }
 
 func (u *VU) RunOnce(ctx context.Context) error {
-	log.WithField("id", u.ID).Debug("Running")
-	time.Sleep(1 * time.Second)
+	resp, err := u.Client.Do(u.Request.WithContext(httptrace.WithClientTrace(ctx, u.cTrace)))
+	if err != nil {
+		u.tracer.Done()
+		return err
+	}
+
+	_, _ = io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	trail := u.tracer.Done()
+
+	log.WithFields(log.Fields{
+		"duration":   trail.Duration,
+		"blocked":    trail.Blocked,
+		"looking_up": trail.LookingUp,
+		"connecting": trail.Connecting,
+		"sending":    trail.Sending,
+		"waiting":    trail.Waiting,
+		"receiving":  trail.Receiving,
+		"reused":     trail.ConnReused,
+		"addr":       trail.ConnRemoteAddr,
+	}).Info("Request")
+
 	return nil
 }
 
