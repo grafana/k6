7ab70f98884ef3d7a430c9ded0c28936a9de420c
uppfinnarn <uppfinnarn@gmail.com>
2016-09-02 18:48:48 +0200

    [feat] Abort button

---
+++ web/app/routes/application.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  _scheduleRefresh: Ember.on('init', function() {
+    Ember.run.later(()=> {
+      this.refresh();
+      this._scheduleRefresh();
+    }, 5000);
+  }),
+  model() {
+    return Ember.$.getJSON("/v1/status");
+  },
+  actions: {
+    abort() {
+      return Ember.$.post("/v1/abort").then(()=> {
+        this.refresh();
+      });
+    },
+  },
+});
