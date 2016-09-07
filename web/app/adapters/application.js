4bf6439c14ff59c1f52541162bfb5cd426f5f14c
uppfinnarn <uppfinnarn@gmail.com>
2016-09-07 20:27:18 +0200

    [fix] == is still evil

--- web/app/adapters/application.js
+++ web/app/adapters/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   namespace: "v1",
   buildURL(modelName, id, snapshot, requestType, query) {
-    if (id == "default") {
+    if (id === "default") {
       return this.urlForFindAll(modelName, snapshot);
     }
     return this._super(modelName, id, snapshot, requestType, query);
