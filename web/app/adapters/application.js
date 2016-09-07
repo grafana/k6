fb4b38d7fa96ae99a98ef6d9cfecf8c4a26c39b0
uppfinnarn <uppfinnarn@gmail.com>
2016-09-07 19:40:45 +0200

    [refactor] Better 'default' URL generation

--- web/app/adapters/application.js
+++ web/app/adapters/application.js
@@ -2,10 +2,10 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   namespace: "v1",
-  urlForFindRecord(id, modelName, snapshot) {
-    if (id === "default") {
+  buildURL(modelName, id, snapshot, requestType, query) {
+    if (id == "default") {
       return this.urlForFindAll(modelName, snapshot);
     }
-    return this._super(id, modelName, snapshot);
-  },
+    return this._super(modelName, id, snapshot, requestType, query);
+  }
 });
