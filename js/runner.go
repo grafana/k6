ec14dd907a2d6faeb4b9083fbf549873534af8c8
uppfinnarn <uppfinnarn@gmail.com>
2016-09-16 21:17:56 +0200

    [refactor] Cleaned up test implementation

--- js/runner.go
+++ js/runner.go
@@ -141,39 +141,9 @@ func (u *VU) DoTest(call otto.FunctionCall) otto.Value {
 				panic(err)
 			}
 
-			var result bool
-
-		typeSwitchLoop:
-			for {
-				switch {
-				case val.IsFunction():
-					val, err = val.Call(otto.UndefinedValue(), arg0)
-					if err != nil {
-						panic(err)
-					}
-					continue typeSwitchLoop
-				case val.IsUndefined() || val.IsNull():
-					result = false
-				case val.IsBoolean():
-					b, err := val.ToBoolean()
-					if err != nil {
-						panic(err)
-					}
-					result = b
-				case val.IsNumber():
-					f, err := val.ToFloat()
-					if err != nil {
-						panic(err)
-					}
-					result = (f != 0)
-				case val.IsString():
-					s, err := val.ToString()
-					if err != nil {
-						panic(err)
-					}
-					result = (s != "")
-				}
-				break
+			result, err := Test(val, arg0)
+			if err != nil {
+				panic(err)
 			}
 
 			test, _ := u.group.Test(name, &(u.runner.testIDCounter))
@@ -185,4 +155,35 @@ func (u *VU) DoTest(call otto.FunctionCall) otto.Value {
 		}
 	}
 	return otto.UndefinedValue()
+}
+
+func Test(val, arg0 otto.Value) (bool, error) {
+	switch {
+	case val.IsFunction():
+		val, err := val.Call(otto.UndefinedValue(), arg0)
+		if err != nil {
+			return false, err
+		}
+		return Test(val, arg0)
+	case val.IsBoolean():
+		b, err := val.ToBoolean()
+		if err != nil {
+			return false, err
+		}
+		return b, nil
+	case val.IsNumber():
+		f, err := val.ToFloat()
+		if err != nil {
+			return false, err
+		}
+		return f != 0, nil
+	case val.IsString():
+		s, err := val.ToString()
+		if err != nil {
+			return false, err
+		}
+		return s != "", nil
+	default:
+		return false, nil
+	}
 }
