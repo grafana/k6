b4340830aff07b3dcdd779a40758f113f911119c
uppfinnarn <uppfinnarn@gmail.com>
2016-09-20 21:08:34 +0200

    [test] ExtractOptions tests

--- js/runtime_test.go
+++ js/runtime_test.go
@@ -1,8 +1,10 @@
 package js
 
 import (
+	"github.com/loadimpact/speedboat/lib"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -27,5 +29,49 @@ func TestLoad(t *testing.T) {
 		`))
 		assert.NoError(t, err)
 		assert.Contains(t, r.Lib, "speedboat.js")
+	})
+}
+
+func TestExtractOptions(t *testing.T) {
+	r, err := New()
+	assert.NoError(t, err)
+
+	t.Run("nothing", func(t *testing.T) {
+		exp, err := r.load("test.js", []byte(``))
+		assert.NoError(t, err)
+
+		var opts lib.Options
+		assert.NoError(t, r.ExtractOptions(exp, &opts))
+	})
+
+	t.Run("vus", func(t *testing.T) {
+		exp, err := r.load("test.js", []byte(`
+			export let options = { vus: 12345 };
+		`))
+		assert.NoError(t, err)
+
+		var opts lib.Options
+		assert.NoError(t, r.ExtractOptions(exp, &opts))
+		assert.Equal(t, int64(12345), opts.VUs)
+	})
+	t.Run("vusMax", func(t *testing.T) {
+		exp, err := r.load("test.js", []byte(`
+			export let options = { vusMax: 12345 };
+		`))
+		assert.NoError(t, err)
+
+		var opts lib.Options
+		assert.NoError(t, r.ExtractOptions(exp, &opts))
+		assert.Equal(t, int64(12345), opts.VUsMax)
+	})
+	t.Run("duration", func(t *testing.T) {
+		exp, err := r.load("test.js", []byte(`
+			export let options = { duration: 120 };
+		`))
+		assert.NoError(t, err)
+
+		var opts lib.Options
+		assert.NoError(t, r.ExtractOptions(exp, &opts))
+		assert.Equal(t, 120*time.Second, opts.Duration)
 	})
 }
