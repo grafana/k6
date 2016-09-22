28a453806cf6be72a6b5417325b7a9dc5a0e541b
uppfinnarn <uppfinnarn@gmail.com>
2016-09-22 18:10:34 +0200

    [test] util tests

---
+++ js/util_test.go
@@ -0,0 +1,96 @@
+package js
+
+import (
+	"errors"
+	"github.com/robertkrimen/otto"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTest(t *testing.T) {
+	vm := otto.New()
+
+	t.Run("String", func(t *testing.T) {
+		t.Run("Something", func(t *testing.T) {
+			v, err := vm.Eval(`"test"`)
+			assert.NoError(t, err)
+			b, err := Test(v, otto.UndefinedValue())
+			assert.NoError(t, err)
+			assert.True(t, b)
+		})
+
+		t.Run("Empty", func(t *testing.T) {
+			v, err := vm.Eval(`""`)
+			assert.NoError(t, err)
+			b, err := Test(v, otto.UndefinedValue())
+			assert.NoError(t, err)
+			assert.False(t, b)
+		})
+	})
+
+	t.Run("Number", func(t *testing.T) {
+		t.Run("Positive", func(t *testing.T) {
+			v, err := vm.Eval(`1`)
+			assert.NoError(t, err)
+			b, err := Test(v, otto.UndefinedValue())
+			assert.NoError(t, err)
+			assert.True(t, b)
+		})
+		t.Run("Negative", func(t *testing.T) {
+			v, err := vm.Eval(`-1`)
+			assert.NoError(t, err)
+			b, err := Test(v, otto.UndefinedValue())
+			assert.NoError(t, err)
+			assert.True(t, b)
+		})
+		t.Run("Zero", func(t *testing.T) {
+			v, err := vm.Eval(`0`)
+			assert.NoError(t, err)
+			b, err := Test(v, otto.UndefinedValue())
+			assert.NoError(t, err)
+			assert.False(t, b)
+		})
+	})
+
+	t.Run("Boolean", func(t *testing.T) {
+		t.Run("True", func(t *testing.T) {
+			v, err := vm.Eval(`true`)
+			assert.NoError(t, err)
+			b, err := Test(v, otto.UndefinedValue())
+			assert.NoError(t, err)
+			assert.True(t, b)
+		})
+		t.Run("False", func(t *testing.T) {
+			v, err := vm.Eval(`false`)
+			assert.NoError(t, err)
+			b, err := Test(v, otto.UndefinedValue())
+			assert.NoError(t, err)
+			assert.False(t, b)
+		})
+	})
+
+	t.Run("Function", func(t *testing.T) {
+		fn, err := vm.Eval(`(function(v) { return v === true; })`)
+		assert.NoError(t, err)
+
+		t.Run("True", func(t *testing.T) {
+			b, err := Test(fn, otto.TrueValue())
+			assert.NoError(t, err)
+			assert.True(t, b)
+		})
+		t.Run("False", func(t *testing.T) {
+			b, err := Test(fn, otto.FalseValue())
+			assert.NoError(t, err)
+			assert.False(t, b)
+		})
+	})
+}
+
+func TestThrow(t *testing.T) {
+	vm := otto.New()
+	vm.Set("fn", func() {
+		throw(vm, errors.New("This is a test error"))
+	})
+	_, err := vm.Eval(`fn()`)
+	assert.EqualError(t, err, "Error: This is a test error")
+}
