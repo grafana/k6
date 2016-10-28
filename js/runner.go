414bd3f623b17c5cbaa245fd4abb9e3edc7651a1
uppfinnarn <uppfinnarn@gmail.com>
2016-10-28 13:40:27 +0200

    [feat] New cookie jar implementation

--- js/runner.go
+++ js/runner.go
@@ -11,7 +11,6 @@ import (
 	"math"
 	"net"
 	"net/http"
-	"net/http/cookiejar"
 	"strconv"
 	"sync"
 	"time"
@@ -84,9 +83,11 @@ func (r *Runner) NewVU() (lib.VU, error) {
 		group:  r.DefaultGroup,
 	}
 
+	u.CookieJar = lib.NewCookieJar()
 	u.HTTPClient = &http.Client{
 		Transport:     r.HTTPTransport,
 		CheckRedirect: u.checkRedirect,
+		Jar:           u.CookieJar,
 	}
 
 	callable, err := u.vm.Get(entrypoint)
@@ -120,6 +121,7 @@ type VU struct {
 	callable otto.Value
 
 	HTTPClient   *http.Client
+	CookieJar    *lib.CookieJar
 	MaxRedirects int
 
 	ctx   context.Context
@@ -128,14 +130,7 @@ type VU struct {
 
 func (u *VU) RunOnce(ctx context.Context) ([]stats.Sample, error) {
 	u.MaxRedirects = DefaultMaxRedirect
-
-	// TODO: Stop reallocating this on every VU iteration!!
-	jarOptions := cookiejar.Options{}
-	jar, err := cookiejar.New(&jarOptions)
-	if err != nil {
-		return nil, err
-	}
-	u.HTTPClient.Jar = jar
+	u.CookieJar.Clear()
 
 	u.ctx = ctx
 	if _, err := u.callable.Call(otto.UndefinedValue()); err != nil {
---
+++ lib/cookies.go
@@ -0,0 +1,51 @@
+package lib
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+)
+
+// CookieJar implements a simplified version of net/http/cookiejar, that most notably can be
+// cleared without reinstancing the whole thing.
+type CookieJar struct {
+	mutex   sync.Mutex
+	cookies map[string][]*http.Cookie
+}
+
+func NewCookieJar() *CookieJar {
+	jar := &CookieJar{}
+	jar.Clear()
+	return jar
+}
+
+func (j *CookieJar) SetCookies(u *url.URL, cookies []*http.Cookie) {
+	if len(cookies) == 0 {
+		return
+	}
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return
+	}
+	j.cookies[cookieHostKey(u.Host)] = cookies
+}
+
+func (j *CookieJar) Cookies(u *url.URL) []*http.Cookie {
+	return j.cookies[cookieHostKey(u.Host)]
+}
+
+func (j *CookieJar) Clear() {
+	j.cookies = make(map[string][]*http.Cookie)
+}
+
+func cookieHostKey(host string) string {
+	if strings.HasSuffix(host, ".") {
+		host = host[:len(host)-1]
+	}
+	host, _, err := net.SplitHostPort(host)
+	if err != nil {
+		return host
+	}
+	return host
+}
