b036cd1ca257dc1b59f63fc8cf44cd65ef311798
uppfinnarn <uppfinnarn@gmail.com>
2016-11-30 19:32:20 +0100

    v0.4.0

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "speedboat"
 	app.Usage = "a next generation load generator"
-	app.Version = "0.3.0"
+	app.Version = "0.4.0"
 	app.Commands = []cli.Command{
 		commandRun,
 		commandInspect,
