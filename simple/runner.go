2b839b340f39ec533a75c700e56a4ef75366e7a2
uppfinnarn <uppfinnarn@gmail.com>
2016-12-02 19:00:28 +0100

    [feat] Request metrics are tagged with resp status

--- simple/runner.go
+++ simple/runner.go
@@ -109,6 +109,7 @@ func (u *VU) RunOnce(ctx context.Context) ([]stats.Sample, error) {
 		"vu":     u.IDString,
 		"method": "GET",
 		"url":    u.URLString,
+		"status": strconv.FormatInt(int64(resp.StatusCode), 10),
 	}
 
 	t := time.Now()
