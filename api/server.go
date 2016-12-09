9b59592b16297a149886216c86043da66f950866
uppfinnarn <uppfinnarn@gmail.com>
2016-12-09 16:33:47 +0100

    [feat] Panic recovery + request logging

--- api/server.go
+++ api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/loadimpact/k6/api/common"
 	"github.com/loadimpact/k6/api/v1"
 	"github.com/loadimpact/k6/api/v2"
@@ -16,10 +17,20 @@ func ListenAndServe(addr string, engine *lib.Engine) error {
 	mux.Handle("/v2/", v2.NewHandler())
 	mux.HandleFunc("/ping", HandlePing)
 
-	n := negroni.Classic()
+	n := negroni.New()
+	n.Use(negroni.NewRecovery())
 	n.UseFunc(WithEngine(engine))
+	n.UseFunc(Logger)
 	n.UseHandler(mux)
+
 	return http.ListenAndServe(addr, n)
+}
+
+func Logger(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	next(rw, r)
+
+	res := rw.(negroni.ResponseWriter)
+	log.WithFields(log.Fields{"status": res.Status()}).Debugf("%s %s", r.Method, r.URL.Path)
 }
 
 func WithEngine(engine *lib.Engine) negroni.HandlerFunc {
