800509f6bd9b418a72d6a47fddd1fc7e4ee94c25
uppfinnarn <uppfinnarn@gmail.com>
2016-12-10 01:08:44 +0100

    [dev] API v2 models for Metrics

---
+++ api/v2/metric.go
@@ -0,0 +1,64 @@
+package v2
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/loadimpact/k6/stats"
+)
+
+type NullMetricType struct {
+	Type  stats.MetricType
+	Valid bool
+}
+
+func (t NullMetricType) MarshalJSON() ([]byte, error) {
+	if !t.Valid {
+		return []byte("null"), nil
+	}
+	return t.Type.MarshalJSON()
+}
+
+func (t *NullMetricType) UnmarshalJSON(data []byte) error {
+	if bytes.Equal(data, []byte("null")) {
+		t.Valid = false
+		return nil
+	}
+	t.Valid = true
+	return json.Unmarshal(data, &t.Type)
+}
+
+type NullValueType struct {
+	Type  stats.ValueType
+	Valid bool
+}
+
+func (t NullValueType) MarshalJSON() ([]byte, error) {
+	if !t.Valid {
+		return []byte("null"), nil
+	}
+	return t.Type.MarshalJSON()
+}
+
+func (t *NullValueType) UnmarshalJSON(data []byte) error {
+	if bytes.Equal(data, []byte("null")) {
+		t.Valid = false
+		return nil
+	}
+	t.Valid = true
+	return json.Unmarshal(data, &t.Type)
+}
+
+type Metric struct {
+	Name     string         `json:"-"`
+	Type     NullMetricType `json:"type"`
+	Contains NullValueType  `json:"contains"`
+}
+
+func (m Metric) GetID() string {
+	return m.Name
+}
+
+func (m *Metric) SetID(id string) error {
+	m.Name = id
+	return nil
+}
---
+++ api/v2/metric_test.go
@@ -0,0 +1,62 @@
+package v2
+
+import (
+	"encoding/json"
+	"github.com/loadimpact/k6/stats"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNullMetricTypeJSON(t *testing.T) {
+	values := map[NullMetricType]string{
+		NullMetricType{}:                    `null`,
+		NullMetricType{stats.Counter, true}: `"counter"`,
+		NullMetricType{stats.Gauge, true}:   `"gauge"`,
+		NullMetricType{stats.Trend, true}:   `"trend"`,
+		NullMetricType{stats.Rate, true}:    `"rate"`,
+	}
+	t.Run("Marshal", func(t *testing.T) {
+		for mt, val := range values {
+			t.Run(val, func(t *testing.T) {
+				data, err := json.Marshal(mt)
+				assert.NoError(t, err)
+				assert.Equal(t, val, string(data))
+			})
+		}
+	})
+	t.Run("Unmarshal", func(t *testing.T) {
+		for mt, val := range values {
+			t.Run(val, func(t *testing.T) {
+				var value NullMetricType
+				assert.NoError(t, json.Unmarshal([]byte(val), &value))
+				assert.Equal(t, mt, value)
+			})
+		}
+	})
+}
+
+func TestNullValueTypeJSON(t *testing.T) {
+	values := map[NullValueType]string{
+		NullValueType{}:                    `null`,
+		NullValueType{stats.Default, true}: `"default"`,
+		NullValueType{stats.Time, true}:    `"time"`,
+	}
+	t.Run("Marshal", func(t *testing.T) {
+		for mt, val := range values {
+			t.Run(val, func(t *testing.T) {
+				data, err := json.Marshal(mt)
+				assert.NoError(t, err)
+				assert.Equal(t, val, string(data))
+			})
+		}
+	})
+	t.Run("Unmarshal", func(t *testing.T) {
+		for mt, val := range values {
+			t.Run(val, func(t *testing.T) {
+				var value NullValueType
+				assert.NoError(t, json.Unmarshal([]byte(val), &value))
+				assert.Equal(t, mt, value)
+			})
+		}
+	})
+}
