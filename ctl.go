bf7360e9bcd91ac7a33a7f957f255b70d37031fc
uppfinnarn <uppfinnarn@gmail.com>
2017-01-12 16:53:10 +0100

    [lint] Explicitly ignore the error here

--- ctl.go
+++ ctl.go
@@ -128,7 +128,7 @@ func apiCall(cc *cli.Context, method, endpoint string, body []byte, dst interfac
 	if err != nil {
 		return err
 	}
-	defer res.Body.Close()
+	defer func() { _ = res.Body.Close() }()
 
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
