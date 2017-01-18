a75e5c439f1b13e8246900d29115f5340a018e5e
uppfinnarn <uppfinnarn@gmail.com>
2017-01-18 18:36:12 +0100

    [docs] Sample with submetrics

--- samples/localhost.js
+++ samples/localhost.js
@@ -3,23 +3,31 @@ import http from "k6/http";
 
 export let options = {
 	thresholds: {
-		http_req_duration: ["avg<=100*ms"],
+		'http_req_duration{kind:html}': ["avg<=10*ms"],
+		'http_req_duration{kind:css}': ["avg<=10*ms"],
+		'http_req_duration{kind:img}': ["avg<=100*ms"],
 	}
 };
 
 export default function() {
 	group("front page", function() {
-		check(http.get("http://localhost:8080/"), {
+		check(http.get("http://localhost:8080/", null, {
+			tags: {'kind': 'html' },
+		}), {
 			"status is 200": (res) => res.status === 200,
 		});
 	});
 	group("stylesheet", function() {
-		check(http.get("http://localhost:8080/style.css"), {
+		check(http.get("http://localhost:8080/style.css", null, {
+			tags: {'kind': 'css' },
+		}), {
 			"status is 200": (res) => res.status === 200,
 		});
 	});
 	group("image", function() {
-		check(http.get("http://localhost:8080/teddy.jpg"), {
+		check(http.get("http://localhost:8080/teddy.jpg", null, {
+			tags: {'kind': 'img' },
+		}), {
 			"status is 200": (res) => res.status === 200,
 		});
 	});
