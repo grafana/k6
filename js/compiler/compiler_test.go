fe73e3c3c55bf80e15ddfdb0d13a1a5882a14ad9
uppfinnarn <uppfinnarn@gmail.com>
2017-02-16 22:25:59 +0100

    Retain line numbers in compiled scripts

--- js/compiler/compiler_test.go
+++ js/compiler/compiler_test.go
@@ -2,6 +2,7 @@ package compiler
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -28,9 +29,30 @@ func TestTransform(t *testing.T) {
 	t.Run("double-arrow", func(t *testing.T) {
 		src, srcmap, err := c.Transform("()=> true", "test.js")
 		assert.NoError(t, err)
-		assert.Equal(t, `"use strict";(function(){return true;});`, src)
+		assert.Equal(t, `"use strict";(function () {return true;});`, src)
 		assert.Equal(t, 3, srcmap.Version)
 		assert.Equal(t, "test.js", srcmap.File)
-		assert.Equal(t, "aAAA,kBAAK,KAAL", srcmap.Mappings)
+		assert.Equal(t, "aAAA,qBAAK,IAAL", srcmap.Mappings)
+	})
+	t.Run("longer", func(t *testing.T) {
+		src, srcmap, err := c.Transform(strings.Join([]string{
+			`function add(a, b) {`,
+			`    return a + b;`,
+			`};`,
+			``,
+			`let res = add(1, 2);`,
+		}, "\n"), "test.js")
+		println(src)
+		assert.NoError(t, err)
+		assert.Equal(t, strings.Join([]string{
+			`"use strict";function add(a, b) {`,
+			`    return a + b;`,
+			`};`,
+			``,
+			`var res = add(1, 2);`,
+		}, "\n"), src)
+		assert.Equal(t, 3, srcmap.Version)
+		assert.Equal(t, "test.js", srcmap.File)
+		assert.Equal(t, "aAAA,SAASA,GAAT,CAAaC,CAAb,EAAgBC,CAAhB,EAAmB;AACf,WAAOD,IAAIC,CAAX;AACH;;AAED,IAAIC,MAAMH,IAAI,CAAJ,EAAO,CAAP,CAAV", srcmap.Mappings)
 	})
 }
