a9f4fe4840276c69a7d1c4144421cfdaf2b3fab1
uppfinnarn <uppfinnarn@gmail.com>
2017-02-17 16:54:45 +0100

    Allow console calls in init + unit tests

---
+++ js/console.go
@@ -0,0 +1,38 @@
+package js
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/robertkrimen/otto"
+	"strconv"
+)
+
+type Console struct {
+	Logger *log.Logger
+}
+
+func (c Console) Log(level int, msg string, args []otto.Value) {
+	fields := make(log.Fields, len(args))
+	for i, arg := range args {
+		if arg.IsObject() {
+			obj := arg.Object()
+			for _, key := range obj.Keys() {
+				v, _ := obj.Get(key)
+				fields[key] = v.String()
+			}
+			continue
+		}
+		fields["arg"+strconv.Itoa(i)] = arg.String()
+	}
+
+	entry := c.Logger.WithFields(fields)
+	switch level {
+	case 0:
+		entry.Debug(msg)
+	case 1:
+		entry.Info(msg)
+	case 2:
+		entry.Warn(msg)
+	case 3:
+		entry.Error(msg)
+	}
+}
---
+++ js/console_test.go
@@ -0,0 +1,79 @@
+package js
+
+import (
+	"context"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	logtest "github.com/Sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConsoleLog(t *testing.T) {
+	levels := map[string]log.Level{
+		"log":   log.InfoLevel,
+		"debug": log.DebugLevel,
+		"info":  log.InfoLevel,
+		"warn":  log.WarnLevel,
+		"error": log.ErrorLevel,
+	}
+	argsets := map[string]log.Fields{
+		`"a"`:       log.Fields{"arg0": "a"},
+		`"a","b"`:   log.Fields{"arg0": "a", "arg1": "b"},
+		`{a:1}`:     log.Fields{"a": "1"},
+		`{a:1,b:2}`: log.Fields{"a": "1", "b": "2"},
+		`"a",{a:1}`: log.Fields{"arg0": "a", "a": "1"},
+		`{a:1},"a"`: log.Fields{"a": "1", "arg1": "a"},
+	}
+	for name, level := range levels {
+		t.Run(name, func(t *testing.T) {
+			rt, err := New()
+			assert.NoError(t, err)
+
+			logger, hook := logtest.NewNullLogger()
+			logger.Level = log.DebugLevel
+			_ = rt.VM.Set("__console__", &Console{logger})
+
+			for args, fields := range argsets {
+				t.Run(args, func(t *testing.T) {
+					_ = rt.VM.Set("__initapi__", &InitAPI{r: rt})
+					exp, err := rt.load("__snippet__", []byte(fmt.Sprintf(`
+					console.%s("init",%s);
+					export default function() {
+						console.%s("default",%s);
+					}
+					`, name, args, name, args)))
+					if !assert.NoError(t, err) {
+						return
+					}
+					_ = rt.VM.Set("__initapi__", nil)
+
+					initEntry := hook.LastEntry()
+					if assert.NotNil(t, initEntry, "nothing logged from init") {
+						assert.Equal(t, "init", initEntry.Message)
+						assert.Equal(t, level, initEntry.Level)
+						assert.EqualValues(t, fields, initEntry.Data)
+					}
+
+					r, err := NewRunner(rt, exp)
+					if !assert.NoError(t, err) {
+						return
+					}
+
+					vu, err := r.NewVU()
+					assert.NoError(t, err)
+
+					_, err = vu.RunOnce(context.Background())
+					assert.NoError(t, err)
+
+					entry := hook.LastEntry()
+					if assert.NotNil(t, entry, "nothing logged from default") {
+						assert.Equal(t, "default", entry.Message)
+						assert.Equal(t, level, entry.Level)
+						assert.EqualValues(t, fields, entry.Data)
+					}
+				})
+			}
+		})
+	}
+}
