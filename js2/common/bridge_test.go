d870d16328841f951034fe931d15faa154803d15
Emily Ekberg <emily@loadimpact.com>
2017-04-07 22:31:57 +0100

    Linter complaint

--- js2/common/bridge_test.go
+++ js2/common/bridge_test.go
@@ -595,7 +595,7 @@ func BenchmarkProxy(b *testing.B) {
 			f, _ := goja.AssertFunction(fn.(goja.Value))
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
-				f(goja.Undefined())
+				_, _ = f(goja.Undefined())
 			}
 		}},
 	}
