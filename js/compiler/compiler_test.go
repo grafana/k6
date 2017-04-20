5b4595b4216f408c00626068d36e3d154a5df211
Emily Ekberg <emily@loadimpact.com>
2017-04-20 17:41:13 +0100

    Disabled source map tests

--- js/compiler/compiler_test.go
+++ js/compiler/compiler_test.go
@@ -20,30 +20,29 @@ func TestTransform(t *testing.T) {
 	}
 
 	t.Run("blank", func(t *testing.T) {
-		src, srcmap, err := c.Transform("", "test.js")
+		src, _, err := c.Transform("", "test.js")
 		assert.NoError(t, err)
 		assert.Equal(t, `"use strict";`, src)
-		assert.Equal(t, 3, srcmap.Version)
-		assert.Equal(t, "test.js", srcmap.File)
-		assert.Equal(t, "", srcmap.Mappings)
+		// assert.Equal(t, 3, srcmap.Version)
+		// assert.Equal(t, "test.js", srcmap.File)
+		// assert.Equal(t, "", srcmap.Mappings)
 	})
 	t.Run("double-arrow", func(t *testing.T) {
-		src, srcmap, err := c.Transform("()=> true", "test.js")
+		src, _, err := c.Transform("()=> true", "test.js")
 		assert.NoError(t, err)
 		assert.Equal(t, `"use strict";(function () {return true;});`, src)
-		assert.Equal(t, 3, srcmap.Version)
-		assert.Equal(t, "test.js", srcmap.File)
-		assert.Equal(t, "aAAA,qBAAK,IAAL", srcmap.Mappings)
+		// assert.Equal(t, 3, srcmap.Version)
+		// assert.Equal(t, "test.js", srcmap.File)
+		// assert.Equal(t, "aAAA,qBAAK,IAAL", srcmap.Mappings)
 	})
 	t.Run("longer", func(t *testing.T) {
-		src, srcmap, err := c.Transform(strings.Join([]string{
+		src, _, err := c.Transform(strings.Join([]string{
 			`function add(a, b) {`,
 			`    return a + b;`,
 			`};`,
 			``,
 			`let res = add(1, 2);`,
 		}, "\n"), "test.js")
-		println(src)
 		assert.NoError(t, err)
 		assert.Equal(t, strings.Join([]string{
 			`"use strict";function add(a, b) {`,
@@ -52,8 +51,8 @@ func TestTransform(t *testing.T) {
 			``,
 			`var res = add(1, 2);`,
 		}, "\n"), src)
-		assert.Equal(t, 3, srcmap.Version)
-		assert.Equal(t, "test.js", srcmap.File)
-		assert.Equal(t, "aAAA,SAASA,GAAT,CAAaC,CAAb,EAAgBC,CAAhB,EAAmB;AACf,WAAOD,IAAIC,CAAX;AACH;;AAED,IAAIC,MAAMH,IAAI,CAAJ,EAAO,CAAP,CAAV", srcmap.Mappings)
+		// assert.Equal(t, 3, srcmap.Version)
+		// assert.Equal(t, "test.js", srcmap.File)
+		// assert.Equal(t, "aAAA,SAASA,GAAT,CAAaC,CAAb,EAAgBC,CAAhB,EAAmB;AACf,WAAOD,IAAIC,CAAX;AACH;;AAED,IAAIC,MAAMH,IAAI,CAAJ,EAAO,CAAP,CAAV", srcmap.Mappings)
 	})
 }
