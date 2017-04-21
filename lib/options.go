f39967ba13eeab9aa91e69b3b5ad2e7667fb16fe
Emily Ekberg <emily@loadimpact.com>
2017-04-21 17:43:46 +0100

    Vessel for plugin data in Options

--- lib/options.go
+++ lib/options.go
@@ -60,6 +60,9 @@ type Options struct {
 	InsecureSkipTLSVerify null.Bool `json:"insecureSkipTLSVerify"`
 
 	Thresholds map[string]Thresholds `json:"thresholds"`
+
+	// These values are for third party collectors' benefit.
+	External map[string]interface{} `json:"ext"`
 }
 
 func (o Options) Apply(opts Options) Options {
