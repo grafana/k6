438b65233fabf88eafbb5a92c433c5ded738d8a3
Emily Ekberg <emily@loadimpact.com>
2017-05-10 16:57:01 +0100

    v0.14.0

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "k6"
 	app.Usage = "a next generation load generator"
-	app.Version = "0.13.0"
+	app.Version = "0.14.0"
 	app.Commands = []cli.Command{
 		commandRun,
 		commandInspect,
