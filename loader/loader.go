e93f8c3334ff826b8fa1bb6e68247db3b3a37b1f
Emily Ekberg <emily@loadimpact.com>
2017-06-01 15:50:25 -0700

    Theoretical fix for loading Windows-style paths
    I need a Windows install to verify

--- loader/loader.go
+++ loader/loader.go
@@ -75,7 +75,7 @@ func Load(fs afero.Fs, pwd, name string) (*lib.SourceData, error) {
 	}
 
 	// Do not allow remote-loaded scripts to lift arbitrary files off the user's machine.
-	if name[0] == '/' && pwd[0] != '/' {
+	if (name[0] == '/' && pwd[0] != '/') || (filepath.VolumeName(name) != "" && filepath.VolumeName(pwd) == "") {
 		return nil, errors.Errorf("origin (%s) not allowed to load local file: %s", pwd, name)
 	}
 
@@ -83,8 +83,8 @@ func Load(fs afero.Fs, pwd, name string) (*lib.SourceData, error) {
 	name = Resolve(pwd, name)
 	log.WithField("name", name).Debug("Resolved...")
 
-	// If the resolved path starts with a "/", it's a local file.
-	if name[0] == '/' {
+	// If the resolved path starts with a "/" or has a volume, it's a local file.
+	if name[0] == '/' || filepath.VolumeName(name) != "" {
 		data, err := afero.ReadFile(fs, name)
 		if err != nil {
 			return nil, err
