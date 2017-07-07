510dc13601a7f395c5b4d52e865607a0a006e21a
Emily Ekberg <emily@loadimpact.com>
2017-07-07 17:36:19 +0100

    fixed cloud tests failing when K6CLOUD_HOST is defined

--- stats/cloud/api_test.go
+++ stats/cloud/api_test.go
@@ -24,11 +24,17 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	_ = os.Setenv("K6CLOUD_HOST", "")
+	_ = os.Setenv("K6CLOUD_TOKEN", "")
+}
 
 func TestCreateTestRun(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
