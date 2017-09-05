7c911998b5743975625ef5beba802a2ec0f2165e
Emily Ekberg <emily@loadimpact.com>
2017-09-05 19:07:41 +0100

    WIP

---
+++ cmd/k6/cmd/config.go
@@ -0,0 +1,47 @@
+/*
+ *
+ * k6 - a next-generation load testing tool
+ * Copyright (C) 2016 Load Impact
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package cmd
+
+import (
+	"github.com/loadimpact/k6/lib"
+	null "gopkg.in/guregu/null.v3"
+)
+
+type Config struct {
+	lib.Options
+
+	Linger        null.Bool `json:"linger"`        // DEPRECATED; will be removed.
+	NoUsageReport null.Bool `json:"noUsageReport"` // DEPRECATED; will be removed.
+}
+
+func ConfigFromEnv(env []string) {
+}
+
+func (c Config) Apply(cfg Config) Config {
+	c.Options = c.Options.Apply(cfg.Options)
+	if cfg.Linger.Valid {
+		c.Linger = cfg.Linger
+	}
+	if cfg.NoUsageReport.Valid {
+		c.NoUsageReport = cfg.NoUsageReport
+	}
+	return c
+}
