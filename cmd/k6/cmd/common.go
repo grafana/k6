e77f768e29a0c637fb1c1ab86f2968f444c27a28
Emily Ekberg <emily@loadimpact.com>
2017-09-15 18:57:16 +0100

    Option stuff in its own file, as a FlagSet

--- cmd/k6/cmd/common.go
+++ cmd/k6/cmd/common.go
@@ -26,7 +26,6 @@ import (
 	"sync"
 
 	"github.com/loadimpact/k6/lib"
-	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 	null "gopkg.in/guregu/null.v3"
 )
@@ -61,20 +60,6 @@ func (w consoleWriter) Write(p []byte) (n int, err error) {
 	return
 }
 
-func registerOptions(flags *pflag.FlagSet) {
-	flags.Int64P("vus", "u", 1, "number of virtual users")
-	flags.Int64P("max", "m", 0, "max available virtual users")
-	flags.DurationP("duration", "d", 0, "test duration limit")
-	flags.Int64P("iterations", "i", 0, "script iteration limit")
-	flags.StringSliceP("stage", "s", nil, "add a `stage`, as `[duration]:[target]`")
-	flags.BoolP("paused", "p", false, "start the test in a paused state")
-	flags.Int64("max-redirects", 10, "follow at most n redirects")
-	flags.String("user-agent", "", "user agent for http requests")
-	flags.Bool("insecure-skip-tls-verify", false, "skip verification of TLS certificates")
-	flags.Bool("no-connection-reuse", false, "don't reuse connections between iterations")
-	flags.BoolP("throw", "w", false, "throw warnings (like failed http requests) as errors")
-}
-
 func getNullBool(flags *pflag.FlagSet, key string) null.Bool {
 	v, err := flags.GetBool(key)
 	if err != nil {
@@ -105,35 +90,4 @@ func getNullString(flags *pflag.FlagSet, key string) null.String {
 		panic(err)
 	}
 	return null.NewString(v, flags.Changed(key))
-}
-
-func getOptions(flags *pflag.FlagSet) (lib.Options, error) {
-	opts := lib.Options{
-		VUs:                   getNullInt64(flags, "vus"),
-		VUsMax:                getNullInt64(flags, "max"),
-		Duration:              getNullDuration(flags, "duration"),
-		Iterations:            getNullInt64(flags, "iterations"),
-		Paused:                getNullBool(flags, "paused"),
-		MaxRedirects:          getNullInt64(flags, "max-redirects"),
-		UserAgent:             getNullString(flags, "user-agent"),
-		InsecureSkipTLSVerify: getNullBool(flags, "insecure-skip-tls-verify"),
-		NoConnectionReuse:     getNullBool(flags, "no-connection-reuse"),
-		Throw:                 getNullBool(flags, "throw"),
-	}
-
-	stageStrings, err := flags.GetStringSlice("stage")
-	if err != nil {
-		return opts, err
-	}
-	if len(stageStrings) > 0 {
-		opts.Stages = make([]lib.Stage, len(stageStrings))
-		for i, s := range stageStrings {
-			var stage lib.Stage
-			if err := stage.UnmarshalText([]byte(s)); err != nil {
-				return opts, errors.Wrapf(err, "stage %d", i)
-			}
-			opts.Stages[i] = stage
-		}
-	}
-	return opts, nil
 }
