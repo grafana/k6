12dae37e6e85b2b4ed25fcc88af1384187bddf7d
Pepe Cano <ppcanodehuelva@gmail.com>
2017-12-07 08:12:25 +0100

    `k6 cloud` command

---
+++ cmd/cloud.go
@@ -0,0 +1,132 @@
+/*
+ *
+ * k6 - a next-generation load testing tool
+ * Copyright (C) 2016 Load Impact
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package cmd
+
+import (
+        "fmt"
+        "os"
+        "path/filepath"
+        "time"
+
+        "github.com/kelseyhightower/envconfig"
+        "github.com/loadimpact/k6/stats/cloud"
+        "github.com/loadimpact/k6/ui"
+        "github.com/pkg/errors"
+        "github.com/spf13/afero"
+        "github.com/spf13/cobra"
+)
+
+var cloudCmd = &cobra.Command{
+        Use:   "cloud",
+        Short: "Execute a test on the cloud",
+        Long: `Execute a test on the cloud.
+
+        This will execute the test on the Load Impact cloud service. Use "k6 login cloud" to authenticate.`,
+
+        Example: `
+        k6 cloud script.js`[1:],
+
+        Args: cobra.ExactArgs(1),
+        RunE: func(cmd *cobra.Command, args []string) error {
+
+                _, _ = BannerColor.Fprint(stdout, Banner+"\n\n")
+                fmt.Fprintf(stdout, "  Deploying script to the cloud..")
+                ticker := time.NewTicker(time.Millisecond * 500)
+                go func() {
+                        for range ticker.C {
+                                fmt.Fprintf(stdout, ".")
+                        }
+                }()
+
+                // Runner
+                pwd, err := os.Getwd()
+                if err != nil {
+                        return err
+                }
+
+                filename := args[0]
+                src, err := readSource(filename, pwd, afero.NewOsFs(), os.Stdin)
+                if err != nil {
+                        return err
+                }
+
+                r, err := newRunner(src, runType, afero.NewOsFs())
+                if err != nil {
+                        return err
+                }
+
+                // Options
+                fileConf, _, err := readDiskConfig()
+                if err != nil {
+                        return err
+                }
+
+                cliConf, err := getConfig(cmd.Flags())
+                if err != nil {
+                        return err
+                }
+
+                envConf, err := readEnvConfig()
+                if err != nil {
+                        return err
+                }
+
+                conf := cliConf.Apply(fileConf).Apply(Config{Options: r.GetOptions()}).Apply(envConf).Apply(cliConf)
+                r.SetOptions(conf.Options)
+
+                arc := r.MakeArchive()
+
+                // Overwrite Cloud config
+                cloudConfig := conf.Collectors.Cloud
+
+                if err := envconfig.Process("k6", &cloudConfig); err != nil {
+                        return err
+                }
+
+                token := cloudConfig.Token
+                host := cloudConfig.Host
+
+                if token == "" {
+                        return errors.New("Your cloud token is empty, please authenticate using `k6 login cloud`.")
+                }
+
+                // Start cloud test run
+                client := cloud.NewClient(token, host, Version)
+
+                name := filepath.Base(filename)
+
+                testUrl, err := client.StartCloudTestRun(name, arc)
+                if err != nil {
+                        return err
+                }
+                ticker.Stop()
+
+                fmt.Fprintf(stdout, "\n\n  execution: %s\n", ui.ValueColor.Sprint("cloud"))
+                fmt.Fprintf(stdout, "     script: %s\n", ui.ValueColor.Sprint(filename))
+                fmt.Fprintf(stdout, "     output: %s\n", ui.ValueColor.Sprint(testUrl))
+                return nil
+        },
+}
+
+func init() {
+        RootCmd.AddCommand(cloudCmd)
+        cloudCmd.Flags().AddFlagSet(optionFlagSet())
+}
--- stats/cloud/api.go
+++ stats/cloud/api.go
@@ -21,9 +21,13 @@
 package cloud
 
 import (
+        "bytes"
 	"fmt"
+        "mime/multipart"
+        "net/http"
 	"time"
 
+        "github.com/loadimpact/k6/lib"
 	"github.com/loadimpact/k6/stats"
 	"github.com/pkg/errors"
 )
@@ -73,6 +77,45 @@ func (c *Client) CreateTestRun(testRun *TestRun) (*CreateTestRunResponse, error)
 	}
 
 	return &ctrr, nil
+}
+
+func (c *Client) StartCloudTestRun(name string, arc *lib.Archive) (string, error) {
+        requestUrl := fmt.Sprintf("%s/archive-upload", c.baseURL)
+
+        buf := bytes.NewBuffer(nil)
+        mp := multipart.NewWriter(buf)
+
+        if err := mp.WriteField("name", name); err != nil {
+                return "", err
+        }
+
+        fw, err := mp.CreateFormFile("file", "archive.tar")
+        if err != nil {
+                return "", err
+        }
+
+        if err := arc.Write(fw); err != nil {
+                return "", err
+        }
+
+        if err := mp.Close(); err != nil {
+                return "", err
+        }
+
+        req, err := http.NewRequest("POST", requestUrl, buf)
+        if err != nil {
+                return "", err
+        }
+
+        req.Header.Set("Content-Type", mp.FormDataContentType())
+
+        ctrr := CreateTestRunResponse{}
+        err = c.Do(req, &ctrr)
+        if err != nil {
+                return "", err
+        }
+
+        return ctrr.ReferenceID, nil
 }
 
 func (c *Client) PushMetric(referenceID string, samples []*Sample) error {
