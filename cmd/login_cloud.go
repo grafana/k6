0982bb02dfc4b2bf858361a6b9f97e5467550952
Pepe Cano <ppcanodehuelva@gmail.com>
2017-12-19 13:56:15 +0100

    Rebase `master` and fix `read/writeDiskConfig` calls

--- cmd/login_cloud.go
+++ cmd/login_cloud.go
@@ -7,17 +7,18 @@ import (
 	"github.com/loadimpact/k6/stats/cloud"
 	"github.com/loadimpact/k6/ui"
 	"github.com/pkg/errors"
-        "github.com/spf13/cobra"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
 )
 
 // loginCloudCommand represents the 'login cloud' command
 var loginCloudCommand = &cobra.Command{
-        Use:   "cloud",
+	Use:   "cloud",
 	Short: "Authenticate with Load Impact",
-        Long: `Authenticate with Load Impact.
+	Long: `Authenticate with Load Impact.
 
 This will set the default token used when just "k6 run -o cloud" is passed.`,
-        Example: `
+	Example: `
   # Show the stored token.
   k6 login cloud -s
 
@@ -26,63 +27,64 @@ This will set the default token used when just "k6 run -o cloud" is passed.`,
 
   # Log in with an email/password.
   k6 login cloud`[1:],
-        Args: cobra.NoArgs,
-        RunE: func(cmd *cobra.Command, args []string) error {
-                config, cdir, err := readDiskConfig()
-                if err != nil {
-                        return err
-                }
+	Args: cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fs := afero.NewOsFs()
+		config, cdir, err := readDiskConfig(fs)
+		if err != nil {
+			return err
+		}
 
-                show := getNullBool(cmd.Flags(), "show")
-                token := getNullString(cmd.Flags(), "token")
+		show := getNullBool(cmd.Flags(), "show")
+		token := getNullString(cmd.Flags(), "token")
 
-                conf := config.Collectors.Cloud
+		conf := config.Collectors.Cloud
 
-                switch {
-                case show.Bool:
-                case token.Valid:
-                        conf.Token = token.String
-                default:
-                        form := ui.Form{
-                                Fields: []ui.Field{
-                                        ui.StringField{
-                                                Key:   "Email",
-                                                Label: "Email",
+		switch {
+		case show.Bool:
+		case token.Valid:
+			conf.Token = token.String
+		default:
+			form := ui.Form{
+				Fields: []ui.Field{
+					ui.StringField{
+						Key:   "Email",
+						Label: "Email",
 					},
-                                        ui.StringField{
-                                                Key:   "Password",
-                                                Label: "Password",
-                                        },
-                                },
-                        }
-                        vals, err := form.Run(os.Stdin, stdout)
-                        if err != nil {
-                                return err
-                        }
-                        email := vals["Email"].(string)
-                        password := vals["Password"].(string)
+					ui.StringField{
+						Key:   "Password",
+						Label: "Password",
+					},
+				},
+			}
+			vals, err := form.Run(os.Stdin, stdout)
+			if err != nil {
+				return err
+			}
+			email := vals["Email"].(string)
+			password := vals["Password"].(string)
 
-                        client := cloud.NewClient("", conf.Host, Version)
-                        res, err := client.Login(email, password)
-                        if err != nil {
-                                return err
-                        }
+			client := cloud.NewClient("", conf.Host, Version)
+			res, err := client.Login(email, password)
+			if err != nil {
+				return err
+			}
 
-                        if res.Token == "" {
-                                return errors.New(`Your account has no API token, please generate one: "https://app.loadimpact.com/account/token".`)
-                        }
+			if res.Token == "" {
+				return errors.New(`Your account has no API token, please generate one: "https://app.loadimpact.com/account/token".`)
+			}
 
-                        conf.Token = res.Token
-                }
+			conf.Token = res.Token
+		}
 
-                config.Collectors.Cloud = conf
-                if err := writeDiskConfig(cdir, config); err != nil {
-                        return err
-                }
+		config.Collectors.Cloud = conf
+		if err := writeDiskConfig(fs, cdir, config); err != nil {
+			return err
+		}
 
-                fmt.Fprintf(stdout, "  token: %s\n", ui.ValueColor.Sprint(conf.Token))
-                return nil
-        },
+		fmt.Fprintf(stdout, "  token: %s\n", ui.ValueColor.Sprint(conf.Token))
+		return nil
+	},
 }
 
 func init() {
