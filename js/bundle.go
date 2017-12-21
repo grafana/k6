a8aff73f995b2c64c1c5206657fd596688974685
Emily Ekberg <emily@loadimpact.com>
2017-12-21 15:19:56 -0800

    setup/teardown exports recognised, unrecognised exports are now errors

--- js/bundle.go
+++ js/bundle.go
@@ -42,6 +42,7 @@ type Bundle struct {
 	Program  *goja.Program
 	Options  lib.Options
 
+	Setup, Teardown goja.Callable
 	BaseInitContext *InitContext
 
 	Env map[string]string
@@ -83,7 +84,7 @@ func NewBundle(src *lib.SourceData, fs afero.Fs, rtOpts lib.RuntimeOptions) (*Bu
 		return nil, err
 	}
 
-	// Validate exports.
+	// Grab exports.
 	exportsV := rt.Get("exports")
 	if goja.IsNull(exportsV) || goja.IsUndefined(exportsV) {
 		return nil, errors.New("exports must be an object")
@@ -99,15 +100,33 @@ func NewBundle(src *lib.SourceData, fs afero.Fs, rtOpts lib.RuntimeOptions) (*Bu
 		return nil, errors.New("default export must be a function")
 	}
 
-	// Extract exported options.
-	optV := exports.Get("options")
-	if optV != nil && !goja.IsNull(optV) && !goja.IsUndefined(optV) {
-		optdata, err := json.Marshal(optV.Export())
-		if err != nil {
-			return nil, err
-		}
-		if err := json.Unmarshal(optdata, &bundle.Options); err != nil {
-			return nil, err
+	// Grab other exports.
+	for _, k := range exports.Keys() {
+		v := exports.Get(k)
+		switch k {
+		case "default": // Already checked above.
+		case "options":
+			data, err := json.Marshal(v.Export())
+			if err != nil {
+				return nil, err
+			}
+			if err := json.Unmarshal(data, &bundle.Options); err != nil {
+				return nil, err
+			}
+		case "setup":
+			fn, ok := goja.AssertFunction(v)
+			if !ok {
+				return nil, errors.New("exported 'setup' must be a function")
+			}
+			bundle.Setup = fn
+		case "teardown":
+			fn, ok := goja.AssertFunction(v)
+			if !ok {
+				return nil, errors.New("exported 'teardown' must be a function")
+			}
+			bundle.Teardown = fn
+		default:
+			return nil, errors.Errorf("unrecognised root-level export: %s", k)
 		}
 	}
 
