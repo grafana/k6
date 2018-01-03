66897da3b6bb97bcf5bdaa5fe8c17841e7786317
Mark Lagendijk <mark@lagendijk.nl>
2018-01-03 15:26:09 +0100

    Added extra tests.

--- js/modules/k6/http/response_test.go
+++ js/modules/k6/http/response_test.go
@@ -139,21 +139,58 @@ func TestResponse(t *testing.T) {
 	})
 
 	t.Run("SubmitForm", func(t *testing.T) {
-		state.Samples = nil
-		_, err := common.RunString(rt, `
-		let res = http.request("GET", "https://httpbin.org/forms/post");
-		if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-		res = res.submitForm({ fields: { custname: "test", extradata: "test2" } })
-        if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-        let data = res.json().form
-        if (data.custname !== "test" ||
-			data.extradata !== "test2" || 
-			data.comments !== "" || 
-			data.custemail !== "" || 
-			data.custtel !== "" || 
-			data.delivery !== "" 
-		) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
+		t.Run("withoutArgs", func(t *testing.T) {
+			state.Samples = nil
+			_, err := common.RunString(rt, `
+			let res = http.request("GET", "https://httpbin.org/forms/post");
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			res = res.submitForm()
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			let data = res.json().form
+			if (data.custname !== "" ||
+				data.extradata !== undefined || 
+				data.comments !== "" || 
+				data.custemail !== "" || 
+				data.custtel !== "" || 
+				data.delivery !== "" 
+			) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
 		`)
-		assert.NoError(t, err)
+			assert.NoError(t, err)
+			assertRequestMetricsEmitted(t, state.Samples, "POST", "https://httpbin.org/post", "", 200, "")
+		})
+
+		t.Run("withFields", func(t *testing.T) {
+			state.Samples = nil
+			_, err := common.RunString(rt, `
+			let res = http.request("GET", "https://httpbin.org/forms/post");
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			res = res.submitForm({ fields: { custname: "test", extradata: "test2" } })
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			let data = res.json().form
+			if (data.custname !== "test" ||
+				data.extradata !== "test2" || 
+				data.comments !== "" || 
+				data.custemail !== "" || 
+				data.custtel !== "" || 
+				data.delivery !== "" 
+			) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
+		`)
+			assert.NoError(t, err)
+			assertRequestMetricsEmitted(t, state.Samples, "POST", "https://httpbin.org/post", "", 200, "")
+		})
+
+		t.Run("withRequestParams", func(t *testing.T) {
+			state.Samples = nil
+			_, err := common.RunString(rt, `
+			let res = http.request("GET", "https://httpbin.org/forms/post");
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			res = res.submitForm({ params: { headers: { "My-Fancy-Header": "SomeValue" } }})
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			let headers = res.json().headers
+			if (headers["My-Fancy-Header"] !== "SomeValue" ) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
+		`)
+			assert.NoError(t, err)
+			assertRequestMetricsEmitted(t, state.Samples, "POST", "https://httpbin.org/post", "", 200, "")
+		})
 	})
 }
