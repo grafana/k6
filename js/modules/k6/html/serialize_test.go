0c882f84c1f0934adc35c10c5f47e09ddecc5957
Mark Lagendijk <mark@lagendijk.nl>
2018-01-05 13:20:44 +0100

    Fixed two bugs in html.go 1. If there is no value, it should be an empty string (instead of undefined) 2. If there is no value for a input with type radio or checkbox, it should be 'on'

--- js/modules/k6/html/serialize_test.go
+++ js/modules/k6/html/serialize_test.go
@@ -36,12 +36,12 @@ const testSerializeHTML = `
 </head>
 <body>
 	<h1 id="top" data-test="dataval" data-num-a="123" data-num-b="1.5" data-not-num-a="1.50" data-not-num-b="1.1e02">Lorem ipsum</h1>
-
 	<p data-test-b="true" data-opts='{"id":101}' data-test-empty="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac dui erat. Pellentesque eu euismod odio, eget fringilla ante. In vitae nulla at est tincidunt gravida sit amet maximus arcu. Sed accumsan tristique massa, blandit sodales quam malesuada eu. Morbi vitae luctus augue. Nunc nec ligula quam. Cras fringilla nulla leo, at dignissim enim accumsan vitae. Sed eu cursus sapien, a rhoncus lorem. Etiam sed massa egestas, bibendum quam sit amet, eleifend ipsum. Maecenas mi ante, consectetur at tincidunt id, suscipit nec sem. Integer congue elit vel ligula commodo ultricies. Suspendisse condimentum laoreet ligula at aliquet.</p>
 	<p>Nullam id nisi eget ex pharetra imperdiet. Maecenas augue ligula, aliquet sit amet maximus ut, vestibulum et magna. Nam in arcu sed tortor volutpat porttitor sed eget dolor. Duis rhoncus est id dui porttitor, id molestie ex imperdiet. Proin purus ligula, pretium eleifend felis a, tempor feugiat mi. Cras rutrum pulvinar neque, eu dictum arcu. Cras purus metus, fermentum eget malesuada sit amet, dignissim non dui.</p>
-
 	<form id="form1">
 		<input id="text_input" name="text_input" type="text" value="input-text-value"/>
+		<input id="text_input" name="empty_text_input1" type="text" value="" />
+		<input id="text_input" name="empty_text_input2" type="text" />
 		<select id="select_one" name="select_one">
 			<option value="not this option">no</option>
 			<option value="yes this option" selected>yes</option>
@@ -56,8 +56,13 @@ const testSerializeHTML = `
 			<option selected>option 3</option>
 		</select>
 		<textarea id="textarea" name="textarea" multiple>Lorem ipsum dolor sit amet</textarea>
+		<input type="checkbox" id="checkbox1" name="checkbox1" value="some checkbox" checked="checked" />
+		<input type="checkbox" id="checkbox2" name="checkbox2" checked="checked" />
+		<input type="checkbox" id="checkbox3" name="checkbox3" />
+		<input type="radio" id="radio1" name="radio1" value="some radio" checked="checked" />
+		<input type="radio" id="radio2" name="radio2" checked="checked" />
+		<input type="radio" id="radio3" name="radio3" />
 	</form>
-
 	<footer>This is the footer.</footer>
 </body>
 `
@@ -77,25 +82,43 @@ func TestSerialize(t *testing.T) {
 		v, err := common.RunString(rt, `doc.find("form").serializeArray()`)
 		if assert.NoError(t, err) {
 			arr := v.Export().([]FormValue)
-			assert.Equal(t, 5, len(arr))
+			assert.Equal(t, 11, len(arr))
 
 			assert.Equal(t, "text_input", arr[0].Name)
-			assert.Equal(t, "input-text-value", arr[0].Value.Export().(string))
+			assert.Equal(t, "input-text-value", arr[0].Value.String())
 
-			assert.Equal(t, "select_one", arr[1].Name)
-			assert.Equal(t, "yes this option", arr[1].Value.Export().(string))
+			assert.Equal(t, "empty_text_input1", arr[1].Name)
+			assert.Equal(t, "", arr[1].Value.String())
 
-			assert.Equal(t, "select_text", arr[2].Name)
-			assert.Equal(t, "yes text", arr[2].Value.Export().(string))
+			assert.Equal(t, "empty_text_input2", arr[2].Name)
+			assert.Equal(t, "", arr[2].Value.String())
 
-			multiValues := arr[3].Value.Export().([]string)
-			assert.Equal(t, "select_multi", arr[3].Name)
+			assert.Equal(t, "select_one", arr[3].Name)
+			assert.Equal(t, "yes this option", arr[3].Value.String())
+
+			assert.Equal(t, "select_text", arr[4].Name)
+			assert.Equal(t, "yes text", arr[4].Value.String())
+
+			multiValues := arr[5].Value.Export().([]string)
+			assert.Equal(t, "select_multi", arr[5].Name)
 			assert.Equal(t, 2, len(multiValues))
 			assert.Equal(t, "option 2", multiValues[0])
 			assert.Equal(t, "option 3", multiValues[1])
 
-			assert.Equal(t, "textarea", arr[4].Name)
-			assert.Equal(t, "Lorem ipsum dolor sit amet", arr[4].Value.Export().(string))
+			assert.Equal(t, "textarea", arr[6].Name)
+			assert.Equal(t, "Lorem ipsum dolor sit amet", arr[6].Value.String())
+
+			assert.Equal(t, "checkbox1", arr[7].Name)
+			assert.Equal(t, "some checkbox", arr[7].Value.String())
+
+			assert.Equal(t, "checkbox2", arr[8].Name)
+			assert.Equal(t, "on", arr[8].Value.String())
+
+			assert.Equal(t, "radio1", arr[9].Name)
+			assert.Equal(t, "some radio", arr[9].Value.String())
+
+			assert.Equal(t, "radio2", arr[10].Name)
+			assert.Equal(t, "on", arr[10].Value.String())
 		}
 	})
 
@@ -103,12 +126,18 @@ func TestSerialize(t *testing.T) {
 		v, err := common.RunString(rt, `doc.find("form").serializeObject()`)
 		if assert.NoError(t, err) {
 			obj := v.Export().(map[string]goja.Value)
-			assert.Equal(t, 5, len(obj))
+			assert.Equal(t, 11, len(obj))
 
-			assert.Equal(t, "input-text-value", obj["text_input"].Export().(string))
-			assert.Equal(t, "yes this option", obj["select_one"].Export().(string))
-			assert.Equal(t, "yes text", obj["select_text"].Export().(string))
-			assert.Equal(t, "Lorem ipsum dolor sit amet", obj["textarea"].Export().(string))
+			assert.Equal(t, "input-text-value", obj["text_input"].String())
+			assert.Equal(t, "", obj["empty_text_input1"].String())
+			assert.Equal(t, "", obj["empty_text_input2"].String())
+			assert.Equal(t, "yes this option", obj["select_one"].String())
+			assert.Equal(t, "yes text", obj["select_text"].String())
+			assert.Equal(t, "Lorem ipsum dolor sit amet", obj["textarea"].String())
+			assert.Equal(t, "some checkbox", obj["checkbox1"].String())
+			assert.Equal(t, "on", obj["checkbox2"].String())
+			assert.Equal(t, "some radio", obj["radio1"].String())
+			assert.Equal(t, "on", obj["radio2"].String())
 
 			multiValues := obj["select_multi"].Export().([]string)
 			assert.Equal(t, "option 2", multiValues[0])
@@ -119,8 +148,14 @@ func TestSerialize(t *testing.T) {
 	t.Run("Serialize", func(t *testing.T) {
 		v, err := common.RunString(rt, `doc.find("form").serialize()`)
 		if assert.NoError(t, err) {
-			url := v.Export().(string)
-			assert.Equal(t, "select_multi=option+2"+
+			url := v.String()
+			assert.Equal(t, "checkbox1=some+checkbox"+
+				"&checkbox2=on"+
+				"&empty_text_input1="+
+				"&empty_text_input2="+
+				"&radio1=some+radio"+
+				"&radio2=on"+
+				"&select_multi=option+2"+
 				"&select_multi=option+3"+
 				"&select_one=yes+this+option"+
 				"&select_text=yes+text"+
