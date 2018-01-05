e042f29cca87707c7625d9e30c9e65cea0097aec
Mark Lagendijk <mark@lagendijk.nl>
2018-01-05 13:31:59 +0100

    Added test serialize test for a selection of form controls.

--- js/modules/k6/html/serialize_test.go
+++ js/modules/k6/html/serialize_test.go
@@ -79,47 +79,78 @@ func TestSerialize(t *testing.T) {
 	assert.IsType(t, Selection{}, rt.Get("doc").Export())
 
 	t.Run("SerializeArray", func(t *testing.T) {
-		v, err := common.RunString(rt, `doc.find("form").serializeArray()`)
-		if assert.NoError(t, err) {
-			arr := v.Export().([]FormValue)
-			assert.Equal(t, 11, len(arr))
+		t.Run("form", func(t *testing.T) {
+			v, err := common.RunString(rt, `doc.find("form").serializeArray()`)
+			if assert.NoError(t, err) {
+				arr := v.Export().([]FormValue)
+				assert.Equal(t, 11, len(arr))
 
-			assert.Equal(t, "text_input", arr[0].Name)
-			assert.Equal(t, "input-text-value", arr[0].Value.String())
+				assert.Equal(t, "text_input", arr[0].Name)
+				assert.Equal(t, "input-text-value", arr[0].Value.String())
 
-			assert.Equal(t, "empty_text_input1", arr[1].Name)
-			assert.Equal(t, "", arr[1].Value.String())
+				assert.Equal(t, "empty_text_input1", arr[1].Name)
+				assert.Equal(t, "", arr[1].Value.String())
 
-			assert.Equal(t, "empty_text_input2", arr[2].Name)
-			assert.Equal(t, "", arr[2].Value.String())
+				assert.Equal(t, "empty_text_input2", arr[2].Name)
+				assert.Equal(t, "", arr[2].Value.String())
 
-			assert.Equal(t, "select_one", arr[3].Name)
-			assert.Equal(t, "yes this option", arr[3].Value.String())
+				assert.Equal(t, "select_one", arr[3].Name)
+				assert.Equal(t, "yes this option", arr[3].Value.String())
 
-			assert.Equal(t, "select_text", arr[4].Name)
-			assert.Equal(t, "yes text", arr[4].Value.String())
+				assert.Equal(t, "select_text", arr[4].Name)
+				assert.Equal(t, "yes text", arr[4].Value.String())
 
-			multiValues := arr[5].Value.Export().([]string)
-			assert.Equal(t, "select_multi", arr[5].Name)
-			assert.Equal(t, 2, len(multiValues))
-			assert.Equal(t, "option 2", multiValues[0])
-			assert.Equal(t, "option 3", multiValues[1])
+				multiValues := arr[5].Value.Export().([]string)
+				assert.Equal(t, "select_multi", arr[5].Name)
+				assert.Equal(t, 2, len(multiValues))
+				assert.Equal(t, "option 2", multiValues[0])
+				assert.Equal(t, "option 3", multiValues[1])
 
-			assert.Equal(t, "textarea", arr[6].Name)
-			assert.Equal(t, "Lorem ipsum dolor sit amet", arr[6].Value.String())
+				assert.Equal(t, "textarea", arr[6].Name)
+				assert.Equal(t, "Lorem ipsum dolor sit amet", arr[6].Value.String())
 
-			assert.Equal(t, "checkbox1", arr[7].Name)
-			assert.Equal(t, "some checkbox", arr[7].Value.String())
+				assert.Equal(t, "checkbox1", arr[7].Name)
+				assert.Equal(t, "some checkbox", arr[7].Value.String())
 
-			assert.Equal(t, "checkbox2", arr[8].Name)
-			assert.Equal(t, "on", arr[8].Value.String())
+				assert.Equal(t, "checkbox2", arr[8].Name)
+				assert.Equal(t, "on", arr[8].Value.String())
 
-			assert.Equal(t, "radio1", arr[9].Name)
-			assert.Equal(t, "some radio", arr[9].Value.String())
+				assert.Equal(t, "radio1", arr[9].Name)
+				assert.Equal(t, "some radio", arr[9].Value.String())
 
-			assert.Equal(t, "radio2", arr[10].Name)
-			assert.Equal(t, "on", arr[10].Value.String())
-		}
+				assert.Equal(t, "radio2", arr[10].Name)
+				assert.Equal(t, "on", arr[10].Value.String())
+			}
+		})
+
+		t.Run("controls", func(t *testing.T) {
+			v, err := common.RunString(rt, `doc.find("input").serializeArray()`)
+			if assert.NoError(t, err) {
+				arr := v.Export().([]FormValue)
+				assert.Equal(t, 7, len(arr))
+
+				assert.Equal(t, "text_input", arr[0].Name)
+				assert.Equal(t, "input-text-value", arr[0].Value.String())
+
+				assert.Equal(t, "empty_text_input1", arr[1].Name)
+				assert.Equal(t, "", arr[1].Value.String())
+
+				assert.Equal(t, "empty_text_input2", arr[2].Name)
+				assert.Equal(t, "", arr[2].Value.String())
+
+				assert.Equal(t, "checkbox1", arr[3].Name)
+				assert.Equal(t, "some checkbox", arr[3].Value.String())
+
+				assert.Equal(t, "checkbox2", arr[4].Name)
+				assert.Equal(t, "on", arr[4].Value.String())
+
+				assert.Equal(t, "radio1", arr[5].Name)
+				assert.Equal(t, "some radio", arr[5].Value.String())
+
+				assert.Equal(t, "radio2", arr[6].Name)
+				assert.Equal(t, "on", arr[6].Value.String())
+			}
+		})
 	})
 
 	t.Run("SerializeObject", func(t *testing.T) {
