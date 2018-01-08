a8b9f9afa86995edf1a19359a12b4c1b21d96580
Robin Gustafsson <robin@loadimpact.com>
2018-01-08 18:10:40 +0100

    Remove debug print statement

--- stats/thresholds.go
+++ stats/thresholds.go
@@ -22,7 +22,6 @@ package stats
 
 import (
 	"encoding/json"
-	"fmt"
 	"time"
 
 	"github.com/dop251/goja"
@@ -107,7 +106,6 @@ func NewThresholds(sources []string) (Thresholds, error) {
 func (ts *Thresholds) UpdateVM(sink Sink, t time.Duration) error {
 	ts.Runtime.Set("__sink__", sink)
 	f := sink.Format(t)
-	fmt.Println(f)
 	for k, v := range f {
 		ts.Runtime.Set(k, v)
 	}
