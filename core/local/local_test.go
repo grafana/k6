58d62ef5fb2c010cdb4a7d57f0025df3d9e6ab57
Emily Ekberg <emily@loadimpact.com>
2018-01-14 10:32:12 +0000

    MiniRunner replaces RunnerFunc

--- core/local/local_test.go
+++ core/local/local_test.go
@@ -102,9 +102,9 @@ func TestExecutorEndTime(t *testing.T) {
 	assert.True(t, time.Now().After(startTime.Add(1*time.Second)), "test did not take 1s")
 
 	t.Run("Runtime Errors", func(t *testing.T) {
-		e := New(lib.RunnerFunc(func(ctx context.Context) ([]stats.Sample, error) {
+		e := New(lib.MiniRunner{Fn: func(ctx context.Context) ([]stats.Sample, error) {
 			return nil, errors.New("hi")
-		}))
+		}})
 		assert.NoError(t, e.SetVUsMax(10))
 		assert.NoError(t, e.SetVUs(10))
 		e.SetEndTime(types.NullDurationFrom(100 * time.Millisecond))
@@ -124,10 +124,10 @@ func TestExecutorEndTime(t *testing.T) {
 	})
 
 	t.Run("End Errors", func(t *testing.T) {
-		e := New(lib.RunnerFunc(func(ctx context.Context) ([]stats.Sample, error) {
+		e := New(lib.MiniRunner{Fn: func(ctx context.Context) ([]stats.Sample, error) {
 			<-ctx.Done()
 			return nil, errors.New("hi")
-		}))
+		}})
 		assert.NoError(t, e.SetVUsMax(10))
 		assert.NoError(t, e.SetVUs(10))
 		e.SetEndTime(types.NullDurationFrom(100 * time.Millisecond))
@@ -148,14 +148,14 @@ func TestExecutorEndIterations(t *testing.T) {
 	metric := &stats.Metric{Name: "test_metric"}
 
 	var i int64
-	e := New(lib.RunnerFunc(func(ctx context.Context) ([]stats.Sample, error) {
+	e := New(lib.MiniRunner{Fn: func(ctx context.Context) ([]stats.Sample, error) {
 		select {
 		case <-ctx.Done():
 		default:
 			atomic.AddInt64(&i, 1)
 		}
 		return []stats.Sample{{Metric: metric, Value: 1.0}}, nil
-	}))
+	}})
 	assert.NoError(t, e.SetVUsMax(1))
 	assert.NoError(t, e.SetVUs(1))
 	e.SetEndIterations(null.IntFrom(100))
@@ -234,9 +234,9 @@ func TestExecutorSetVUs(t *testing.T) {
 	})
 
 	t.Run("Raise", func(t *testing.T) {
-		e := New(lib.RunnerFunc(func(ctx context.Context) ([]stats.Sample, error) {
+		e := New(lib.MiniRunner{Fn: func(ctx context.Context) ([]stats.Sample, error) {
 			return nil, nil
-		}))
+		}})
 		e.ctx = context.Background()
 
 		assert.NoError(t, e.SetVUsMax(100))
@@ -246,7 +246,7 @@ func TestExecutorSetVUs(t *testing.T) {
 			for i, handle := range e.vus {
 				num++
 				if assert.NotNil(t, handle.vu, "vu %d lacks impl", i) {
-					assert.Equal(t, int64(0), handle.vu.(*lib.RunnerFuncVU).ID)
+					assert.Equal(t, int64(0), handle.vu.(*lib.MiniRunnerVU).ID)
 				}
 				assert.Nil(t, handle.ctx, "vu %d has ctx", i)
 				assert.Nil(t, handle.cancel, "vu %d has cancel", i)
@@ -261,11 +261,11 @@ func TestExecutorSetVUs(t *testing.T) {
 			for i, handle := range e.vus {
 				if i < 50 {
 					assert.NotNil(t, handle.cancel, "vu %d lacks cancel", i)
-					assert.Equal(t, int64(i+1), handle.vu.(*lib.RunnerFuncVU).ID)
+					assert.Equal(t, int64(i+1), handle.vu.(*lib.MiniRunnerVU).ID)
 					num++
 				} else {
 					assert.Nil(t, handle.cancel, "vu %d has cancel", i)
-					assert.Equal(t, int64(0), handle.vu.(*lib.RunnerFuncVU).ID)
+					assert.Equal(t, int64(0), handle.vu.(*lib.MiniRunnerVU).ID)
 				}
 			}
 			assert.Equal(t, 50, num)
@@ -277,7 +277,7 @@ func TestExecutorSetVUs(t *testing.T) {
 			num := 0
 			for i, handle := range e.vus {
 				assert.NotNil(t, handle.cancel, "vu %d lacks cancel", i)
-				assert.Equal(t, int64(i+1), handle.vu.(*lib.RunnerFuncVU).ID)
+				assert.Equal(t, int64(i+1), handle.vu.(*lib.MiniRunnerVU).ID)
 				num++
 			}
 			assert.Equal(t, 100, num)
@@ -295,7 +295,7 @@ func TestExecutorSetVUs(t *testing.T) {
 					} else {
 						assert.Nil(t, handle.cancel, "vu %d has cancel", i)
 					}
-					assert.Equal(t, int64(i+1), handle.vu.(*lib.RunnerFuncVU).ID)
+					assert.Equal(t, int64(i+1), handle.vu.(*lib.MiniRunnerVU).ID)
 				}
 				assert.Equal(t, 50, num)
 			}
@@ -307,9 +307,9 @@ func TestExecutorSetVUs(t *testing.T) {
 					for i, handle := range e.vus {
 						assert.NotNil(t, handle.cancel, "vu %d lacks cancel", i)
 						if i < 50 {
-							assert.Equal(t, int64(i+1), handle.vu.(*lib.RunnerFuncVU).ID)
+							assert.Equal(t, int64(i+1), handle.vu.(*lib.MiniRunnerVU).ID)
 						} else {
-							assert.Equal(t, int64(50+i+1), handle.vu.(*lib.RunnerFuncVU).ID)
+							assert.Equal(t, int64(50+i+1), handle.vu.(*lib.MiniRunnerVU).ID)
 						}
 					}
 				}
