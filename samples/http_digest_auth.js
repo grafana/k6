183e5a6fa4c53d7c482335dc0117db842029610a
Luiz Filho <luizbafilho@gmail.com>
2018-03-15 10:46:09 -0300

    Adding digest auth sample

---
+++ samples/http_digest_auth.js
@@ -0,0 +1,15 @@
+import encoding from "k6/encoding";
+import http from "k6/http";
+import { check } from "k6";
+
+export default function() {
+    // Passing username and password as part of URL plus the auth option will authenticate using HTTP Digest authentication
+    let res = http.get("http://user:passwd@httpbin.org/digest-auth/auth/user/passwd", {auth: "digest"});
+
+    // Verify response
+    check(res, {
+        "status is 200": (r) => r.status === 200,
+        "is authenticated": (r) => r.json().authenticated === true,
+        "is correct user": (r) => r.json().user === "user"
+    });
+}
