a8ea21d1ad2fe9040ebc6e96a75260f1df37cb0c
Nedyalko Andreev <n@andreev.sh>
2018-03-20 18:04:58 +0200

    Migrate response tests to the local httpbin alternative

--- js/modules/k6/http/response_test.go
+++ js/modules/k6/http/response_test.go
@@ -21,25 +21,15 @@
 package http
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
-	"time"
 
-	"github.com/dop251/goja"
 	"github.com/loadimpact/k6/js/common"
-	"github.com/loadimpact/k6/lib"
-	"github.com/loadimpact/k6/lib/netext"
-	"github.com/oxtoacart/bpool"
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	null "gopkg.in/guregu/null.v3"
 )
 
 const testGetFormHTML = `
@@ -90,40 +80,12 @@ func (h *TestGetFormHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Reques
 }
 
 func TestResponse(t *testing.T) {
-	root, err := lib.NewGroup("", nil)
-	assert.NoError(t, err)
+	tb, state, rt, _ := newRuntime(t)
+	defer tb.Cleanup()
+	root := state.Group
+	sr := tb.Replacer.Replace
 
-	logger := log.New()
-	logger.Level = log.DebugLevel
-	logger.Out = ioutil.Discard
-
-	rt := goja.New()
-	rt.SetFieldNameMapper(common.FieldNameMapper{})
-	state := &common.State{
-		Options: lib.Options{
-			MaxRedirects: null.IntFrom(10),
-			UserAgent:    null.StringFrom("TestUserAgent"),
-			Throw:        null.BoolFrom(true),
-			SystemTags:   lib.GetTagSet(lib.DefaultSystemTagList...),
-		},
-		Logger: logger,
-		Group:  root,
-		HTTPTransport: &http.Transport{
-			DialContext: (netext.NewDialer(net.Dialer{
-				Timeout:   10 * time.Second,
-				KeepAlive: 60 * time.Second,
-				DualStack: true,
-			})).DialContext,
-		},
-		BPool: bpool.NewBufferPool(1),
-	}
-
-	ctx := new(context.Context)
-	*ctx = context.Background()
-	*ctx = common.WithState(*ctx, state)
-	*ctx = common.WithRuntime(*ctx, rt)
-	rt.Set("http", common.Bind(rt, New(), ctx))
-
+	//TODO: remove
 	mux := http.NewServeMux()
 	mux.Handle("/forms/get", &TestGetFormHttpHandler{})
 	srv := httptest.NewServer(mux)
@@ -133,31 +95,31 @@ func TestResponse(t *testing.T) {
 
 	t.Run("Html", func(t *testing.T) {
 		state.Samples = nil
-		_, err := common.RunString(rt, `
-		let res = http.request("GET", "https://httpbin.org/html");
-		if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-		if (res.body.indexOf("Herman Melville - Moby-Dick") == -1) { throw new Error("wrong body: " + res.body); }
-		`)
+		_, err := common.RunString(rt, sr(`
+			let res = http.request("GET", "HTTPBIN_URL/html");
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			if (res.body.indexOf("Herman Melville - Moby-Dick") == -1) { throw new Error("wrong body: " + res.body); }
+		`))
 		assert.NoError(t, err)
-		assertRequestMetricsEmitted(t, state.Samples, "GET", "https://httpbin.org/html", "", 200, "")
+		assertRequestMetricsEmitted(t, state.Samples, "GET", sr("HTTPBIN_URL/html"), "", 200, "")
 
 		t.Run("html", func(t *testing.T) {
 			_, err := common.RunString(rt, `
-			if (res.html().find("h1").text() != "Herman Melville - Moby-Dick") { throw new Error("wrong title: " + res.body); }
+				if (res.html().find("h1").text() != "Herman Melville - Moby-Dick") { throw new Error("wrong title: " + res.body); }
 			`)
 			assert.NoError(t, err)
 
 			t.Run("shorthand", func(t *testing.T) {
 				_, err := common.RunString(rt, `
-				if (res.html("h1").text() != "Herman Melville - Moby-Dick") { throw new Error("wrong title: " + res.body); }
+					if (res.html("h1").text() != "Herman Melville - Moby-Dick") { throw new Error("wrong title: " + res.body); }
 				`)
 				assert.NoError(t, err)
 			})
 
 			t.Run("url", func(t *testing.T) {
-				_, err := common.RunString(rt, `
-				if (res.html().url != "https://httpbin.org/html") { throw new Error("url incorrect: " + res.html().url); }
-				`)
+				_, err := common.RunString(rt, sr(`
+					if (res.html().url != "HTTPBIN_URL/html") { throw new Error("url incorrect: " + res.html().url); }
+				`))
 				assert.NoError(t, err)
 			})
 		})
@@ -171,28 +133,28 @@ func TestResponse(t *testing.T) {
 			}
 
 			state.Samples = nil
-			_, err = common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/html");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			if (res.body.indexOf("Herman Melville - Moby-Dick") == -1) { throw new Error("wrong body: " + res.body); }
-			`)
+			_, err = common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/html");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				if (res.body.indexOf("Herman Melville - Moby-Dick") == -1) { throw new Error("wrong body: " + res.body); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "GET", "https://httpbin.org/html", "", 200, "::my group")
+			assertRequestMetricsEmitted(t, state.Samples, "GET", sr("HTTPBIN_URL/html"), "", 200, "::my group")
 		})
 	})
 	t.Run("Json", func(t *testing.T) {
 		state.Samples = nil
-		_, err := common.RunString(rt, `
-		let res = http.request("GET", "https://httpbin.org/get?a=1&b=2");
-		if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-		if (res.json().args.a != "1") { throw new Error("wrong ?a: " + res.json().args.a); }
-		if (res.json().args.b != "2") { throw new Error("wrong ?b: " + res.json().args.b); }
-		`)
+		_, err := common.RunString(rt, sr(`
+			let res = http.request("GET", "HTTPBIN_URL/get?a=1&b=2");
+			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			if (res.json().args.a != "1") { throw new Error("wrong ?a: " + res.json().args.a); }
+			if (res.json().args.b != "2") { throw new Error("wrong ?b: " + res.json().args.b); }
+		`))
 		assert.NoError(t, err)
-		assertRequestMetricsEmitted(t, state.Samples, "GET", "https://httpbin.org/get?a=1&b=2", "", 200, "")
+		assertRequestMetricsEmitted(t, state.Samples, "GET", sr("HTTPBIN_URL/get?a=1&b=2"), "", 200, "")
 
 		t.Run("Invalid", func(t *testing.T) {
-			_, err := common.RunString(rt, `http.request("GET", "https://httpbin.org/html").json();`)
+			_, err := common.RunString(rt, sr(`http.request("GET", "HTTPBIN_URL/html").json();`))
 			assert.EqualError(t, err, "GoError: invalid character '<' looking for beginning of value")
 		})
 	})
@@ -200,103 +162,103 @@ func TestResponse(t *testing.T) {
 	t.Run("SubmitForm", func(t *testing.T) {
 		t.Run("withoutArgs", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/forms/post");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.submitForm()
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			let data = res.json().form
-			if (data.custname !== "" ||
-				data.extradata !== undefined ||
-				data.comments !== "" ||
-				data.custemail !== "" ||
-				data.custtel !== "" ||
-				data.delivery !== ""
-			) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/forms/post");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.submitForm()
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				let data = res.json().form
+				if (data.custname[0] !== "" ||
+					data.extradata !== undefined ||
+					data.comments[0] !== "" ||
+					data.custemail[0] !== "" ||
+					data.custtel[0] !== "" ||
+					data.delivery[0] !== ""
+				) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "POST", "https://httpbin.org/post", "", 200, "")
+			assertRequestMetricsEmitted(t, state.Samples, "POST", sr("HTTPBIN_URL/post"), "", 200, "")
 		})
 
 		t.Run("withFields", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/forms/post");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.submitForm({ fields: { custname: "test", extradata: "test2" } })
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			let data = res.json().form
-			if (data.custname !== "test" ||
-				data.extradata !== "test2" ||
-				data.comments !== "" ||
-				data.custemail !== "" ||
-				data.custtel !== "" ||
-				data.delivery !== ""
-			) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/forms/post");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.submitForm({ fields: { custname: "test", extradata: "test2" } })
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				let data = res.json().form
+				if (data.custname[0] !== "test" ||
+					data.extradata[0] !== "test2" ||
+					data.comments[0] !== "" ||
+					data.custemail[0] !== "" ||
+					data.custtel[0] !== "" ||
+					data.delivery[0] !== ""
+				) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "POST", "https://httpbin.org/post", "", 200, "")
+			assertRequestMetricsEmitted(t, state.Samples, "POST", sr("HTTPBIN_URL/post"), "", 200, "")
 		})
 
 		t.Run("withRequestParams", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/forms/post");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.submitForm({ params: { headers: { "My-Fancy-Header": "SomeValue" } }})
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			let headers = res.json().headers
-			if (headers["My-Fancy-Header"] !== "SomeValue" ) { throw new Error("incorrect header: " + headers["My-Fancy-Header"]); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/forms/post");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.submitForm({ params: { headers: { "My-Fancy-Header": "SomeValue" } }})
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				let headers = res.json().headers
+				if (headers["My-Fancy-Header"][0] !== "SomeValue" ) { throw new Error("incorrect headers: " + JSON.stringify(headers)); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "POST", "https://httpbin.org/post", "", 200, "")
+			assertRequestMetricsEmitted(t, state.Samples, "POST", sr("HTTPBIN_URL/post"), "", 200, "")
 		})
 
 		t.Run("withFormSelector", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/forms/post");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.submitForm({ formSelector: 'form[method="post"]' })
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			let data = res.json().form
-			if (data.custname !== "" ||
-				data.extradata !== undefined ||
-				data.comments !== "" ||
-				data.custemail !== "" ||
-				data.custtel !== "" ||
-				data.delivery !== ""
-			) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/forms/post");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.submitForm({ formSelector: 'form[method="post"]' })
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				let data = res.json().form
+				if (data.custname[0] !== "" ||
+					data.extradata !== undefined ||
+					data.comments[0] !== "" ||
+					data.custemail[0] !== "" ||
+					data.custtel[0] !== "" ||
+					data.delivery[0] !== ""
+				) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "POST", "https://httpbin.org/post", "", 200, "")
+			assertRequestMetricsEmitted(t, state.Samples, "POST", sr("HTTPBIN_URL/post"), "", 200, "")
 		})
 
 		t.Run("withNonExistentForm", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/forms/post");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res.submitForm({ formSelector: "#doesNotExist" })
-		`)
-			assert.EqualError(t, err, "GoError: no form found for selector '#doesNotExist' in response 'https://httpbin.org/forms/post'")
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/forms/post");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res.submitForm({ formSelector: "#doesNotExist" })
+			`))
+			assert.EqualError(t, err, sr("GoError: no form found for selector '#doesNotExist' in response 'HTTPBIN_URL/forms/post'"))
 		})
 
 		t.Run("withGetMethod", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", httpTestServerURL + "/forms/get");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.submitForm()
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			let data = res.json().query
-			if (data.input_with_value[0] !== "value" ||
-				data.input_without_value[0] !== "" ||
-				data.select_one[0] !== "yes this option" ||
-				data.select_multi[0] !== "option 2,option 3" ||
-				data.textarea[0] !== "Lorem ipsum dolor sit amet"
-			) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", httpTestServerURL + "/forms/get");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.submitForm()
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				let data = res.json().query
+				if (data.input_with_value[0] !== "value" ||
+					data.input_without_value[0] !== "" ||
+					data.select_one[0] !== "yes this option" ||
+					data.select_multi[0] !== "option 2,option 3" ||
+					data.textarea[0] !== "Lorem ipsum dolor sit amet"
+				) { throw new Error("incorrect body: " + JSON.stringify(data, null, 4) ); }
+			`))
 			assert.NoError(t, err)
 			assertRequestMetricsEmitted(t, state.Samples, "GET", srv.URL+"/forms/get", "", 200, "")
 		})
@@ -305,50 +267,50 @@ func TestResponse(t *testing.T) {
 	t.Run("ClickLink", func(t *testing.T) {
 		t.Run("withoutArgs", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/links/10/0");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.clickLink()
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/links/10/0");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.clickLink()
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "GET", "https://httpbin.org/links/10/1", "", 200, "")
+			assertRequestMetricsEmitted(t, state.Samples, "GET", sr("HTTPBIN_URL/links/10/1"), "", 200, "")
 		})
 
 		t.Run("withSelector", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/links/10/0");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.clickLink({ selector: 'a:nth-child(4)' })
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/links/10/0");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.clickLink({ selector: 'a:nth-child(4)' })
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "GET", "https://httpbin.org/links/10/4", "", 200, "")
+			assertRequestMetricsEmitted(t, state.Samples, "GET", sr("HTTPBIN_URL/links/10/4"), "", 200, "")
 		})
 
 		t.Run("withNonExistentLink", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org/links/10/0");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.clickLink({ selector: 'a#doesNotExist' })
-		`)
-			assert.EqualError(t, err, "GoError: no element found for selector 'a#doesNotExist' in response 'https://httpbin.org/links/10/0'")
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL/links/10/0");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.clickLink({ selector: 'a#doesNotExist' })
+			`))
+			assert.EqualError(t, err, sr("GoError: no element found for selector 'a#doesNotExist' in response 'HTTPBIN_URL/links/10/0'"))
 		})
 
 		t.Run("withRequestParams", func(t *testing.T) {
 			state.Samples = nil
-			_, err := common.RunString(rt, `
-			let res = http.request("GET", "https://httpbin.org");
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			res = res.clickLink({ selector: 'a[href="/get"]', params: { headers: { "My-Fancy-Header": "SomeValue" } } })
-			if (res.status != 200) { throw new Error("wrong status: " + res.status); }
-			let headers = res.json().headers
-			if (headers["My-Fancy-Header"] !== "SomeValue" ) { throw new Error("incorrect header: " + headers["My-Fancy-Header"]); }
-		`)
+			_, err := common.RunString(rt, sr(`
+				let res = http.request("GET", "HTTPBIN_URL");
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				res = res.clickLink({ selector: 'a[href="/get"]', params: { headers: { "My-Fancy-Header": "SomeValue" } } })
+				if (res.status != 200) { throw new Error("wrong status: " + res.status); }
+				let headers = res.json().headers
+				if (headers["My-Fancy-Header"][0] !== "SomeValue" ) { throw new Error("incorrect headers: " + JSON.stringify(headers)); }
+			`))
 			assert.NoError(t, err)
-			assertRequestMetricsEmitted(t, state.Samples, "GET", "https://httpbin.org/get", "", 200, "")
+			assertRequestMetricsEmitted(t, state.Samples, "GET", sr("HTTPBIN_URL/get"), "", 200, "")
 		})
 	})
 }
