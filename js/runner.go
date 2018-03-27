ba7b725f8bc08f451d77c8e4ff3ad622373a03bd
Luiz Filho <luizbafilho@gmail.com>
2018-03-27 13:11:35 -0300

    Fixing NTLM Implementation
    We are now using a RoundTripper in order to reuse the same authenticated
    connection to comply with NTLM spec
    
    Signed-off-by: Luiz Filho <luizbafilho@gmail.com>

--- js/runner.go
+++ js/runner.go
@@ -165,7 +165,7 @@ func (r *Runner) newVU() (*VU, error) {
 	vu := &VU{
 		BundleInstance: *bi,
 		Runner:         r,
-		HTTPTransport:  transport,
+		HTTPTransport:  netext.NewHTTPTransport(transport),
 		Dialer:         dialer,
 		TLSConfig:      tlsConfig,
 		Console:        NewConsole(),
@@ -245,7 +245,7 @@ type VU struct {
 	BundleInstance
 
 	Runner        *Runner
-	HTTPTransport *http.Transport
+	HTTPTransport *netext.HTTPTransport
 	Dialer        *netext.Dialer
 	TLSConfig     *tls.Config
 	ID            int64
