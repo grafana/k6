7a6b55c3ed84f9af3b3e7394a0ca1d144d413f24
Nedyalko Andreev <n@andreev.sh>
2018-03-28 16:11:07 +0300

    Fix data race from shared rand source that was not thread safe

--- js/common/randsource.go
+++ js/common/randsource.go
@@ -23,18 +23,20 @@ package common
 import (
 	crand "crypto/rand"
 	"encoding/binary"
+	"fmt"
 	"math/rand"
 
 	"github.com/dop251/goja"
-	"github.com/pkg/errors"
 )
 
-var DefaultRandSource = NewRandSource()
-
+// NewRandSource is copied from goja's source code:
+// https://github.com/dop251/goja/blob/master/goja/main.go#L44
+// The returned RandSource is NOT safe for concurrent use:
+// https://golang.org/pkg/math/rand/#NewSource
 func NewRandSource() goja.RandSource {
 	var seed int64
 	if err := binary.Read(crand.Reader, binary.LittleEndian, &seed); err != nil {
-		panic(errors.New("Couldn't read bytes for random seed"))
+		panic(fmt.Errorf("Could not read random bytes: %v", err))
 	}
 	return rand.New(rand.NewSource(seed)).Float64
 }
