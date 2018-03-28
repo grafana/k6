f0034d978be14bcfe75ae7c4b55449926132044c
Luiz Filho <luizbafilho@gmail.com>
2018-03-28 13:07:16 -0300

    Returning a better error message when using the open function outside init code

--- js/runner.go
+++ js/runner.go
@@ -172,6 +172,11 @@ func (r *Runner) newVU() (*VU, error) {
 		BPool:          bpool.NewBufferPool(100),
 	}
 	vu.Runtime.Set("console", common.Bind(vu.Runtime, vu.Console, vu.Context))
+	common.BindToGlobal(vu.Runtime, map[string]interface{}{
+		"open": func() {
+			common.Throw(vu.Runtime, errors.New("open function it is only available at init code"))
+		},
+	})
 
 	// Give the VU an initial sense of identity.
 	if err := vu.Reconfigure(0); err != nil {
