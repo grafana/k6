8e4eb353fa86c51b2ea829a00b35ad4fd947d523
Mihail Stoykov <M.Stoikov@gmail.com>
2019-02-07 15:46:43 +0200

    refactor the test code into different functions

--- js/modules/k6/http/error_codes_test.go
+++ js/modules/k6/http/error_codes_test.go
@@ -11,8 +11,20 @@ import (
 	"golang.org/x/net/http2"
 )
 
-func TestErrorCodeForError(t *testing.T) {
+func TestDefaultError(t *testing.T) {
 	// TODO find better way to test
+	testErrorCode(t, defaultErrorCode, fmt.Errorf("random error"))
+}
+func TestHTTP2Errors(t *testing.T) {
+	var testTable = map[errCode]error{
+		http2ConnectionErrorCode: new(http2.ConnectionError),
+		http2StreamErrorCode:     new(http2.StreamError),
+		http2GoAwayErrorCode:     new(http2.GoAwayError),
+	}
+	testMapOfErrorCodes(t, testTable)
+}
+
+func TestTCPErrors(t *testing.T) {
 	var (
 		nonTCPError  = new(net.OpError)
 		econnreset   = new(net.OpError)
@@ -36,20 +48,26 @@ func TestErrorCodeForError(t *testing.T) {
 
 	tcperror.Net = "tcp"
 
-	var testTable = map[error]errCode{
-		fmt.Errorf("random error"): defaultErrorCode,
-		new(http2.ConnectionError): http2ConnectionErrorCode,
-		new(http2.StreamError):     http2StreamErrorCode,
-		new(http2.GoAwayError):     http2GoAwayErrorCode,
-		nonTCPError:                defaultNetNonTCPErrorCode,
-		econnreset:                 tcpResetByPeerErrorCode,
-		epipeerror:                 tcpBrokenPipeErrorCode,
-		econnrefused:               tcpDialRefusedErrorCode,
-		tcperror:                   defaultTCPErrorCode,
+	var testTable = map[errCode]error{
+		defaultNetNonTCPErrorCode: nonTCPError,
+		tcpResetByPeerErrorCode:   econnreset,
+		tcpBrokenPipeErrorCode:    epipeerror,
+		tcpDialRefusedErrorCode:   econnrefused,
+		defaultTCPErrorCode:       tcperror,
 	}
 
-	for err, code := range testTable {
-		require.Equalf(t, code, errorCodeForError(err), "Wrong error code for error `%s`", err)
-		require.Equalf(t, code, errorCodeForError(errors.WithStack(err)), "Wrong error code for error `%s`", err)
+	testMapOfErrorCodes(t, testTable)
+}
+
+func testErrorCode(t *testing.T, code errCode, err error) {
+	t.Helper()
+	require.Equalf(t, code, errorCodeForError(err), "Wrong error code for error `%s`", err)
+	require.Equalf(t, code, errorCodeForError(errors.WithStack(err)), "Wrong error code for error `%s`", err)
+}
+
+func testMapOfErrorCodes(t *testing.T, testTable map[errCode]error) {
+	t.Helper()
+	for code, err := range testTable {
+		testErrorCode(t, code, err)
 	}
 }
