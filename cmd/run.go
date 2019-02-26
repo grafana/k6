62c910c305090c2e3d6f94c3f7ea9f6166cd08d6
Nedyalko Andreev <n@andreev.sh>
2019-02-26 08:25:20 +0200

    Remove the debug output

--- cmd/run.go
+++ cmd/run.go
@@ -37,8 +37,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
-
 	"github.com/loadimpact/k6/api"
 	"github.com/loadimpact/k6/core"
 	"github.com/loadimpact/k6/core/local"
@@ -169,7 +167,6 @@ a commandline interface for interacting with it.`,
 		}
 
 		//TODO: move a bunch of the logic above to a config "constructor" and to the Validate() method
-		spew.Dump(conf.Execution)
 		if errList := conf.Validate(); len(errList) != 0 {
 
 			errMsg := []string{"There were problems with the specified script configuration:"}
