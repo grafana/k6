4731e7298b93475778fbddad6ab6cf93fb736f81
Mihail Stoykov <M.Stoikov@gmail.com>
2019-03-08 14:10:02 +0200

    a bunch of breaking changes

--- cmd/options.go
+++ cmd/options.go
@@ -55,7 +55,7 @@ func optionFlagSet() *pflag.FlagSet {
 	flags.Int64("batch-per-host", 20, "max parallel batch reqs per host")
 	flags.Int64("rps", 0, "limit requests per second")
 	flags.String("user-agent", fmt.Sprintf("k6/%s (https://k6.io/)", Version), "user agent for http requests")
-	flags.String("http-debug", "", "log all HTTP requests and responses. Excludes body by default. To include body use '--http-debug=full'")
+	flags.String("http-debug", "", "log all HTTP requests and responses.  Excludes body by default. To include body use '--http-debug=full'")
 	flags.Lookup("http-debug").NoOptDefVal = "headers"
 	flags.Bool("insecure-skip-tls-verify", false, "skip verification of TLS certificates")
 	flags.Bool("no-connection-reuse", false, "disable keep-alive connections")
@@ -63,9 +63,9 @@ func optionFlagSet() *pflag.FlagSet {
 	flags.Duration("min-iteration-duration", 0, "minimum amount of time k6 will take executing a single iteration")
 	flags.BoolP("throw", "w", false, "throw warnings (like failed http requests) as errors")
 	flags.StringSlice("blacklist-ip", nil, "blacklist an `ip range` from being called")
-	flags.StringSlice("summary-trend-stats", nil, "define `stats` for trend metrics (response times), one or more as 'avg,p(95),...'")
-	flags.String("summary-time-unit", "", "define the time unit used to display the trend stats. Possible units are: 's', 'ms' and 'us'")
-	flags.StringSlice("system-tags", lib.DefaultSystemTagList, "only include these system tags in metrics")
+	flags.StringSlice("summary-trend-stats", nil, "define `stats` for trend metrics(response times), one or more as 'avg,p(95),...'")
+	flags.String("summary-time-unit", "", "define the time unit  used to display the trend stats. Possible units are: 's', 'ms' and 'us'")
+	flags.StringSlice("system-tags", lib.DefaultSystemTagList, "only  include these system tags in metrics")
 	flags.StringSlice("tag", nil, "add a `tag` to be applied to all samples, as `[name]=[value]`")
 	flags.String("console-output", "", "redirects the console logging to the provided output file")
 	flags.Bool("discard-response-bodies", false, "Read but don't process or save HTTP response bodies")
@@ -92,9 +92,8 @@ func getOptions(flags *pflag.FlagSet) (lib.Options, error) {
 		DiscardResponseBodies: getNullBool(flags, "discard-response-bodies"),
 		// Default values for options without CLI flags:
 		// TODO: find a saner and more dev-friendly and error-proof way to handle options
-		SetupTimeout:    types.NullDuration{Duration: types.Duration(10 * time.Second), Valid: false},
-		TeardownTimeout: types.NullDuration{Duration: types.Duration(10 * time.Second), Valid: false},
-
+		SetupTimeout:            types.NullDuration{Duration: types.Duration(10 * time.Second), Valid: false},
+		TeardownTimeout:         types.NullDuration{Duration: types.Duration(10 * time.Second), Valid: false},
 		MetricSamplesBufferSize: null.NewInt(1000, false),
 	}
 
