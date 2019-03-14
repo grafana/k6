039599080889e8b2082a0c07506cc4a73826c547
Mihail Stoykov <M.Stoikov@gmail.com>
2019-03-14 14:12:05 +0200

    Fix for a test with a changed message in go1.12

--- js/modules/k6/http/request_test.go
+++ js/modules/k6/http/request_test.go
@@ -24,6 +24,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"go/build"
 	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
@@ -1347,6 +1348,13 @@ func TestErrorCodes(t *testing.T) {
 	state.Options.Throw = null.BoolFrom(false)
 	defer tb.Cleanup()
 	sr := tb.Replacer.Replace
+	var badLocationRedirectMsg = "first path segment in URL cannot contain colon"
+	for _, tag := range build.Default.ReleaseTags {
+		if tag == "go1.12" {
+			badLocationRedirectMsg = "net/url: invalid control character in URL"
+			break
+		}
+	}
 
 	// Handple paths with custom logic
 	tb.Mux.HandleFunc("/digest-auth/failure", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -1416,7 +1424,7 @@ func TestErrorCodes(t *testing.T) {
 			script:            `let res = http.request("GET", "HTTPBIN_URL/bad-location-redirect");`,
 			expectedScriptError: sr(
 				"GoError: Get HTTPBIN_URL/bad-location-redirect: failed to parse Location header" +
-					" \"h\\t:/\": parse h\t:/: first path segment in URL cannot contain colon"),
+					" \"h\\t:/\": parse h\t:/: " + badLocationRedirectMsg),
 		},
 		{
 			name:              "Missing protocol",
