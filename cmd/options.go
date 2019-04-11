c6fc6fb015b2f927d98084ef8358d1eecfc63a7a
Olivier Fauchon <ofauchon2204@gmail.com>
2019-04-11 22:36:04 +0200

    fix golangci error 'line is 136 characters'

--- cmd/options.go
+++ cmd/options.go
@@ -65,7 +65,7 @@ func optionFlagSet() *pflag.FlagSet {
 	flags.StringSlice("blacklist-ip", nil, "blacklist an `ip range` from being called")
 	flags.StringSlice("summary-trend-stats", nil, "define `stats` for trend metrics (response times), one or more as 'avg,p(95),...'")
 	flags.String("summary-time-unit", "", "define the time unit used to display the trend stats. Possible units are: 's', 'ms' and 'us'")
-	flags.StringP("nic", "n", "", "define comma-separated list of network interface(s). All IPs from the interfaces will be used randomly")
+	flags.StringP("nic", "n", "", "comma-separated list of network interface(s). All IPs from the selected interfaces will be used randomly") //nolint:lll
 	flags.StringSlice("system-tags", lib.DefaultSystemTagList, "only include these system tags in metrics")
 	flags.StringSlice("tag", nil, "add a `tag` to be applied to all samples, as `[name]=[value]`")
 	flags.String("console-output", "", "redirects the console logging to the provided output file")
