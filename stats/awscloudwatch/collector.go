53b8235987ca238ad2575150f7757aa2f2b04b5e
Jorge Velasco <zen.chen@gmail.com>
2019-05-29 20:11:32 +0200

    Fix error on collector comment

--- stats/awscloudwatch/collector.go
+++ stats/awscloudwatch/collector.go
@@ -10,7 +10,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Collect collects metrics and send them to CloudWatch
+// Collector collects metrics and send them to CloudWatch
 type Collector struct {
 	client          cloudWatchClient
 	bufferedSamples []*sample
