f8a88b6b645a88bfdbd4cd509aec8a9d46adfc7f
Nedyalko Andreev <n@andreev.sh>
2019-08-13 13:45:31 +0300

    Update the README and version number after the release

--- lib/consts/consts.go
+++ lib/consts/consts.go
@@ -6,7 +6,7 @@ import (
 
 // Version contains the current semantic version of k6.
 //nolint:gochecknoglobals
-var Version = "0.25.1"
+var Version = "0.26.0-dev"
 
 // Banner contains the ASCII-art banner with the k6 logo and stylized website URL
 //TODO: make these into methods, only the version needs to be a variable
