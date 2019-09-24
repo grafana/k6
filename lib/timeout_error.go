303f893d417633983eb0c0ff59536af5d5bb6c81
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2019-09-24 11:49:55 +0700

    lib: better error message for timeout error
    TimeoutError is currently very uninformative, even if there are
    duplicated error messages.
    
    This PR change TimeoutError to a struct, which contains both the place
    and the timeout duration, so it helps constructing error message with
    more useful information to user.
    
    While at it, also prevent duplicating timeout error message for setup
    and teardown.
    
    Close #702

--- lib/timeout_error.go
+++ lib/timeout_error.go
@@ -1,18 +1,58 @@
 package lib
 
+import (
+	"fmt"
+	"time"
+)
+
+//nolint:gochecknoglobals
+// Keep stages in sync with js/runner.go
+// We set it here to prevent import cycle.
+var (
+	stageSetup    = "setup"
+	stageTeardown = "teardown"
+)
+
 // TimeoutError is used when somethings timeouts
-type TimeoutError string
-
-// NewTimeoutError returns a new TimeoutError reporting that timeout has happened at the provieded
-// place
-func NewTimeoutError(place string) TimeoutError {
-	return TimeoutError(place)
+type TimeoutError struct {
+	place string
+	d     time.Duration
 }
 
+// NewTimeoutError returns a new TimeoutError reporting that timeout has happened
+// at the given place and given duration.
+func NewTimeoutError(place string, d time.Duration) TimeoutError {
+	return TimeoutError{place: place, d: d}
+}
+
+// String returns timeout error in human readable format.
 func (t TimeoutError) String() string {
-	return "Timeout during " + (string)(t)
+	s := t.place + " execution timed out"
+	if t.d.Seconds() > 0 {
+		s += fmt.Sprintf(" after %.f seconds", t.d.Seconds())
+	}
+	return s
 }
 
+// Error implements error interface.
 func (t TimeoutError) Error() string {
 	return t.String()
+}
+
+// Place returns the place where timeout occurred.
+func (t TimeoutError) Place() string {
+	return t.place
+}
+
+// Hint returns a hint message for logging with given stage.
+func (t TimeoutError) Hint() string {
+	hint := ""
+
+	switch t.place {
+	case stageSetup:
+		hint = "You can increase the time limit via the setupTimeout option"
+	case stageTeardown:
+		hint = "You can increase the time limit via the teardownTimeout option"
+	}
+	return hint
 }
