14cca20cfa2d855e875450898c078464e890f305
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2019-10-10 14:43:05 +0700

    js/modules: fix js map causes wrong return type
    Selection.map currently calls the underlying goquery.Selection Map. It
    causes problem when running inside goja runtime, all the nodes inside
    the function body is treated as goquery.Selection instead of our wrapper
    Selection, thus lead to wrong calling methods.
    
    To fix it, we implement our own Map, by emulating what
    goquery.Selection.Map does under the hood.
    
    Fixes #1195

--- js/modules/k6/html/html.go
+++ js/modules/k6/html/html.go
@@ -373,20 +373,28 @@ func (s Selection) Is(v goja.Value) bool {
 	}
 }
 
-func (s Selection) Map(v goja.Value) (result []string) {
+func (s Selection) Map(v goja.Value) []string {
 	gojaFn, isFn := goja.AssertFunction(v)
 	if !isFn {
 		common.Throw(s.rt, errors.New("Argument to map() must be a function"))
 	}
 
-	fn := func(idx int, sel *goquery.Selection) string {
+	fn := func(idx int, sel *Selection) string {
 		if fnRes, fnErr := gojaFn(v, s.rt.ToValue(idx), s.rt.ToValue(sel)); fnErr == nil {
 			return fnRes.String()
 		}
 		return ""
 	}
 
-	return s.sel.Map(fn)
+	// Mimics goquery.Selector.Map function.
+	// We can not use s.sel.Map directly, otherwise, goja will see the function body elements as type
+	// *gohtml.Selection instead of our *Selection wrapper, so goja runtime will call wrong methods.
+	// See issue #1195
+	result := make([]string, len(s.sel.Nodes))
+	for i, n := range s.sel.Nodes {
+		result[i] = fn(i, &Selection{rt: s.rt, sel: &goquery.Selection{Nodes: []*gohtml.Node{n}}})
+	}
+	return result
 }
 
 func (s Selection) Slice(start int, def ...int) Selection {
