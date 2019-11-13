25ed0cff840f515608037beb2ed0c0648607a926
Nedyalko Andreev <n@andreev.sh>
2019-11-13 13:58:58 +0200

    Fix shellcheck and style issues in the build-release script

--- build-release.sh
+++ build-release.sh
@@ -1,52 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
-eval $(go env)
+set -eEuo pipefail
+
+eval "$(go env)"
 
 # To override the latest git tag as the version, pass something else as the first arg.
 VERSION=${1:-$(git describe --tags --abbrev=0)}
 
 make_archive() {
-	FMT=$1
-	DIR=$2
+	local FMT="$1" DIR="$2"
 
 	case $FMT in
 	zip)
-		zip -rq9 $DIR.zip $DIR
+		zip -rq9 "$DIR.zip" "$DIR"
 		;;
 	tgz)
-		tar -zcf $DIR.tar.gz $DIR
+		tar -zcf "$DIR.tar.gz" "$DIR"
 		;;
 	esac
 }
 
 build_dist() {
-	ALIAS=$1
-	GOOS=$2
-	GOARCH=$3
-	FMT=$4
-	SUFFIX=$5
+	local ALIAS="$1" GOOS="$2" GOARCH="$3" FMT="${4}" SUFFIX="${5:-}"
 
 	echo "- Building platform: ${ALIAS} (${GOOS} ${GOARCH})"
-	DIR=k6-${VERSION}-${ALIAS}
-	BIN=k6${SUFFIX}
+	local DIR="k6-${VERSION}-${ALIAS}"
 
 	# Clean out any old remnants of failed builds.
-	rm -rf dist/$DIR
-	mkdir -p dist/$DIR
+	rm -rf "dist/$DIR"
+	mkdir -p "dist/$DIR"
 
 	# Build a binary, embed what we can by means of static assets inside it.
-	GOARCH=$GOARCH GOOS=$GOOS go build -o dist/$DIR/$BIN
+	GOARCH="$GOARCH" GOOS="$GOOS" go build -o "dist/$DIR/k6${SUFFIX}"
 
 	# Archive it all, native format depends on the platform. Subshell to not mess with $PWD.
-	( cd dist && make_archive $FMT $DIR )
+	( cd dist && make_archive "$FMT" "$DIR" )
 
 	# Delete the source files.
-	rm -rf dist/$DIR
+	rm -rf "dist/$DIR"
 }
 
 checksum() {
-	CHECKSUM_FILE=k6-${VERSION}-checksums.txt
+	local CHECKSUM_FILE="k6-${VERSION}-checksums.txt"
 
 	if command -v sha256sum > /dev/null; then
 		CHECKSUM_CMD="sha256sum"
@@ -57,8 +52,8 @@ checksum() {
 		return 1
 	fi
 
-	rm -f dist/$CHECKSUM_FILE
-	( cd dist && for x in $(ls -1); do $CHECKSUM_CMD $x >> $CHECKSUM_FILE; done )
+	rm -f "dist/$CHECKSUM_FILE"
+	( cd dist && for x in *; do "$CHECKSUM_CMD" "$x" >> "$CHECKSUM_FILE"; done )
 }
 
 echo "--- Building Release: ${VERSION}"
