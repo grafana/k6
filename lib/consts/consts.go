7b8bfe6c6b446518445bed79d147befbb076ae8b
Nedyalko Andreev <n@andreev.sh>
2019-11-13 12:49:50 +0200

    Emit more detailed k6 version information

--- lib/consts/consts.go
+++ lib/consts/consts.go
@@ -1,12 +1,32 @@
 package consts
 
 import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
 	"strings"
 )
 
 // Version contains the current semantic version of k6.
-//nolint:gochecknoglobals
-var Version = "0.26.0-dev"
+var Version = "0.26.0-dev" //nolint:gochecknoglobals
+
+// VersionDetails can be set externally as part of the build process
+var VersionDetails = "" // nolint:gochecknoglobals
+
+// FullVersion returns the maximully full version and build information for
+// the currently running k6 executable.
+func FullVersion() string {
+	goVersionArch := fmt.Sprintf("%s, %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	if VersionDetails != "" {
+		return fmt.Sprintf("%s (%s, %s)", Version, VersionDetails, goVersionArch)
+	}
+
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		return fmt.Sprintf("%s (%s, %s)", Version, buildInfo.Main.Version, goVersionArch)
+	}
+
+	return fmt.Sprintf("%s (dev build, %s)", Version, goVersionArch)
+}
 
 // Banner contains the ASCII-art banner with the k6 logo and stylized website URL
 //TODO: make these into methods, only the version needs to be a variable
