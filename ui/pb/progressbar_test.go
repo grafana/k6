9dba77e4a62825a4bcb07069b4562a4302bb3557
Ivan Mirić <ivan@loadimpact.com>
2019-12-20 12:12:20 +0100

    Add progressbar tests

--- ui/pb/progressbar_test.go
+++ ui/pb/progressbar_test.go
@@ -20,4 +20,109 @@
 
 package pb
 
-//TODO
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProgressBarRender(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		options  []ProgressBarOption
+		expected string
+	}{
+		{[]ProgressBarOption{WithLeft(func() string { return "left" })},
+			"left [--------------------------------------]"},
+		{[]ProgressBarOption{WithConstLeft("constLeft")},
+			"constLeft [--------------------------------------]"},
+		{[]ProgressBarOption{
+			WithLeft(func() string { return "left" }),
+			WithProgress(func() (float64, string) { return 0, "right" }),
+		},
+			"left [--------------------------------------] right"},
+		{[]ProgressBarOption{
+			WithLeft(func() string { return "left" }),
+			WithProgress(func() (float64, string) { return 0.5, "right" }),
+		},
+			"left [==================>-------------------] right"},
+		{[]ProgressBarOption{
+			WithLeft(func() string { return "left" }),
+			WithProgress(func() (float64, string) { return 1.0, "right" }),
+		},
+			"left [======================================] right"},
+		{[]ProgressBarOption{
+			WithLeft(func() string { return "left" }),
+			WithProgress(func() (float64, string) { return -1, "right" }),
+		},
+			"left [" + strings.Repeat("-", 76) + "] right"},
+		{[]ProgressBarOption{
+			WithLeft(func() string { return "left" }),
+			WithProgress(func() (float64, string) { return 2, "right" }),
+		},
+			"left [" + strings.Repeat("=", 76) + "] right"},
+		{[]ProgressBarOption{
+			WithLeft(func() string { return "left" }),
+			WithConstProgress(0.2, "constProgress"),
+		},
+			"left [======>-------------------------------] constProgress"},
+		{[]ProgressBarOption{
+			WithHijack(func() string { return "progressbar hijack!" }),
+		},
+			"progressbar hijack!"},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.expected, func(t *testing.T) {
+			pbar := New(tc.options...)
+			assert.NotNil(t, pbar)
+			assert.Equal(t, tc.expected, pbar.String(0))
+		})
+	}
+}
+
+func TestProgressBarRenderPaddingLeft(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		padding  int
+		expected string
+	}{
+		{-1, "left [--------------------------------------]"},
+		{0, "left [--------------------------------------]"},
+		{10, "left       [--------------------------------------]"},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.expected, func(t *testing.T) {
+			pbar := New(WithLeft(func() string { return "left" }))
+			assert.NotNil(t, pbar)
+			assert.Equal(t, tc.expected, pbar.String(tc.padding))
+		})
+	}
+}
+
+func TestProgressBarLeft(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		left     func() string
+		expected string
+	}{
+		{nil, ""},
+		{func() string { return " left " }, " left "},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.expected, func(t *testing.T) {
+			pbar := New(WithLeft(tc.left))
+			assert.NotNil(t, pbar)
+			assert.Equal(t, tc.expected, pbar.Left())
+		})
+	}
+}
