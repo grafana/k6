81d01cfcd67b02347e25654b0756d27d0ed3ae2a
Ivan Mirić <ivan@loadimpact.com>
2020-01-16 19:17:37 +0100

    Fix progress bar tests, add cmd.renderMultipleBars test

---
+++ cmd/ui_test.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/loadimpact/k6/ui/pb"
+)
+
+// Return progressbars with different content lengths, to test for
+// padding.
+func createTestProgressBars(num, padding, colIdx int) []*pb.ProgressBar {
+	pbs := make([]*pb.ProgressBar, num)
+	for i := 0; i < num; i++ {
+		left := fmt.Sprintf("left %d", i)
+		rightCol1 := fmt.Sprintf("right %d", i)
+		progress := 0.0
+		status := pb.Running
+		if i == colIdx {
+			pad := strings.Repeat("+", padding)
+			left += pad
+			rightCol1 += pad
+			progress = 1.0
+			status = pb.Done
+		}
+		pbs[i] = pb.New(
+			pb.WithLeft(func() string { return left }),
+			pb.WithStatus(status),
+			pb.WithProgress(func() (float64, []string) {
+				return progress, []string{rightCol1, "000"}
+			}),
+		)
+	}
+	return pbs
+}
+
+func TestRenderMultipleBars(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		padding  int
+		expected string
+	}{
+		{"nopad", 0, `
+left 0   [--------------------------------------] right 0  000
+left 1 ✓ [======================================] right 1  000
+left 2   [--------------------------------------] right 2  000
+
+`},
+		{"pad2", 2, `
+left 0     [--------------------------------------] right 0    000
+left 1++ ✓ [======================================] right 1++  000
+left 2     [--------------------------------------] right 2    000
+
+`},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			pbs := createTestProgressBars(3, tc.padding, 1)
+			out := renderMultipleBars(false, false, 6+tc.padding, pbs)
+			assert.Equal(t, tc.expected, out)
+		})
+	}
+}
