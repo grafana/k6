ba5dab43a80ef92909acb9d996cc98458988c0fa
Adrian Lewis <adrianlewis@localhost.localdomain>
2020-02-24 11:26:19 +0000

    changed http/2 URL and ran

--- samples/http_2.js
+++ samples/http_2.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { check } from "k6";
 
 export default function() {
-  check(http.get("https://www.bbc.co.uk/"), {
+  check(http.get("https://test-api.loadimpact.com/public/crocodiles/"), {
     "status is 200": (r) => r.status == 200,
     "protocol is HTTP/2": (r) => r.proto == "HTTP/2.0",
   });
