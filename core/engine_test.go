ac4ab4e366d63ca354c90c720d9f1b949a7772bb
Mihail Stoykov <M.Stoikov@gmail.com>
2020-06-03 17:18:37 +0300

    Increase setup/teardown timeouts in some failing tests

--- core/engine_test.go
+++ core/engine_test.go
@@ -879,7 +879,7 @@ func TestMinIterationDurationInSetupTeardownStage(t *testing.T) {
 					gracefulStop: "0s",
 				},
 			},
-			setupTimeout: "2s",
+			setupTimeout: "3s",
 		};
 
 		export default function () {
@@ -897,7 +897,7 @@ func TestMinIterationDurationInSetupTeardownStage(t *testing.T) {
 					gracefulStop: "0s",
 				},
 			},
-			teardownTimeout: "2s",
+			teardownTimeout: "3s",
 		};
 
 		export default function () {
