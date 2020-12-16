5adb5efa6027b72bfa79200efa76b9f01f9bf29e
Mihail Stoykov <M.Stoikov@gmail.com>
2020-12-16 10:34:17 +0200

    Specify node version and force the install

--- js/lib/corejs-build.sh
+++ js/lib/corejs-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-npm i .
+npm i --force .
 modules="$(cat shim.js | grep ^require  | sed "s|require('\./modules/||g"  | sed "s/');//g" | tr "\n" ",")"
 npm run grunt "build:${modules}" "--library=off" "--path=custom" "uglify"
