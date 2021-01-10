add9937c52b833d3af3ab2bf01cd0ef9a5cca166
Nedyalko Andreev <n@andreev.sh>
2021-01-10 20:40:57 +0200

    Fix sample scripts

--- samples/grpc.js
+++ samples/grpc.js
@@ -2,7 +2,7 @@ import grpc from 'k6/net/grpc';
 import { check } from "k6";
 
 let client = new grpc.Client();
-client.load([], "samples/grpc_server/route_guide.proto")
+client.load([], "./grpc_server/route_guide.proto")
 
 
 export default () => {
