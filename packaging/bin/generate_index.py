90da63464625ae384851c20dee41f7237e911b8b
Ivan Mirić <ivan@loadimpact.com>
2021-04-09 12:43:11 +0200

    Link k6 logo to home
    Resolves https://github.com/k6io/k6/pull/1916#discussion_r610494374

--- packaging/bin/generate_index.py
+++ packaging/bin/generate_index.py
@@ -310,7 +310,9 @@ def process_dir(top_dir, opts):
     </defs>
     </svg>
 <header>
-    <svg class="logo" viewBox="0 0 220.227 213.73"><use xlink:href="#logo"></use></svg>
+    <a href="/" title="Home">
+      <svg class="logo" viewBox="0 0 220.227 213.73"><use xlink:href="#logo"></use></svg>
+    </a>
     <h1>"""
         f'{path_top_dir.name}'
     """</h1>
