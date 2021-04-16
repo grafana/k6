c6ccbb957dff8f0841cc8285a4a0081725dc34a2
Mihail Stoykov <M.Stoikov@gmail.com>
2021-04-16 15:39:02 +0300

    Add test based on https://github.com/k6io/k6/issues/994#issue-430796678

--- core/engine_test.go
+++ core/engine_test.go
@@ -29,6 +29,7 @@ import (
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/guregu/null.v3"
@@ -700,6 +701,59 @@ func TestSetupTeardownThresholds(t *testing.T) {
 	case err := <-errC:
 		require.NoError(t, err)
 		require.False(t, engine.IsTainted())
+	}
+}
+
+func TestSetupException(t *testing.T) {
+	t.Parallel()
+
+	script := []byte(`
+	import bar from "./bar.js";
+	export function setup() {
+		bar();
+	};
+	export default function() {
+	};
+	`)
+
+	memfs := afero.NewMemMapFs()
+	require.NoError(t, afero.WriteFile(memfs, "/bar.js", []byte(`
+	export default function () {
+        baz();
+	}
+	function baz() {
+		        throw new Error("baz");
+			}
+	`), 0x666))
+	runner, err := js.New(
+		testutils.NewLogger(t),
+		&loader.SourceData{URL: &url.URL{Scheme: "file", Path: "/script.js"}, Data: script},
+		map[string]afero.Fs{"file": memfs},
+		lib.RuntimeOptions{},
+	)
+	require.NoError(t, err)
+
+	_, run, wait := newTestEngine(t, nil, runner, nil, lib.Options{
+		SystemTags:      &stats.DefaultSystemTagSet,
+		SetupTimeout:    types.NullDurationFrom(3 * time.Second),
+		TeardownTimeout: types.NullDurationFrom(3 * time.Second),
+		VUs:             null.IntFrom(3),
+	})
+	defer wait()
+
+	errC := make(chan error)
+	go func() { errC <- run() }()
+
+	select {
+	case <-time.After(10 * time.Second):
+		t.Fatal("Test timed out")
+	case err := <-errC:
+		require.Error(t, err)
+		var exception types.ScriptException
+		require.ErrorAs(t, err, &exception)
+		require.Equal(t, "Error: baz\n\tat baz (file:///bar.js:7:8(4))\n"+
+			"\tat file:///bar.js:4:5(3)\n\tat setup (file:///script.js:7:204(4))\n",
+			err.Error())
 	}
 }
 
