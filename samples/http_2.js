556630d5ec677f1c1027fab532568da974347dd9
Mihail Stoykov <M.Stoikov@gmail.com>
2021-07-02 19:08:20 +0300

    fix tc39 tests

--- samples/http_2.js
+++ samples/http_2.js
@@ -2,8 +2,10 @@ import http from "k6/http";
 import { check } from "k6";
 
 export default function () {
+  throw somethign
   check(http.get("https://test-api.k6.io/"), {
     "status is 200": (r) => r.status == 200,
     "protocol is HTTP/2": (r) => r.proto == "HTTP/2.0",
   });
 }
+
