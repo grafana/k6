8513d5825802b942bdec77dc065a10575cee6e0f
Ivan <2103732+codebien@users.noreply.github.com>
2021-09-17 16:29:29 +0200

    fixup! output/influxdb: updated the client to v2

--- output/influxdb/output_test.go
+++ output/influxdb/output_test.go
@@ -23,15 +23,19 @@ package influxdb
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
+	influxdomain "github.com/influxdata/influxdb-client-go/v2/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"go.k6.io/k6/lib/testutils"
 	"go.k6.io/k6/output"
 	"go.k6.io/k6/stats"
@@ -100,36 +104,28 @@ func TestNew(t *testing.T) {
 	})
 }
 
-func testOutputCycle(t testing.TB, handler http.HandlerFunc, body func(testing.TB, *Output)) {
-	s := &http.Server{
-		Addr:           ":",
-		Handler:        handler,
-		MaxHeaderBytes: 1 << 20,
-	}
-	l, err := net.Listen("tcp", "127.0.0.1:0")
-	require.NoError(t, err)
-	defer func() {
-		_ = l.Close()
-	}()
-
-	defer func() {
-		require.NoError(t, s.Shutdown(context.Background()))
-	}()
-
-	go func() {
-		require.Equal(t, http.ErrServerClosed, s.Serve(l))
-	}()
-	c, err := newOutput(output.Params{
+func TestExtractTagsToValues(t *testing.T) {
+	t.Parallel()
+	o, err := newOutput(output.Params{
 		Logger:         testutils.NewLogger(t),
-		ConfigArgument: "http://" + l.Addr().String(),
 		JSONConfig:     []byte(`{"bucket":"mybucket"}`),
+		ConfigArgument: "?tagsAsFields=stringField&tagsAsFields=stringField2:string&tagsAsFields=boolField:bool&tagsAsFields=floatField:float&tagsAsFields=intField:int",
 	})
 	require.NoError(t, err)
+	tags := map[string]string{
+		"stringField":  "string",
+		"stringField2": "string2",
+		"boolField":    "true",
+		"floatField":   "3.14",
+		"intField":     "12345",
+	}
+	values := o.extractTagsToValues(tags, map[string]interface{}{})
 
-	require.NoError(t, c.Start())
-	body(t, c)
-
-	require.NoError(t, c.Stop())
+	require.Equal(t, "string", values["stringField"])
+	require.Equal(t, "string2", values["stringField2"])
+	require.Equal(t, true, values["boolField"])
+	require.Equal(t, 3.14, values["floatField"])
+	require.Equal(t, int64(12345), values["intField"])
 }
 
 func TestOutput(t *testing.T) {
@@ -183,26 +179,253 @@ func TestOutput(t *testing.T) {
 	})
 }
 
-func TestExtractTagsToValues(t *testing.T) {
+func TestOutputStart(t *testing.T) {
 	t.Parallel()
-	o, err := newOutput(output.Params{
+
+	t.Run("SuccessWithBucketCreation", func(t *testing.T) {
+		dataset := influxMemory{Orgs: []string{"org1"}}
+		ts := startInfluxv2Mock(&dataset)
+		defer ts.Close()
+
+		c, err := newOutput(output.Params{
+			Logger:         testutils.NewLogger(t),
+			ConfigArgument: ts.URL,
+			JSONConfig:     []byte(`{"bucket":"mybucket","organization":"org1"}`),
+		})
+		require.NoError(t, err)
+		require.NoError(t, c.Start())
+		assert.Contains(t, dataset.Buckets, "mybucket")
+	})
+	t.Run("SuccessWithAlreadyExistingBucket", func(t *testing.T) {
+		dataset := influxMemory{
+			Orgs:    []string{"org1"},
+			Buckets: []string{"bucket1"},
+		}
+		ts := startInfluxv2Mock(&dataset)
+		defer ts.Close()
+
+		c, err := newOutput(output.Params{
+			Logger:         testutils.NewLogger(t),
+			ConfigArgument: ts.URL,
+			JSONConfig:     []byte(`{"bucket":"mybucket","organization":"org1"}`),
+		})
+		require.NoError(t, err)
+		require.NoError(t, c.Start())
+	})
+	t.Run("SuccessWithDatabase18Creation", func(t *testing.T) {
+		dataset := influxMemory{}
+		ts := startInfluxv1Mock(&dataset, "", "")
+		defer ts.Close()
+
+		c, err := newOutput(output.Params{
+			Logger:         testutils.NewLogger(t),
+			ConfigArgument: ts.URL,
+			JSONConfig:     []byte(`{"db":"mydb"}`),
+		})
+		require.NoError(t, err)
+		require.NoError(t, c.Start())
+		assert.Contains(t, dataset.Databases, "mydb")
+	})
+	t.Run("SuccessWithAlreadyExistingDatabase18", func(t *testing.T) {
+		dataset := influxMemory{Databases: []string{"db1"}}
+		ts := startInfluxv1Mock(&dataset, "", "")
+		defer ts.Close()
+
+		c, err := newOutput(output.Params{
+			Logger:         testutils.NewLogger(t),
+			ConfigArgument: ts.URL,
+			JSONConfig:     []byte(`{"db":"db1"}`),
+		})
+		require.NoError(t, err)
+		require.NoError(t, c.Start())
+	})
+	t.Run("SuccessWithDatabaseAuth", func(t *testing.T) {
+		dataset := influxMemory{Databases: []string{"db1"}}
+		ts := startInfluxv1Mock(&dataset, "joe", "passw")
+		defer ts.Close()
+
+		c, err := newOutput(output.Params{
+			Logger:         testutils.NewLogger(t),
+			ConfigArgument: ts.URL,
+			JSONConfig:     []byte(`{"db":"db1","username":"joe","password":"passw"}`),
+		})
+		require.NoError(t, err)
+		require.NoError(t, c.Start())
+	})
+	t.Run("DatabaseAuthFailed", func(t *testing.T) {
+		dataset := influxMemory{Databases: []string{"db1"}}
+		ts := startInfluxv1Mock(&dataset, "joe", "passw")
+		defer ts.Close()
+
+		c, err := newOutput(output.Params{
+			Logger:         testutils.NewLogger(t),
+			ConfigArgument: ts.URL,
+			JSONConfig:     []byte(`{"db":"db1","username":"joe","password":"p"}`),
+		})
+		require.NoError(t, err)
+		assert.NotNil(t, c.Start())
+	})
+	t.Run("OrganizationDoesNotExist", func(t *testing.T) {
+		dataset := influxMemory{
+			Orgs:    []string{"org1"},
+			Buckets: []string{"bucket1"},
+		}
+		ts := startInfluxv2Mock(&dataset)
+		defer ts.Close()
+
+		c, err := newOutput(output.Params{
+			Logger:         testutils.NewLogger(t),
+			ConfigArgument: ts.URL,
+			JSONConfig:     []byte(`{"bucket":"mybucket","organization":"org0"}`),
+		})
+		require.NoError(t, err)
+		err = c.Start()
+		require.NotNil(t, err)
+		assert.Contains(t, err.Error(), "organization 'org0' not found")
+	})
+}
+
+type influxMemory struct {
+	Orgs    []string
+	Buckets []string
+
+	// InfluxDB 1.x
+	Databases []string
+}
+
+func testOutputCycle(t testing.TB, handler http.HandlerFunc, body func(testing.TB, *Output)) {
+	s := &http.Server{
+		Addr:           ":",
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 20,
+	}
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+	defer func() {
+		_ = l.Close()
+	}()
+
+	defer func() {
+		require.NoError(t, s.Shutdown(context.Background()))
+	}()
+
+	go func() {
+		require.Equal(t, http.ErrServerClosed, s.Serve(l))
+	}()
+	c, err := newOutput(output.Params{
 		Logger:         testutils.NewLogger(t),
+		ConfigArgument: "http://" + l.Addr().String(),
 		JSONConfig:     []byte(`{"bucket":"mybucket"}`),
-		ConfigArgument: "?tagsAsFields=stringField&tagsAsFields=stringField2:string&tagsAsFields=boolField:bool&tagsAsFields=floatField:float&tagsAsFields=intField:int",
 	})
 	require.NoError(t, err)
-	tags := map[string]string{
-		"stringField":  "string",
-		"stringField2": "string2",
-		"boolField":    "true",
-		"floatField":   "3.14",
-		"intField":     "12345",
-	}
-	values := o.extractTagsToValues(tags, map[string]interface{}{})
 
-	require.Equal(t, "string", values["stringField"])
-	require.Equal(t, "string2", values["stringField2"])
-	require.Equal(t, true, values["boolField"])
-	require.Equal(t, 3.14, values["floatField"])
-	require.Equal(t, int64(12345), values["intField"])
+	require.NoError(t, c.Start())
+	body(t, c)
+
+	require.NoError(t, c.Stop())
+}
+
+func startInfluxv1Mock(db *influxMemory, expuser, exppassword string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		// on startup the version returned from the server is checked
+		if r.URL.Path == "/health" {
+			rw.Header().Set("Content-Type", "application/json")
+			_, _ = rw.Write([]byte(`{"status":"pass","version":"1.8"}`))
+			return
+		}
+
+		if expuser != "" {
+			u, p, ok := r.BasicAuth()
+			if !ok || u != expuser || p != exppassword {
+				rw.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+		}
+
+		// create a database for version 1.x
+		if r.URL.Path == "/query" {
+			b, err := io.ReadAll(r.Body)
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			q := string(b)
+			prefix := "q=CREATE+DATABASE+"
+			if !strings.HasPrefix(q, prefix) {
+				rw.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			dbname := strings.TrimPrefix(q, prefix)
+			db.Databases = append(db.Databases, dbname)
+			return
+		}
+
+		rw.WriteHeader(http.StatusInternalServerError)
+	}))
+}
+
+func startInfluxv2Mock(db *influxMemory) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		// on startup the version returned from the server is checked
+		if r.URL.Path == "/health" {
+			rw.Header().Set("Content-Type", "application/json")
+			_, _ = rw.Write([]byte(`{"status":"pass","version":"2.0"}`))
+			return
+		}
+		if r.URL.Path == "/api/v2/buckets" {
+			// list buckets
+			if r.Method == http.MethodGet {
+				filter := r.URL.Query().Get("name")
+
+				buckets := func() string {
+					items := make([]string, 0, len(db.Buckets))
+					for _, bucket := range db.Buckets {
+						// apply the filter if required
+						if filter != "" && filter != bucket {
+							continue
+						}
+						items = append(items, fmt.Sprintf(`{"name":%q}`, bucket))
+					}
+					return strings.Join(items, ",")
+				}()
+				rw.Header().Set("Content-Type", "application/json")
+				_, _ = rw.Write([]byte(fmt.Sprintf(`{"buckets":[%s]}`, buckets)))
+				return
+			}
+
+			// create a bucket
+			if r.Method == http.MethodPost {
+				var bucket influxdomain.Bucket
+				err := json.NewDecoder(r.Body).Decode(&bucket)
+				if err != nil {
+					rw.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				db.Buckets = append(db.Buckets, bucket.Name)
+				return
+			}
+		}
+
+		// list orgs
+		if r.Method == http.MethodGet && r.URL.Path == "/api/v2/orgs" {
+			filter := r.URL.Query().Get("org")
+
+			orgs := func() string {
+				items := make([]string, 0, len(db.Orgs))
+				for i, org := range db.Orgs {
+					if filter != "" && filter != org {
+						continue
+					}
+					items = append(items, fmt.Sprintf(`{"id":"%d","name":%q}`, i, org))
+				}
+				return strings.Join(items, ",")
+			}()
+			rw.Header().Set("Content-Type", "application/json")
+			_, _ = rw.Write([]byte(fmt.Sprintf(`{"orgs":[%s]}`, orgs)))
+			return
+		}
+
+		rw.WriteHeader(http.StatusInternalServerError)
+	}))
 }
