ac736b0f9e7c19d1ad089c42fca2d434e73c64ba
Joseph Woodward <joseph.woodward@xeuse.com>
2021-10-06 00:37:17 +0100

    Remove nolint comment in favour of formatting long function accordingly

--- output/csv/config.go
+++ output/csv/config.go
@@ -99,8 +99,9 @@ func ParseArg(arg string, logger *logrus.Logger) (Config, error) {
 
 // GetConsolidatedConfig combines {default config values + JSON config +
 // environment vars + arg config values}, and returns the final result.
-//nolint: lll
-func GetConsolidatedConfig(jsonRawConf json.RawMessage, env map[string]string, arg string, logger *logrus.Logger) (Config, error) {
+func GetConsolidatedConfig(
+	jsonRawConf json.RawMessage, env map[string]string, arg string, logger *logrus.Logger,
+) (Config, error) {
 	result := NewConfig()
 	if jsonRawConf != nil {
 		jsonConf := Config{}
