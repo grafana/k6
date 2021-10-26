bfaa13d2dc2754d95a04fff6fc5da44502a6132e
Ivan Mirić <ivan.miric@grafana.com>
2021-10-26 16:29:35 +0200

    ci: move Chrome deps installation to a standalone script
    Resolves https://github.com/grafana/xk6-browser/pull/34#discussion_r736537779

---
+++ .github/bin/install_chrome_deps_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 \
+  libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 \
+  libgconf-2-4 libasound2 libatk1.0-0 libgtk-3-0
+sudo apt-get install -y libgbm-dev libxshmfence1
