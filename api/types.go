dc786262ab93c41a6aad4d9ae36e78fc9f3bffa8
Ivan Mirić <ivan.miric@grafana.com>
2021-11-05 17:26:38 +0100

    Add HTTPMessageSize.Total method and document HTTP types

--- api/types.go
+++ api/types.go
@@ -20,14 +20,21 @@
 
 package api
 
+// HTTPHeader is a single HTTP header.
 type HTTPHeader struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
 
+// HTTPMessageSize are the sizes in bytes of the HTTP message header and body.
 type HTTPMessageSize struct {
 	Headers int64 `json:"headers"`
 	Body    int64 `json:"body"`
+}
+
+// Total returns the total size in bytes of the HTTP message.
+func (s HTTPMessageSize) Total() int64 {
+	return s.Headers + s.Body
 }
 
 type Rect struct {
