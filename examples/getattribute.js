7eaf7c2a23e041ad626fdc3e6a06388dd60aa366
İnanç Gümüş <inanc.gumus@grafana.com>
2021-11-05 18:32:14 +0300

    Use spaces in getattribute.js example

--- examples/getattribute.js
+++ examples/getattribute.js
@@ -2,20 +2,20 @@ import launcher from "k6/x/browser";
 import { check } from 'k6';
 
 export default function () {
-        const browser = launcher.launch('chromium', {
-                headless: __ENV.XK6_HEADLESS ? true : false,
-        });
-        const context = browser.newContext();
-        const page = context.newPage();
+  const browser = launcher.launch('chromium', {
+    headless: __ENV.XK6_HEADLESS ? true : false,
+  });
+  const context = browser.newContext();
+  const page = context.newPage();
 
-        page.goto('https://googlechromelabs.github.io/dark-mode-toggle/demo/', {
-                waitUntil: 'load',
-        });
-        let el = page.$('#dark-mode-toggle-3')
-        check(el, {
-                "GetAttribute('mode')": e => e.getAttribute('mode') == 'light',
-        });
+  page.goto('https://googlechromelabs.github.io/dark-mode-toggle/demo/', {
+    waitUntil: 'load',
+  });
+  let el = page.$('#dark-mode-toggle-3')
+  check(el, {
+    "GetAttribute('mode')": e => e.getAttribute('mode') == 'light',
+  });
 
-        page.close();
-        browser.close();
+  page.close();
+  browser.close();
 }
