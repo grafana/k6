f7dea610217c87d8d3ff1f0c5167388fc637259c
Ivan <2103732+codebien@users.noreply.github.com>
2021-12-01 18:24:36 +0100

    samples/grpc: multiple services in the same proto
    It asserts that the grpc reflection feature works also when
    multiple services are defined in the same proto file.

--- samples/grpc_server/go.mod
+++ samples/grpc_server/go.mod
@@ -1,9 +1,16 @@
 module go.k6.io/k6/samples/grpc_server
 
-go 1.15
+go 1.17
 
 require (
-	github.com/golang/protobuf v1.4.2
-	google.golang.org/grpc v1.31.1
-	google.golang.org/protobuf v1.25.0
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/grpc v1.42.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- samples/grpc_server/main.go
+++ samples/grpc_server/main.go
@@ -57,16 +57,17 @@ var (
 	port       = flag.Int("port", 10000, "The server port")
 )
 
-type routeGuideServer struct {
-	UnimplementedRouteGuideServer
+type featureExplorerServer struct {
+	UnimplementedFeatureExplorerServer
 	savedFeatures []*Feature // read-only after initialized
+}
 
-	mu         sync.Mutex // protects routeNotes
-	routeNotes map[string][]*RouteNote
+func newFeatureExplorerServer(features ...*Feature) *featureExplorerServer {
+	return &featureExplorerServer{savedFeatures: features}
 }
 
 // GetFeature returns the feature at the given point.
-func (s *routeGuideServer) GetFeature(ctx context.Context, point *Point) (*Feature, error) {
+func (s *featureExplorerServer) GetFeature(ctx context.Context, point *Point) (*Feature, error) {
 	n := rand.Intn(1000)
 	time.Sleep(time.Duration(n) * time.Millisecond)
 
@@ -80,7 +81,7 @@ func (s *routeGuideServer) GetFeature(ctx context.Context, point *Point) (*Featu
 }
 
 // ListFeatures lists all features contained within the given bounding Rectangle.
-func (s *routeGuideServer) ListFeatures(rect *Rectangle, stream RouteGuide_ListFeaturesServer) error {
+func (s *featureExplorerServer) ListFeatures(rect *Rectangle, stream FeatureExplorer_ListFeaturesServer) error {
 	for _, feature := range s.savedFeatures {
 		if inRange(feature.Location, rect) {
 			time.Sleep(500 * time.Millisecond)
@@ -90,6 +91,22 @@ func (s *routeGuideServer) ListFeatures(rect *Rectangle, stream RouteGuide_ListF
 		}
 	}
 	return nil
+}
+
+type routeGuideServer struct {
+	UnimplementedRouteGuideServer
+	savedFeatures []*Feature // read-only after initialized
+
+	mu         sync.Mutex // protects routeNotes
+	routeNotes map[string][]*RouteNote
+}
+
+func newRouteGuideServer(features ...*Feature) *routeGuideServer {
+	s := &routeGuideServer{
+		savedFeatures: features,
+		routeNotes:    make(map[string][]*RouteNote),
+	}
+	return s
 }
 
 // RecordRoute records a route composited of a sequence of points.
@@ -159,7 +176,7 @@ func (s *routeGuideServer) RouteChat(stream RouteGuide_RouteChatServer) error {
 }
 
 // loadFeatures loads features from a JSON file.
-func (s *routeGuideServer) loadFeatures(filePath string) {
+func loadFeatures(filePath string) []*Feature {
 	var data []byte
 	if filePath != "" {
 		var err error
@@ -170,9 +187,11 @@ func (s *routeGuideServer) loadFeatures(filePath string) {
 	} else {
 		data = exampleData
 	}
-	if err := json.Unmarshal(data, &s.savedFeatures); err != nil {
+	var features []*Feature
+	if err := json.Unmarshal(data, &features); err != nil {
 		log.Fatalf("Failed to load default features: %v", err)
 	}
+	return features
 }
 
 func toRadians(num float64) float64 {
@@ -219,12 +238,6 @@ func serialize(point *Point) string {
 	return fmt.Sprintf("%d %d", point.Latitude, point.Longitude)
 }
 
-func newServer() *routeGuideServer {
-	s := &routeGuideServer{routeNotes: make(map[string][]*RouteNote)}
-	s.loadFeatures(*jsonDBFile)
-	return s
-}
-
 func main() {
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
@@ -245,8 +258,10 @@ func main() {
 		}
 		opts = []grpc.ServerOption{grpc.Creds(creds)}
 	}
+	features := loadFeatures(*jsonDBFile)
 	grpcServer := grpc.NewServer(opts...)
-	RegisterRouteGuideServer(grpcServer, newServer())
+	RegisterRouteGuideServer(grpcServer, newRouteGuideServer(features...))
+	RegisterFeatureExplorerServer(grpcServer, newFeatureExplorerServer(features...))
 	reflection.Register(grpcServer)
 	grpcServer.Serve(lis)
 }
