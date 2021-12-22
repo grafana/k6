dac5c9a472eeda4190dcc3f3b3af297f37db8168
Ivan Mirić <ivan.miric@grafana.com>
2021-12-22 15:31:01 +0000

    Add metrics emission test (#167)
    * Add metrics emission test
    
    This adds a missing test for #40, and all other metrics emitted for
    page.goto().
    
    It uses a slightly modified version of the assertRequestMetricsEmittedSingle
    helper from k6[1].
    
    [1]: https://github.com/grafana/k6/blob/804203bcf574da57cd05d67b39f03d05c54523ab/js/modules/k6/http/request_test.go#L109
    
    * Replace sleep in test with Page.WaitForLoadState()
    
    Resolves https://github.com/grafana/xk6-browser/pull/167#discussion_r773912188
    
    * Make NetworkManager tests parallel

--- tests/network_manager_test.go
+++ tests/network_manager_test.go
@@ -23,10 +23,15 @@ package tests
 import (
 	"testing"
 
+	"github.com/grafana/xk6-browser/common"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	k6metrics "go.k6.io/k6/lib/metrics"
+	k6stats "go.k6.io/k6/stats"
 )
 
 func TestDataURLSkipRequest(t *testing.T) {
+	t.Parallel()
 	tb := newTestBrowser(t)
 	p := tb.NewPage(nil)
 
@@ -35,4 +40,64 @@ func TestDataURLSkipRequest(t *testing.T) {
 	p.Goto("data:text/html,hello", nil)
 
 	assert.True(t, lc.contains("skipped request handling of data URL"))
+}
+
+func TestMetricsEmission(t *testing.T) {
+	t.Parallel()
+	tb := newTestBrowser(t, withHTTPServer())
+
+	url := tb.URL("/get")
+	browserTags := map[string]string{
+		"group": "",
+		"url":   "about:blank",
+	}
+	httpTags := map[string]string{
+		"method":              "GET",
+		"url":                 url,
+		"status":              "200",
+		"group":               "",
+		"proto":               "http/1.1",
+		"from_cache":          "false",
+		"from_prefetch_cache": "false",
+		"from_service_worker": "false",
+	}
+	expMetricTags := map[string]map[string]string{
+		common.BrowserDOMContentLoaded.Name: browserTags,
+		common.BrowserLoaded.Name:           browserTags,
+		k6metrics.DataSentName: map[string]string{
+			"group":  "",
+			"method": "GET",
+			"url":    url,
+		},
+		k6metrics.HTTPReqsName:              httpTags,
+		k6metrics.HTTPReqDurationName:       httpTags,
+		k6metrics.DataReceivedName:          httpTags,
+		k6metrics.HTTPReqConnectingName:     httpTags,
+		k6metrics.HTTPReqTLSHandshakingName: httpTags,
+		k6metrics.HTTPReqSendingName:        httpTags,
+		k6metrics.HTTPReqReceivingName:      httpTags,
+	}
+
+	p := tb.NewPage(nil)
+	resp := p.Goto(url, nil)
+	require.NotNil(t, resp)
+
+	// Wait for all metrics to be emitted
+	p.WaitForLoadState("networkidle", nil)
+
+	bufSamples := k6stats.GetBufferedSamples(tb.samples)
+
+	var reqsCount int
+	cb := func(sample k6stats.Sample) {
+		switch sample.Metric.Name {
+		case k6metrics.HTTPReqsName:
+			reqsCount += int(sample.Value)
+		case k6metrics.DataSentName, k6metrics.DataReceivedName:
+			assert.Greaterf(t, int(sample.Value), 0,
+				"metric %s", sample.Metric.Name)
+		}
+	}
+
+	assertMetricsEmitted(t, bufSamples, expMetricTags, cb)
+	assert.Equal(t, 1, reqsCount)
 }
