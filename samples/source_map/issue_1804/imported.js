34f899cece17bf526329ecd94f119479dddec0cb
Mihail Stoykov <M.Stoikov@gmail.com>
2022-01-17 14:21:51 +0200

    Add source map samples

---
+++ samples/source_map/issue_1804/imported.js
@@ -0,0 +1,11 @@
+export function f1() {
+  throw "line 2";
+}
+
+export function f2() {
+  throw "line 6";
+}
+
+export function f3() {
+  throw "line 10";
+}
---
+++ samples/source_map/typescript_template/test1.ts
@@ -0,0 +1,6 @@
+function coolThrow(s: string) {
+    throw "cool "+ s
+}
+export default () => {
+    coolThrow("is cool")
+};
