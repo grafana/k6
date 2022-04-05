fc35a51eadae3ada58a9da3a97f2a4e395bccb94
Ivan Mirić <ivan.miric@grafana.com>
2022-04-05 12:11:41 +0200

    Add JS example for emulating a mobile device

---
+++ examples/device_emulation.js
@@ -0,0 +1,39 @@
+import { check, sleep } from 'k6';
+import k6b from 'k6/x/browser';
+
+export default function() {
+  const browser = k6b.launch('chromium', {
+    headless: __ENV.XK6_HEADLESS ? true : false,
+  });
+
+  const device = k6b.devices['iPhone X'];
+  // The spread operator is currently unsupported by k6's Babel, so use
+  // Object.assign instead to merge browser context and device options.
+  // See https://github.com/grafana/k6/issues/2296
+  const options = Object.assign({ locale: 'es-ES' }, device);
+  const context = browser.newContext(options);
+  const page = context.newPage();
+
+  page.goto('https://k6.io/', { waitUntil: 'networkidle' });
+
+  const dimensions = page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+
+  check(dimensions, {
+    'width': d => d.width === device.viewport.width,
+    'height': d => d.height === device.viewport.height,
+    'scale': d => d.deviceScaleFactor === device.deviceScaleFactor,
+  });
+
+  if (!__ENV.XK6_HEADLESS) {
+    sleep(10);
+  }
+
+  page.close();
+  browser.close();
+}
