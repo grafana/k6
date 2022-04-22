1d32ee477b763962e2bc8f2ef86f240285845325
Ivan Mirić <ivan.miric@grafana.com>
2022-04-22 19:21:31 +0200

    Add lifecycle event validation to WaitForLoadState

--- common/frame.go
+++ common/frame.go
@@ -1583,12 +1583,9 @@ func (f *Frame) WaitForLoadState(state string, opts goja.Value) {
 		k6Throw(f.ctx, "cannot parse waitForLoadState options: %v", err)
 	}
 
-	waitUntil := LifecycleEventLoad
-	switch state {
-	case "domcontentloaded":
-		waitUntil = LifecycleEventDOMContentLoad
-	case "networkidle":
-		waitUntil = LifecycleEventNetworkIdle
+	var waitUntil LifecycleEvent
+	if err = waitUntil.UnmarshalText([]byte(state)); err != nil {
+		k6Throw(f.ctx, "waitForLoadState error: %v", err)
 	}
 
 	if f.hasLifecycleEventFired(waitUntil) {
