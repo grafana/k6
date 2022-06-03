5d402a46e14c7925e42d70fcad3ab4363990fa17
İnanç Gümüş <inanc.gumus@grafana.com>
2022-06-03 12:06:44 +0300

    Refactor locator.Check|Uncheck tests
    Merges the Check and Uncheck tests into one, so that we can use the same
    checkbox element and window.check property for both tests to reduce
    clutter and duplication.
    
    Resolves: https://github.com/grafana/xk6-browser/pull/365#discussion_r888745847

--- tests/locator_test.go
+++ tests/locator_test.go
@@ -58,15 +58,20 @@ func TestLocatorCheck(t *testing.T) {
 	p := tb.NewPage(nil)
 	require.NotNil(t, p.Goto(tb.staticURL("/locators.html"), nil))
 
-	// Selecting a single element and checking it is OK.
-	t.Run("ok", func(t *testing.T) {
-		check := func() bool {
-			cr := p.Evaluate(tb.toGojaValue(`() => window.check`))
-			return cr.(goja.Value).ToBoolean() //nolint:forcetypeassert
-		}
-		input := p.Locator("#input", nil)
-		input.Check(nil)
+	check := func() bool {
+		cr := p.Evaluate(tb.toGojaValue(`() => window.check`))
+		return cr.(goja.Value).ToBoolean() //nolint:forcetypeassert
+	}
+
+	t.Run("check", func(t *testing.T) {
+		checkbox := p.Locator("#inputCheckbox", nil)
+		checkbox.Check(nil)
 		require.True(t, check(), "could not check the input box")
+	})
+	t.Run("uncheck", func(t *testing.T) {
+		checkbox := p.Locator("#inputCheckbox", nil)
+		checkbox.Uncheck(nil)
+		require.False(t, check(), "could not uncheck the input box")
 	})
 	// There are two input boxes in the document (locators.html).
 	// The strict mode should disallow selecting multiple elements.
@@ -74,32 +79,6 @@ func TestLocatorCheck(t *testing.T) {
 		input := p.Locator("input", nil)
 		require.Panics(t,
 			func() { input.Check(nil) },
-			"should not select multiple elements",
-		)
-	})
-}
-
-func TestLocatorUncheck(t *testing.T) {
-	tb := newTestBrowser(t, withFileServer())
-	p := tb.NewPage(nil)
-	require.NotNil(t, p.Goto(tb.staticURL("/locators.html"), nil))
-
-	// Selecting a single element and checking it is OK.
-	t.Run("ok", func(t *testing.T) {
-		uncheck := func() bool {
-			cr := p.Evaluate(tb.toGojaValue(`() => window.uncheck`))
-			return cr.(goja.Value).ToBoolean() //nolint:forcetypeassert
-		}
-		input := p.Locator("#checkedInput", nil)
-		input.Uncheck(nil)
-		require.True(t, uncheck(), "could not uncheck the input box")
-	})
-	// There are two checked input boxes in the document (locators.html).
-	// The strict mode should disallow selecting multiple elements.
-	t.Run("strict", func(t *testing.T) {
-		input := p.Locator("input", nil)
-		require.Panics(t,
-			func() { input.Uncheck(nil) },
 			"should not select multiple elements",
 		)
 	})
