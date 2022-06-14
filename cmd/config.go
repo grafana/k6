318c7dea77c6c8676eaa44793e3c59a5aa1085d2
Mihail Stoykov <M.Stoikov@gmail.com>
2022-06-14 10:11:49 +0300

    appease the linter

--- cmd/config.go
+++ cmd/config.go
@@ -137,12 +137,12 @@ func readDiskConfig(globalState *globalState) (Config, error) {
 
 	data, err := afero.ReadFile(globalState.fs, globalState.flags.configFilePath)
 	if err != nil {
-		return Config{}, fmt.Errorf("Couldn't load configuration from %q:%w", globalState.flags.configFilePath, err)
+		return Config{}, fmt.Errorf("couldn't load configuration from %q:%w", globalState.flags.configFilePath, err)
 	}
 	var conf Config
 	err = json.Unmarshal(data, &conf)
 	if err != nil {
-		return Config{}, fmt.Errorf("Couldn't parse configuration from %q:%w", globalState.flags.configFilePath, err)
+		return Config{}, fmt.Errorf("couldn't parse configuration from %q:%w", globalState.flags.configFilePath, err)
 	}
 	return conf, nil
 }
