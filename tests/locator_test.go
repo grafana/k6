cfcd36f213d627f1ce7c29acd0ec27c8afa79bae
İnanç Gümüş <inanc.gumus@grafana.com>
2022-06-22 19:30:21 +0300

    Sort locator tests
    Resolves: https://github.com/grafana/xk6-browser/pull/412#discussion_r903906310
    Resolves: https://github.com/grafana/xk6-browser/pull/412#pullrequestreview-1015375567

--- tests/locator_test.go
+++ tests/locator_test.go
@@ -25,121 +25,11 @@ func TestLocator(t *testing.T) {
 		do   func(*testBrowser, api.Page)
 	}{
 		{
-			"WaitFor", func(tb *testBrowser, p api.Page) {
-				timeout := tb.toGojaValue(jsFrameBaseOpts{Timeout: "100"})
-				require.NotPanics(t, func() { p.Locator("#link", nil).WaitFor(timeout) })
-			},
-		},
-		{
-			"DispatchEvent", func(tb *testBrowser, p api.Page) {
-				result := func() bool {
-					return tb.asGojaBool(p.Evaluate(tb.toGojaValue(`() => window.result`)))
-				}
-				require.False(t, result(), "should not be clicked first")
-				p.Locator("#link", nil).DispatchEvent("click", tb.toGojaValue("mouseevent"), nil)
-				require.True(t, result(), "cannot not dispatch event")
-			},
-		},
-		{
-			"Tap", func(tb *testBrowser, p api.Page) {
-				result := func() bool {
-					return tb.asGojaBool(p.Evaluate(tb.toGojaValue(`() => window.result`)))
-				}
-				require.False(t, result(), "should not be tapped first")
-				p.Locator("#inputText", nil).Tap(nil)
-				require.True(t, result(), "should be tapped")
-			},
-		},
-		{
-			"Hover", func(tb *testBrowser, p api.Page) {
-				result := func() bool {
-					return tb.asGojaBool(p.Evaluate(tb.toGojaValue(`() => window.result`)))
-				}
-				require.False(t, result(), "should not be hovered first")
-				p.Locator("#inputText", nil).Hover(nil)
-				require.True(t, result(), "should be hovered")
-			},
-		},
-		{
-			"Type", func(tb *testBrowser, p api.Page) {
-				p.Locator("#inputText", nil).Type("real ", nil)
-				require.Equal(t, "real something", p.InputValue("#inputText", nil))
-			},
-		},
-		{
-			"Press", func(tb *testBrowser, p api.Page) {
-				p.Locator("#inputText", nil).Press("x", nil)
-				require.Equal(t, "xsomething", p.InputValue("#inputText", nil))
-			},
-		},
-		{
-			"SelectOption", func(tb *testBrowser, p api.Page) {
-				rv := p.Locator("#selectElement", nil).SelectOption(tb.toGojaValue(`option text 2`), nil)
-				require.Len(t, rv, 1)
-				require.Equal(t, "option text 2", rv[0])
-			},
-		},
-		{
-			"InputValue", func(tb *testBrowser, p api.Page) {
-				t.Run("input", func(t *testing.T) {
-					require.Equal(t, "something", p.Locator("#inputText", nil).InputValue(nil))
-				})
-				t.Run("textarea", func(t *testing.T) {
-					require.Equal(t, "text area", p.Locator("textarea", nil).InputValue(nil))
-				})
-				t.Run("select", func(t *testing.T) {
-					require.Equal(t, "option text", p.Locator("#selectElement", nil).InputValue(nil))
-				})
-			},
-		},
-		{
-			"TextContent", func(tb *testBrowser, p api.Page) {
-				require.Equal(t, `hello`, p.Locator("#divHello", nil).TextContent(nil))
-			},
-		},
-		{
-			"InnerText", func(tb *testBrowser, p api.Page) {
-				require.Equal(t, `hello`, p.Locator("#divHello > span", nil).InnerText(nil))
-			},
-		},
-		{
-			"InnerHTML", func(tb *testBrowser, p api.Page) {
-				require.Equal(t, `<span>hello</span>`, p.Locator("#divHello", nil).InnerHTML(nil))
-			},
-		},
-		{
-			"GetAttribute", func(tb *testBrowser, p api.Page) {
-				l := p.Locator("#inputText", nil)
-				v := l.GetAttribute("value", nil)
-				require.NotNil(t, v)
-				require.Equal(t, "something", v.ToString().String())
-			},
-		},
-		{
-			"Focus", func(tb *testBrowser, p api.Page) {
-				focused := func() bool {
-					return tb.asGojaBool(p.Evaluate(tb.toGojaValue(
-						`() => document.activeElement == document.getElementById('inputText')`,
-					)))
-				}
-				l := p.Locator("#inputText", nil)
-				require.False(t, focused(), "should not be focused first")
-				l.Focus(nil)
-				require.True(t, focused(), "should be focused")
-			},
-		},
-		{
-			"Fill", func(tb *testBrowser, p api.Page) {
-				const value = "fill me up"
-				p.Locator("#inputText", nil).Fill(value, nil)
-				require.Equal(t, value, p.InputValue("#inputText", nil))
-			},
-		},
-		{
 			"Check", func(tb *testBrowser, p api.Page) {
 				t.Run("check", func(t *testing.T) {
 					check := func() bool {
-						return tb.asGojaBool(p.Evaluate(tb.toGojaValue(`() => window.check`)))
+						v := p.Evaluate(tb.toGojaValue(`() => window.check`))
+						return tb.asGojaBool(v)
 					}
 					l := p.Locator("#inputCheckbox", nil)
 					require.False(t, check(), "should be unchecked first")
@@ -158,17 +48,133 @@ func TestLocator(t *testing.T) {
 			},
 		},
 		{
-			"DblClick", func(tb *testBrowser, p api.Page) {
-				p.Locator("#link", nil).Dblclick(nil)
-				require.True(t, tb.asGojaBool(p.Evaluate(tb.toGojaValue(`() => window.dblclick`))),
-					"cannot not double click the link")
+			"Click", func(tb *testBrowser, p api.Page) {
+				p.Locator("#link", nil).Click(nil)
+				v := p.Evaluate(tb.toGojaValue(`() => window.result`))
+				require.True(t, tb.asGojaBool(v), "cannot not click the link")
 			},
 		},
 		{
-			"Click", func(tb *testBrowser, p api.Page) {
-				p.Locator("#link", nil).Click(nil)
-				require.True(t, tb.asGojaBool(p.Evaluate(tb.toGojaValue(`() => window.result`))),
-					"cannot not click the link")
+			"DblClick", func(tb *testBrowser, p api.Page) {
+				p.Locator("#link", nil).Dblclick(nil)
+				v := p.Evaluate(tb.toGojaValue(`() => window.dblclick`))
+				require.True(t, tb.asGojaBool(v), "cannot not double click the link")
+			},
+		},
+		{
+			"DispatchEvent", func(tb *testBrowser, p api.Page) {
+				result := func() bool {
+					v := p.Evaluate(tb.toGojaValue(`() => window.result`))
+					return tb.asGojaBool(v)
+				}
+				require.False(t, result(), "should not be clicked first")
+				p.Locator("#link", nil).DispatchEvent("click", tb.toGojaValue("mouseevent"), nil)
+				require.True(t, result(), "cannot not dispatch event")
+			},
+		},
+		{
+			"Fill", func(tb *testBrowser, p api.Page) {
+				const value = "fill me up"
+				p.Locator("#inputText", nil).Fill(value, nil)
+				require.Equal(t, value, p.InputValue("#inputText", nil))
+			},
+		},
+		{
+			"Focus", func(tb *testBrowser, p api.Page) {
+				focused := func() bool {
+					v := p.Evaluate(tb.toGojaValue(
+						`() => document.activeElement == document.getElementById('inputText')`,
+					))
+					return tb.asGojaBool(v)
+				}
+				l := p.Locator("#inputText", nil)
+				require.False(t, focused(), "should not be focused first")
+				l.Focus(nil)
+				require.True(t, focused(), "should be focused")
+			},
+		},
+		{
+			"GetAttribute", func(tb *testBrowser, p api.Page) {
+				l := p.Locator("#inputText", nil)
+				v := l.GetAttribute("value", nil)
+				require.NotNil(t, v)
+				require.Equal(t, "something", v.ToString().String())
+			},
+		},
+		{
+			"Hover", func(tb *testBrowser, p api.Page) {
+				result := func() bool {
+					v := p.Evaluate(tb.toGojaValue(`() => window.result`))
+					return tb.asGojaBool(v)
+				}
+				require.False(t, result(), "should not be hovered first")
+				p.Locator("#inputText", nil).Hover(nil)
+				require.True(t, result(), "should be hovered")
+			},
+		},
+		{
+			"InnerHTML", func(tb *testBrowser, p api.Page) {
+				require.Equal(t, `<span>hello</span>`, p.Locator("#divHello", nil).InnerHTML(nil))
+			},
+		},
+		{
+			"InnerText", func(tb *testBrowser, p api.Page) {
+				require.Equal(t, `hello`, p.Locator("#divHello > span", nil).InnerText(nil))
+			},
+		},
+		{
+			"InputValue", func(tb *testBrowser, p api.Page) {
+				t.Run("input", func(t *testing.T) {
+					require.Equal(t, "something", p.Locator("#inputText", nil).InputValue(nil))
+				})
+				t.Run("textarea", func(t *testing.T) {
+					require.Equal(t, "text area", p.Locator("textarea", nil).InputValue(nil))
+				})
+				t.Run("select", func(t *testing.T) {
+					require.Equal(t, "option text", p.Locator("#selectElement", nil).InputValue(nil))
+				})
+			},
+		},
+		{
+			"Press", func(tb *testBrowser, p api.Page) {
+				p.Locator("#inputText", nil).Press("x", nil)
+				require.Equal(t, "xsomething", p.InputValue("#inputText", nil))
+			},
+		},
+		{
+			"SelectOption", func(tb *testBrowser, p api.Page) {
+				l := p.Locator("#selectElement", nil)
+				rv := l.SelectOption(tb.toGojaValue(`option text 2`), nil)
+				require.Len(t, rv, 1)
+				require.Equal(t, "option text 2", rv[0])
+			},
+		},
+		{
+			"Tap", func(tb *testBrowser, p api.Page) {
+				result := func() bool {
+					v := p.Evaluate(tb.toGojaValue(`() => window.result`))
+					return tb.asGojaBool(v)
+				}
+				require.False(t, result(), "should not be tapped first")
+				p.Locator("#inputText", nil).Tap(nil)
+				require.True(t, result(), "should be tapped")
+			},
+		},
+		{
+			"TextContent", func(tb *testBrowser, p api.Page) {
+				require.Equal(t, `hello`, p.Locator("#divHello", nil).TextContent(nil))
+			},
+		},
+		{
+			"Type", func(tb *testBrowser, p api.Page) {
+				p.Locator("#inputText", nil).Type("real ", nil)
+				require.Equal(t, "real something", p.InputValue("#inputText", nil))
+			},
+		},
+		{
+			"WaitFor", func(tb *testBrowser, p api.Page) {
+				timeout := tb.toGojaValue(jsFrameBaseOpts{Timeout: "100"})
+				require.NotPanics(t, func() { p.Locator("#link", nil).WaitFor(timeout) })
 			},
 		},
 	}
@@ -182,6 +188,90 @@ func TestLocator(t *testing.T) {
 			tt.do(tb, p)
 		})
 	}
+
+	timeout := func(tb *testBrowser) goja.Value {
+		return tb.toGojaValue(jsFrameBaseOpts{Timeout: "100"})
+	}
+	sanityTests := []struct {
+		name string
+		do   func(api.Locator, *testBrowser)
+	}{
+		{
+			"Check", func(l api.Locator, tb *testBrowser) { l.Check(timeout(tb)) },
+		},
+		{
+			"Click", func(l api.Locator, tb *testBrowser) { l.Click(timeout(tb)) },
+		},
+		{
+			"Dblclick", func(l api.Locator, tb *testBrowser) { l.Dblclick(timeout(tb)) },
+		},
+		{
+			"DispatchEvent", func(l api.Locator, tb *testBrowser) {
+				l.DispatchEvent("click", tb.toGojaValue("mouseevent"), timeout(tb))
+			},
+		},
+		{
+			"Focus", func(l api.Locator, tb *testBrowser) { l.Focus(timeout(tb)) },
+		},
+		{
+			"Fill", func(l api.Locator, tb *testBrowser) { l.Fill("fill me up", timeout(tb)) },
+		},
+		{
+			"GetAttribute", func(l api.Locator, tb *testBrowser) { l.GetAttribute("value", timeout(tb)) },
+		},
+		{
+			"Hover", func(l api.Locator, tb *testBrowser) { l.Hover(timeout(tb)) },
+		},
+		{
+			"InnerHTML", func(l api.Locator, tb *testBrowser) { l.InnerHTML(timeout(tb)) },
+		},
+		{
+			"InnerText", func(l api.Locator, tb *testBrowser) { l.InnerText(timeout(tb)) },
+		},
+		{
+			"InputValue", func(l api.Locator, tb *testBrowser) { l.InputValue(timeout(tb)) },
+		},
+		{
+			"Press", func(l api.Locator, tb *testBrowser) { l.Press("a", timeout(tb)) },
+		},
+		{
+			"SelectOption", func(l api.Locator, tb *testBrowser) { l.SelectOption(tb.toGojaValue(""), timeout(tb)) },
+		},
+		{
+			"Tap", func(l api.Locator, tb *testBrowser) { l.Tap(timeout(tb)) },
+		},
+		{
+			"Type", func(l api.Locator, tb *testBrowser) { l.Type("a", timeout(tb)) },
+		},
+		{
+			"TextContent", func(l api.Locator, tb *testBrowser) { l.TextContent(timeout(tb)) },
+		},
+		{
+			"Uncheck", func(l api.Locator, tb *testBrowser) { l.Uncheck(timeout(tb)) },
+		},
+		{
+			"WaitFor", func(l api.Locator, tb *testBrowser) { l.WaitFor(timeout(tb)) },
+		},
+	}
+	for _, tt := range sanityTests {
+		t.Run("timeout/"+tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			tb := newTestBrowser(t)
+			p := tb.NewPage(nil)
+			p.SetContent("<html></html>", nil)
+			assert.Panics(t, func() { tt.do(p.Locator("NOTEXIST", nil), tb) })
+		})
+	}
+
+	tb := newTestBrowser(t, withFileServer())
+	p := tb.NewPage(nil)
+	require.NotNil(t, p.Goto(tb.staticURL("/locators.html"), nil))
+	for _, tt := range sanityTests {
+		t.Run("strict/"+tt.name, func(t *testing.T) {
+			assert.Panics(t, func() { tt.do(p.Locator("a", nil), tb) })
+		})
+	}
 }
 
 func TestLocatorElementState(t *testing.T) {
@@ -192,9 +282,9 @@ func TestLocatorElementState(t *testing.T) {
 		query       func(api.Locator) bool
 	}{
 		{
-			"readOnly",
-			`() => document.getElementById('inputText').readOnly = true`,
-			func(l api.Locator) bool { return l.IsEditable(nil) },
+			"disabled",
+			`() => document.getElementById('inputText').disabled = true`,
+			func(l api.Locator) bool { return !l.IsDisabled(nil) },
 		},
 		{
 			"enabled",
@@ -202,19 +292,19 @@ func TestLocatorElementState(t *testing.T) {
 			func(l api.Locator) bool { return l.IsEnabled(nil) },
 		},
 		{
-			"disabled",
-			`() => document.getElementById('inputText').disabled = true`,
-			func(l api.Locator) bool { return !l.IsDisabled(nil) },
+			"hidden",
+			`() => document.getElementById('inputText').style.visibility = 'hidden'`,
+			func(l api.Locator) bool { return !l.IsHidden(nil) },
+		},
+		{
+			"readOnly",
+			`() => document.getElementById('inputText').readOnly = true`,
+			func(l api.Locator) bool { return l.IsEditable(nil) },
 		},
 		{
 			"visible",
 			`() => document.getElementById('inputText').style.visibility = 'hidden'`,
 			func(l api.Locator) bool { return l.IsVisible(nil) },
-		},
-		{
-			"hidden",
-			`() => document.getElementById('inputText').style.visibility = 'hidden'`,
-			func(l api.Locator) bool { return !l.IsHidden(nil) },
 		},
 	}
 
@@ -233,77 +323,17 @@ func TestLocatorElementState(t *testing.T) {
 			require.False(t, tt.query(l))
 		})
 	}
-}
-
-func TestLocatorSanity(t *testing.T) {
-	t.Parallel()
 
 	timeout := func(tb *testBrowser) goja.Value {
 		return tb.toGojaValue(jsFrameBaseOpts{Timeout: "100"})
 	}
-
-	tests := []struct {
+	sanityTests := []struct {
 		name string
 		do   func(api.Locator, *testBrowser)
 	}{
-		{
-			"WaitFor", func(l api.Locator, tb *testBrowser) { l.WaitFor(timeout(tb)) },
-		},
-		{
-			"DispatchEvent", func(l api.Locator, tb *testBrowser) {
-				l.DispatchEvent("click", tb.toGojaValue("mouseevent"), timeout(tb))
-			},
-		},
-		{
-			"Tap", func(l api.Locator, tb *testBrowser) { l.Tap(timeout(tb)) },
-		},
-		{
-			"Hover", func(l api.Locator, tb *testBrowser) { l.Hover(timeout(tb)) },
-		},
-		{
-			"Type", func(l api.Locator, tb *testBrowser) { l.Type("a", timeout(tb)) },
-		},
-		{
-			"Press", func(l api.Locator, tb *testBrowser) { l.Press("a", timeout(tb)) },
-		},
-		{
-			"SelectOption", func(l api.Locator, tb *testBrowser) { l.SelectOption(tb.toGojaValue(""), timeout(tb)) },
-		},
-		{
-			"GetAttribute", func(l api.Locator, tb *testBrowser) { l.GetAttribute("value", timeout(tb)) },
-		},
-		{
-			"InnerHTML", func(l api.Locator, tb *testBrowser) { l.InnerHTML(timeout(tb)) },
-		},
-		{
-			"InnerText", func(l api.Locator, tb *testBrowser) { l.InnerText(timeout(tb)) },
-		},
-		{
-			"TextContent", func(l api.Locator, tb *testBrowser) { l.TextContent(timeout(tb)) },
-		},
-		{
-			"InputValue", func(l api.Locator, tb *testBrowser) { l.InputValue(timeout(tb)) },
-		},
-		{
-			"Focus", func(l api.Locator, tb *testBrowser) { l.Focus(timeout(tb)) },
-		},
-		{
-			"Fill", func(l api.Locator, tb *testBrowser) { l.Fill("fill me up", timeout(tb)) },
-		},
-		{
-			"Check", func(l api.Locator, tb *testBrowser) { l.Check(timeout(tb)) },
-		},
-		{
-			"Uncheck", func(l api.Locator, tb *testBrowser) { l.Uncheck(timeout(tb)) },
-		},
+
 		{
 			"IsChecked", func(l api.Locator, tb *testBrowser) { l.IsChecked(timeout(tb)) },
-		},
-		{
-			"Dblclick", func(l api.Locator, tb *testBrowser) { l.Dblclick(timeout(tb)) },
-		},
-		{
-			"Click", func(l api.Locator, tb *testBrowser) { l.Click(timeout(tb)) },
 		},
 		{
 			"IsEditable", func(l api.Locator, tb *testBrowser) { l.IsEditable(timeout(tb)) },
@@ -321,7 +351,7 @@ func TestLocatorSanity(t *testing.T) {
 			"IsHidden", func(l api.Locator, tb *testBrowser) { l.IsHidden(timeout(tb)) },
 		},
 	}
-	for _, tt := range tests {
+	for _, tt := range sanityTests {
 		t.Run("timeout/"+tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -335,7 +365,7 @@ func TestLocatorSanity(t *testing.T) {
 	tb := newTestBrowser(t, withFileServer())
 	p := tb.NewPage(nil)
 	require.NotNil(t, p.Goto(tb.staticURL("/locators.html"), nil))
-	for _, tt := range tests {
+	for _, tt := range sanityTests {
 		t.Run("strict/"+tt.name, func(t *testing.T) {
 			assert.Panics(t, func() { tt.do(p.Locator("a", nil), tb) })
 		})
