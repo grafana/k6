7f3131bce3d0b80636fc0eb0c89497037ba412e1
Nedyalko Andreev <n@andreev.sh>
2022-07-05 17:25:40 +0300

    Build binaries and packages (with nfpm) on every CI run, not just releases
    We just shouldn't sign or publish these binaries and packages. But if we build them every time, we'll have a higher assurance they won't fail when we try to release every 8 weeks.

--- build-release.sh
+++ build-release.sh
@@ -4,46 +4,58 @@ set -eEuo pipefail
 
 eval "$(go env)"
 
-OUT_DIR="${1-dist}"
+set -x
+export OUT_DIR="${1-dist}"
 # To override the latest git tag as the version, pass something else as the second arg.
-VERSION=${2:-$(git describe --tags --always --dirty)}
+export VERSION=${2:-$(git describe --tags --always --dirty)}
 
 # To overwrite the version details, pass something as the third arg. Empty string disables it.
-VERSION_DETAILS=${3-"$(date -u +"%FT%T%z")/$(git describe --always --long --dirty)"}
+export VERSION_DETAILS=${3-"$(date -u +"%FT%T%z")/$(git describe --always --long --dirty)"}
+set +x
 
 build() {
     local ALIAS="$1" SUFFIX="${2}"  # Any other arguments are passed to the go build command as env vars
     local NAME="k6-${VERSION}-${ALIAS}"
 
-    local BUILD_ENV=("${@:3}")
     local BUILD_ARGS=(-o "${OUT_DIR}/${NAME}/k6${SUFFIX}" -trimpath)
-
     if [ -n "$VERSION_DETAILS" ]; then
         BUILD_ARGS+=(-ldflags "-X go.k6.io/k6/lib/consts.VersionDetails=$VERSION_DETAILS")
     fi
 
-    echo "- Building platform: ${ALIAS} (" "${BUILD_ENV[@]}" "go build" "${BUILD_ARGS[@]}" ")"
+    local PACKAGE_FORMATS
+    IFS="," read -ra PACKAGE_FORMATS <<< "${3}"
+
+    local ENV_VARS
+    IFS="," read -ra ENV_VARS <<< "${4}"
+
+    echo "- Building platform: ${ALIAS} (" "${ENV_VARS[@]}" "go build" "${BUILD_ARGS[@]}" ")"
 
     mkdir -p "${OUT_DIR}/${NAME}"
 
     # Subshell to not mess with the current env vars or CWD
     (
-        export "${BUILD_ENV[@]}"
+        export "${ENV_VARS[@]}"
         # Build a binary
-         go build "${BUILD_ARGS[@]}"
+        go build "${BUILD_ARGS[@]}"
+
+        for format in "${PACKAGE_FORMATS[@]}"; do
+            package "$format"
+        done
     )
 }
 
 package() {
-    local ALIAS="$1" FMT="$2"
-    local NAME="k6-${VERSION}-${ALIAS}"
+    local FMT="$1"
     echo "- Creating ${NAME}.${FMT} package..."
     case $FMT in
     deb|rpm)
-        # The go-bin-* tools expect the binary in /tmp/
-        [ ! -r /tmp/k6 ] && cp "${OUT_DIR}/${NAME}/k6" /tmp/k6
-        "go-bin-${FMT}" generate --file "packaging/${FMT}.json" -a amd64 \
-            --version "${VERSION#v}" -o "${OUT_DIR}/k6-${VERSION}-linux-amd64.${FMT}"
+        # nfpm can't substitute env vars in file paths, so we have to cd...
+        cd "${OUT_DIR}/${NAME}"
+        set -x
+        nfpm package --config ../../packaging/nfpm.yaml --packager "${FMT}" \
+            --target "../k6-${VERSION}-linux-${GOARCH}.${FMT}"
+        set +x
+        cd -
         ;;
     tgz)
         tar -C "${OUT_DIR}" -zcf "${OUT_DIR}/${NAME}.tar.gz" "$NAME"
@@ -68,16 +80,8 @@ echo "--- Building Release: ${VERSION}"
 
 mkdir -p "$OUT_DIR"
 
-build macos-amd64   ""   GOOS=darwin  GOARCH=amd64
-build macos-arm64   ""   GOOS=darwin  GOARCH=arm64
-build windows-amd64 .exe GOOS=windows GOARCH=amd64
-build linux-amd64   ""   GOOS=linux   GOARCH=amd64  CGO_ENABLED=0
-build linux-arm64   ""   GOOS=linux   GOARCH=arm64  CGO_ENABLED=0
-
-package macos-amd64   zip
-package macos-arm64   zip
-package windows-amd64 zip
-package linux-amd64   tgz
-package linux-arm64   tgz
-package linux-amd64   rpm
-package linux-amd64   deb
+build linux-amd64   ""      tgz,rpm,deb     GOOS=linux,GOARCH=amd64,CGO_ENABLED=0
+build linux-arm64   ""      tgz             GOOS=linux,GOARCH=arm64,CGO_ENABLED=0 # TODO: package rpm and dep too
+build macos-amd64   ""      zip             GOOS=darwin,GOARCH=amd64
+build macos-arm64   ""      zip             GOOS=darwin,GOARCH=arm64
+build windows-amd64 .exe    zip             GOOS=windows,GOARCH=amd64
