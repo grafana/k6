c54c5738aea9e3e6101e555d6eec6a570edafe22
Mihail Stoykov <M.Stoikov@gmail.com>
2022-07-12 16:43:33 +0300

    Drop useless bridge benchmark and fix js/common lint errors
    closes #1504 as we no longer even have a bridge - we use goja's own
    logic

--- js/common/bridge_test.go
+++ js/common/bridge_test.go
@@ -21,12 +21,9 @@
 package common
 
 import (
-	"context"
-	"errors"
 	"reflect"
 	"testing"
 
-	"github.com/dop251/goja"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -34,109 +31,25 @@ type bridgeTestFieldsType struct {
 	Exported       string
 	ExportedTag    string `js:"renamed"`
 	ExportedHidden string `js:"-"`
-	unexported     string
-	unexportedTag  string `js:"unexported"`
+	unexported     string //nolint:structcheck,unused // actually checked in the test
+	unexportedTag  string `js:"unexported"` //nolint:structcheck,unused // actually checked in the test
 }
 
 type bridgeTestMethodsType struct{}
 
 func (bridgeTestMethodsType) ExportedFn() {}
 
-//lint:ignore U1000 needed for the actual test to check that it won't be seen
+//nolint:unused // needed for the actual test to check that it won't be seen
 func (bridgeTestMethodsType) unexportedFn() {}
 
 func (*bridgeTestMethodsType) ExportedPtrFn() {}
 
-//lint:ignore U1000 needed for the actual test to check that it won't be seen
+//nolint:unused // needed for the actual test to check that it won't be seen
 func (*bridgeTestMethodsType) unexportedPtrFn() {}
 
 type bridgeTestOddFieldsType struct {
 	TwoWords string
 	URL      string
-}
-
-type bridgeTestErrorType struct{}
-
-func (bridgeTestErrorType) Error() error { return errors.New("error") }
-
-type bridgeTestAddType struct{}
-
-func (bridgeTestAddType) Add(a, b int) int { return a + b }
-
-type bridgeTestAddWithErrorType struct{}
-
-func (bridgeTestAddWithErrorType) AddWithError(a, b int) (int, error) {
-	res := a + b
-	if res < 0 {
-		return 0, errors.New("answer is negative")
-	}
-	return res, nil
-}
-
-type bridgeTestContextType struct{}
-
-func (bridgeTestContextType) Context(ctx context.Context) {}
-
-type bridgeTestContextAddType struct{}
-
-func (bridgeTestContextAddType) ContextAdd(ctx context.Context, a, b int) int {
-	return a + b
-}
-
-type bridgeTestContextAddWithErrorType struct{}
-
-func (bridgeTestContextAddWithErrorType) ContextAddWithError(ctx context.Context, a, b int) (int, error) {
-	res := a + b
-	if res < 0 {
-		return 0, errors.New("answer is negative")
-	}
-	return res, nil
-}
-
-type bridgeTestSumType struct{}
-
-func (bridgeTestSumType) Sum(nums ...int) int {
-	sum := 0
-	for v := range nums {
-		sum += v
-	}
-	return sum
-}
-
-type bridgeTestSumWithContextType struct{}
-
-func (bridgeTestSumWithContextType) SumWithContext(ctx context.Context, nums ...int) int {
-	sum := 0
-	for v := range nums {
-		sum += v
-	}
-	return sum
-}
-
-type bridgeTestSumWithErrorType struct{}
-
-func (bridgeTestSumWithErrorType) SumWithError(nums ...int) (int, error) {
-	sum := 0
-	for v := range nums {
-		sum += v
-	}
-	if sum < 0 {
-		return 0, errors.New("answer is negative")
-	}
-	return sum, nil
-}
-
-type bridgeTestSumWithContextAndErrorType struct{}
-
-func (m bridgeTestSumWithContextAndErrorType) SumWithContextAndError(ctx context.Context, nums ...int) (int, error) {
-	sum := 0
-	for v := range nums {
-		sum += v
-	}
-	if sum < 0 {
-		return 0, errors.New("answer is negative")
-	}
-	return sum, nil
 }
 
 type bridgeTestConstructorType struct{}
@@ -148,6 +61,7 @@ func (bridgeTestConstructorType) XConstructor() bridgeTestConstructorSpawnedType
 }
 
 func TestFieldNameMapper(t *testing.T) {
+	t.Parallel()
 	testdata := []struct {
 		Typ     reflect.Type
 		Fields  map[string]string
@@ -173,8 +87,11 @@ func TestFieldNameMapper(t *testing.T) {
 		}},
 	}
 	for _, data := range testdata {
+		data := data
 		for field, name := range data.Fields {
+			field, name := field, name
 			t.Run(field, func(t *testing.T) {
+				t.Parallel()
 				f, ok := data.Typ.FieldByName(field)
 				if assert.True(t, ok, "no such field") {
 					assert.Equal(t, name, (FieldNameMapper{}).FieldName(data.Typ, f))
@@ -182,7 +99,9 @@ func TestFieldNameMapper(t *testing.T) {
 			})
 		}
 		for meth, name := range data.Methods {
+			meth, name := meth, name
 			t.Run(meth, func(t *testing.T) {
+				t.Parallel()
 				m, ok := data.Typ.MethodByName(meth)
 				if name != "" {
 					if assert.True(t, ok, "no such method") {
@@ -193,141 +112,5 @@ func TestFieldNameMapper(t *testing.T) {
 				}
 			})
 		}
-	}
-}
-
-func BenchmarkProxy(b *testing.B) {
-	types := []struct {
-		Name, FnName string
-		Value        interface{}
-		Fn           func(b *testing.B, fn interface{})
-	}{
-		{"Fields", "", bridgeTestFieldsType{}, nil},
-		{"Methods", "exportedFn", bridgeTestMethodsType{}, func(b *testing.B, fn interface{}) {
-			f := fn.(func())
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f()
-			}
-		}},
-		{"Error", "", bridgeTestErrorType{}, nil},
-		{"Add", "add", bridgeTestAddType{}, func(b *testing.B, fn interface{}) {
-			f := fn.(func(int, int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2)
-			}
-		}},
-		{"AddError", "addWithError", bridgeTestAddWithErrorType{}, func(b *testing.B, fn interface{}) {
-			b.Skip()
-			f := fn.(func(int, int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2)
-			}
-		}},
-		{"Context", "context", bridgeTestContextType{}, func(b *testing.B, fn interface{}) {
-			b.Skip()
-			f := fn.(func())
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f()
-			}
-		}},
-		{"ContextAdd", "contextAdd", bridgeTestContextAddType{}, func(b *testing.B, fn interface{}) {
-			b.Skip()
-			f := fn.(func(int, int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2)
-			}
-		}},
-		{"ContextAddError", "contextAddWithError", bridgeTestContextAddWithErrorType{}, func(b *testing.B, fn interface{}) {
-			b.Skip()
-			f := fn.(func(int, int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2)
-			}
-		}},
-		{"Sum", "sum", bridgeTestSumType{}, func(b *testing.B, fn interface{}) {
-			f := fn.(func(...int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2, 3)
-			}
-		}},
-		{"SumContext", "sumWithContext", bridgeTestSumWithContextType{}, func(b *testing.B, fn interface{}) {
-			b.Skip()
-			f := fn.(func(...int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2, 3)
-			}
-		}},
-		{"SumError", "sumWithError", bridgeTestSumWithErrorType{}, func(b *testing.B, fn interface{}) {
-			b.Skip()
-			f := fn.(func(...int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2, 3)
-			}
-		}},
-		{"SumContextError", "sumWithContextAndError", bridgeTestSumWithContextAndErrorType{}, func(b *testing.B, fn interface{}) {
-			b.Skip()
-			f := fn.(func(...int) int)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				f(1, 2, 3)
-			}
-		}},
-		{"Constructor", "Constructor", bridgeTestConstructorType{}, func(b *testing.B, fn interface{}) {
-			f, _ := goja.AssertFunction(fn.(goja.Value))
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				_, _ = f(goja.Undefined())
-			}
-		}},
-	}
-	vfns := []struct {
-		Name string
-		Fn   func(interface{}) interface{}
-	}{
-		{"Value", func(v interface{}) interface{} { return v }},
-		{"Pointer", func(v interface{}) interface{} {
-			val := reflect.ValueOf(v)
-			ptr := reflect.New(val.Type())
-			ptr.Elem().Set(val)
-			return ptr.Interface()
-		}},
-	}
-
-	for _, vfn := range vfns {
-		b.Run(vfn.Name, func(b *testing.B) {
-			for _, typ := range types {
-				b.Run(typ.Name, func(b *testing.B) {
-					v := vfn.Fn(typ.Value)
-
-					b.Run("ToValue", func(b *testing.B) {
-						rt := goja.New()
-						rt.SetFieldNameMapper(FieldNameMapper{})
-						b.ResetTimer()
-
-						for i := 0; i < b.N; i++ {
-							rt.ToValue(v)
-						}
-					})
-
-					if typ.FnName != "" {
-						b.Run("Call", func(b *testing.B) {
-							rt := goja.New()
-							rt.SetFieldNameMapper(FieldNameMapper{})
-							fn := func() {}
-							typ.Fn(b, fn)
-						})
-					}
-				})
-			}
-		})
 	}
 }
