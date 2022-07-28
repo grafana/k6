88eea8b6a6190f3885093356e4c80dfca10ea888
Mihail Stoykov <M.Stoikov@gmail.com>
2022-07-28 15:17:57 +0300

    Drop SetInitContext and instead have an a okay default value

--- js/modulestest/runtime.go
+++ js/modulestest/runtime.go
@@ -9,13 +9,16 @@ import (
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/eventloop"
 	"go.k6.io/k6/lib"
+	"go.k6.io/k6/lib/testutils"
+	"go.k6.io/k6/metrics"
 )
 
 // Runtime is a helper struct that contains what is needed to run a (simple) module test
 type Runtime struct {
-	VU            *VU
-	EventLoop     *eventloop.EventLoop
-	CancelContext func()
+	VU             *VU
+	EventLoop      *eventloop.EventLoop
+	CancelContext  func()
+	BuiltinMetrics *metrics.BuiltinMetrics
 }
 
 // NewRuntime will create a new test runtime and will cancel the context on test/benchmark end
@@ -27,21 +30,22 @@ func NewRuntime(t testing.TB) *Runtime {
 		RuntimeField: goja.New(),
 	}
 	vu.RuntimeField.SetFieldNameMapper(common.FieldNameMapper{})
+	vu.InitEnvField = &common.InitEnvironment{
+		Logger:   testutils.NewLogger(t),
+		Registry: metrics.NewRegistry(),
+	}
+
 	eventloop := eventloop.New(vu)
 	vu.RegisterCallbackField = eventloop.RegisterCallback
 	result := &Runtime{
-		VU:            vu,
-		EventLoop:     eventloop,
-		CancelContext: cancel,
+		VU:             vu,
+		EventLoop:      eventloop,
+		CancelContext:  cancel,
+		BuiltinMetrics: metrics.RegisterBuiltinMetrics(vu.InitEnvField.Registry),
 	}
 	// let's cancel again in case it has changed
 	t.Cleanup(func() { result.CancelContext() })
 	return result
-}
-
-// SetInitContext will set the SetInitContext of the Runtime
-func (r *Runtime) SetInitContext(i *common.InitEnvironment) {
-	r.VU.InitEnvField = i
 }
 
 // MoveToVUContext will set the state and nil the InitEnv just as a real VU
