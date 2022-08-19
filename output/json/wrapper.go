95921a9e9fb1c7e152a8385420e8116ff505a784
Nedyalko Andreev <n@andreev.sh>
2022-08-19 11:42:23 +0300

    Add support for metadata (non-indexable tags) in the JSON output

--- output/json/wrapper.go
+++ output/json/wrapper.go
@@ -1,8 +1,12 @@
 package json
 
 import (
+	"errors"
 	"time"
 
+	jlexer "github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
+	"github.com/mstoykov/atlas"
 	"go.k6.io/k6/metrics"
 )
 
@@ -10,26 +14,65 @@ import (
 
 //easyjson:json
 type sampleEnvelope struct {
-	Type string `json:"type"`
-	Data struct {
-		Time  time.Time       `json:"time"`
-		Value float64         `json:"value"`
-		Tags  *metrics.TagSet `json:"tags"`
-	} `json:"data"`
 	Metric string `json:"metric"`
+	Type   string `json:"type"`
+	Data   struct {
+		Time  time.Time           `json:"time"`
+		Value float64             `json:"value"`
+		Tags  tagsAndMetaEnvelope `json:"tags"`
+	} `json:"data"`
+}
+
+type tagsAndMetaEnvelope metrics.TagsAndMeta
+
+func (tme tagsAndMetaEnvelope) MarshalEasyJSON(w *jwriter.Writer) {
+	w.RawByte('{')
+	first := true
+
+	// TODO: figure out some way to not rely on the Atlas API?
+	n := (*atlas.Node)(tme.Tags)
+	for !n.IsRoot() {
+		prev, key, value := n.Data()
+		if first {
+			first = false
+		} else {
+			w.RawByte(',')
+		}
+		w.String(key)
+		w.RawByte(':')
+		w.String(value)
+		n = prev
+	}
+	for key, value := range tme.Metadata {
+		if first {
+			first = false
+		} else {
+			w.RawByte(',')
+		}
+		w.String(key)
+		w.RawByte(':')
+		w.String(value)
+	}
+	w.RawByte('}')
+}
+
+func (tme *tagsAndMetaEnvelope) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	l.AddError(errors.New("tagsAndMetaEnvelope cannot be unmarshalled from JSON"))
 }
 
 // wrapSample is used to package a metric sample in a way that's nice to export
-// to JSON.
+// to JSON and backwards-compatible.
 func wrapSample(sample metrics.Sample) sampleEnvelope {
-	// TODO: emit both the Sample's Metadata and Tags as tags
 	s := sampleEnvelope{
 		Type:   "Point",
 		Metric: sample.Metric.Name,
 	}
 	s.Data.Time = sample.Time
 	s.Data.Value = sample.Value
-	s.Data.Tags = sample.Tags
+	s.Data.Tags = tagsAndMetaEnvelope{
+		Tags:     sample.Tags,
+		Metadata: sample.Metadata,
+	}
 	return s
 }
 
