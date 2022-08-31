33017e51915df5b66cde1e4254435f3ac6d63212
Ivan Mirić <ivan.miric@grafana.com>
2022-08-31 11:03:28 +0200

    Remove v prefix from version constant
    The 'v' is redundant, and this way it's consistent with
    browser.version(), which also doesn't include it.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	k6modules "go.k6.io/k6/js/modules"
 )
 
-const version = "v0.4.0"
+const version = "0.4.0"
 
 type (
 	// RootModule is the global module instance that will create module
