7aa50d9d549b65ff5b0e0fef71b669d03070ace9
mdcruz <marie.drake@grafana.com>
2022-09-12 16:08:22 +0100

    add example on how to use xk6-browser with k6

---
+++ examples/multiple-scenario.js
@@ -0,0 +1,49 @@
+import { chromium } from 'k6/x/browser'
+
+export const options = {
+  scenarios: {
+    messages: {
+      executor: 'constant-vus',
+      exec: 'messages',
+      vus: 2,
+      duration: '2s',
+    },
+    news: {
+      executor: 'per-vu-iterations',
+      exec: 'news',
+      vus: 2,
+      iterations: 4,
+      maxDuration: '5s',
+    },
+  },
+  thresholds: {
+    browser_dom_content_loaded: ['p(90) < 1000'],
+    browser_first_contentful_paint: ['max < 1000']
+  }
+}
+
+export function messages() {
+  const browser = chromium.launch({
+    headless: __ENV.XK6_HEADLESS ? true : false,
+  });
+  const context = browser.newContext();
+  const page = context.newPage();
+
+  page.goto('https://test.k6.io/my_messages.php', { waitUntil: 'networkidle' })
+
+  page.close();
+  browser.close();
+}
+
+export function news() {
+  const browser = chromium.launch({
+    headless: __ENV.XK6_HEADLESS ? true : false,
+  });
+  const context = browser.newContext();
+  const page = context.newPage();
+
+  page.goto('https://test.k6.io/news.php', { waitUntil: 'networkidle' })
+
+  page.close();
+  browser.close();
+}
