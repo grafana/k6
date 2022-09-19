b9ea2eb1df0896009d79c7393a9c5a45b58e0d50
Aleksandr Melkikh <5112566+amelkikh@users.noreply.github.com>
2022-09-19 19:31:59 +0200

    Added information about built-in modules for Version command

--- cmd/version.go
+++ cmd/version.go
@@ -4,18 +4,38 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-
+	"go.k6.io/k6/js/modules"
 	"go.k6.io/k6/lib/consts"
 )
 
 func getCmdVersion(globalState *globalState) *cobra.Command {
 	// versionCmd represents the version command.
-	return &cobra.Command{
+	var showModules bool
+
+	versionCmd := &cobra.Command{
 		Use:   "version",
 		Short: "Show application version",
 		Long:  `Show the application version and exit.`,
 		Run: func(_ *cobra.Command, _ []string) {
 			printToStdout(globalState, fmt.Sprintf("k6 v%s\n", consts.FullVersion()))
+
+			if showModules {
+				mods := modules.GetJSModules()
+				if len(mods) > 0 {
+					printToStdout(globalState, "modules:\n")
+					for moduleName, _ := range mods {
+						printToStdout(globalState, fmt.Sprintf("\t- %s\n", moduleName))
+					}
+				}
+			}
 		},
 	}
+
+	versionCmd.Flags().BoolVarP(&showModules,
+		"modules",
+		"m",
+		false,
+		"show built-in modules")
+
+	return versionCmd
 }
