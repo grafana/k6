02d90ba89c5540bcb0584c55f1028badcf0bba42
Nathan Smith <12156185+nsmith5@users.noreply.github.com>
2022-09-21 06:58:53 -0700

    log: add more tokenizer tests (#2687)
    * log: add more tokenizer tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Ivan <2103732+codebien@users.noreply.github.com>
    
    Co-authored-by: Mihail Stoykov <312246+mstoykov@users.noreply.github.com>
    Co-authored-by: Ivan <2103732+codebien@users.noreply.github.com>

--- log/tokenizer_test.go
+++ log/tokenizer_test.go
@@ -4,44 +4,65 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTokenizer(t *testing.T) {
-	tokens, err := tokenize("loki=something,s.e=2231,s=12,12=3,a=[1,2,3],b=[1],s=c")
-	assert.Equal(t, []token{
-		{
-			key:   "loki",
-			value: "something",
-		},
-		{
-			key:   "s.e",
-			value: "2231",
-		},
-		{
-			key:   "s",
-			value: "12",
-		},
-		{
-			key:   "12",
-			value: "3",
-		},
-		{
-			key:    "a",
-			value:  "1,2,3",
-			inside: '[',
-		},
-		{
-			key:    "b",
-			value:  "1",
-			inside: '[',
-		},
-		{
-			key:   "s",
-			value: "c",
-		},
-	}, tokens)
-	assert.NoError(t, err)
+	t.Parallel()
 
-	_, err = tokenize("empty=")
-	assert.EqualError(t, err, "key `empty=` with no value")
+	input := "loki=something,s.e=2231,s=12,12=3,a=[1,2,3],b=[1],s=c"
+	tokens, err := tokenize(input)
+	require.NoError(t, err)
+
+	expected := []token{
+		{key: "loki", value: "something"},
+		{key: "s.e", value: "2231"},
+		{key: "s", value: "12"},
+		{key: "12", value: "3"},
+		{key: "a", value: "1,2,3", inside: '['},
+		{key: "b", value: "1", inside: '['},
+		{key: "s", value: "c"},
+	}
+	assert.Equal(t, expected, tokens)
+}
+
+func TestTokenizerInvalid(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    string
+		errorMsg string
+	}{
+		{
+			name:     "empty value",
+			input:    "empty=",
+			errorMsg: "key `empty=` with no value",
+		},
+		{
+			name:     "unclosed array",
+			input:    "foo=[1,2,3",
+			errorMsg: "array value for key `foo` didn't end",
+		},
+		{
+			name:     "bad characters after array",
+			input:    "foo=[1,2,3]bar",
+			errorMsg: "there was no ',' after an array with key 'foo'",
+		},
+		{
+			name:     "empty key",
+			input:    ",foo=bar",
+			errorMsg: "key `` with no value",
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			_, err := tokenize(test.input)
+			require.EqualError(t, err, test.errorMsg)
+		})
+	}
 }
