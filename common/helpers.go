8573887978e2b15b3112d090785026e5cf4b46bb
ankur22 <ankur.agarwal@grafana.com>
2022-11-03 13:13:14 +0000

    Remove waitForEvent helper function
    This function is no longer being used.
    Also removing a todo comment which no longer is needed since these
    tests have been implemented in this PR.

--- common/helpers.go
+++ common/helpers.go
@@ -198,26 +198,6 @@ func createWaitForEventPredicateHandler(
 	return ch, evCancelFn
 }
 
-func waitForEvent(
-	ctx context.Context,
-	emitter EventEmitter, events []string,
-	predicateFn func(data any) bool,
-	timeout time.Duration,
-) (any, error) { //nolint:unparam
-	ch, evCancelFn := createWaitForEventHandler(ctx, emitter, events, predicateFn)
-	defer evCancelFn() // Remove event handler
-
-	select {
-	case <-ctx.Done():
-	case <-time.After(timeout):
-		return nil, fmt.Errorf("%w after %s", ErrTimedOut, timeout)
-	case evData := <-ch:
-		return evData, nil
-	}
-
-	return nil, nil
-}
-
 // panicOrSlowMo panics if err is not nil, otherwise applies slow motion.
 func panicOrSlowMo(ctx context.Context, err error) {
 	if err != nil {
