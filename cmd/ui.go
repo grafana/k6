6af92297dfed463cad060b4985db6b1eaab77ba5
Ivan Mirić <ivan.miric@grafana.com>
2022-12-06 17:15:37 +0100

    Show extension information in k6 run output

--- cmd/ui.go
+++ cmd/ui.go
@@ -17,6 +17,7 @@ import (
 
 	"gopkg.in/yaml.v3"
 
+	"go.k6.io/k6/ext"
 	"go.k6.io/k6/lib"
 	"go.k6.io/k6/lib/consts"
 	"go.k6.io/k6/output"
@@ -136,6 +137,12 @@ func printExecutionDescription(
 	buf := &strings.Builder{}
 	fmt.Fprintf(buf, "  execution: %s\n", valueColor.Sprint(execution))
 	fmt.Fprintf(buf, "     script: %s\n", valueColor.Sprint(filename))
+
+	if exts := ext.GetAll(); len(exts) > 0 {
+		extsDesc := getExtensionsDescription(exts, 13, 1)
+		fmt.Fprintf(buf, " extensions: %s\n",
+			valueColor.Sprint(strings.Join(extsDesc, "\n")))
+	}
 
 	var outputDescriptions []string
 	switch {
@@ -400,4 +407,18 @@ func yamlPrint(w io.Writer, v interface{}) error {
 		return fmt.Errorf("could flush the data to the output: %w", err)
 	}
 	return nil
+}
+
+func getExtensionsDescription(exts []*ext.Extension, leftPad, padStart int) []string {
+	extsDesc := make([]string, 0, len(exts))
+	for i, e := range exts {
+		desc := e.String()
+		extFmt := "%s"
+		if i >= padStart {
+			extFmt = fmt.Sprintf("%%%ds", len(desc)+leftPad)
+		}
+		extsDesc = append(extsDesc, fmt.Sprintf(extFmt, desc))
+	}
+
+	return extsDesc
 }
