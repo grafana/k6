e4d9ee51aa623e05463a2438d8fe4515f20f13a0
Ivan <2103732+codebien@users.noreply.github.com>
2022-12-15 13:07:57 +0100

    Moved stale marker in a dedicated pkg

---
+++ pkg/stale/stale.go
@@ -0,0 +1,20 @@
+// Package stale handles the staleness process.
+//
+// TODO: migrate here more logic dedicated to this topic
+// from the remote write package.
+package stale
+
+import "math"
+
+// Marker is the Prometheus Remote Write special value for marking
+// a time series as stale.
+//
+// Check https://www.robustperception.io/staleness-and-promql and
+// https://prometheus.io/docs/prometheus/latest/querying/basics/#staleness
+// for details about the Prometheus staleness markers.
+//
+// The value is the same used by the Prometheus package.
+// https://pkg.go.dev/github.com/prometheus/prometheus/pkg/value#pkg-constants
+//
+//nolint:gochecknoglobals
+var Marker = math.Float64frombits(0x7ff0000000000002)
