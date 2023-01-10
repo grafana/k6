b9ad43693e5c8de5397fdd727f4a363fecf62824
Mihail Stoykov <M.Stoikov@gmail.com>
2023-01-10 16:02:36 +0200

    Update goja
    fix #2842

--- vendor/github.com/dop251/goja/object_goreflect.go
+++ vendor/github.com/dop251/goja/object_goreflect.go
@@ -243,7 +243,7 @@ func (o *objectGoReflect) elemToValue(ev reflect.Value) (Value, reflectValueWrap
 		return ret, nil
 	}
 
-	for ev.Kind() == reflect.Interface {
+	if ev.Kind() == reflect.Interface {
 		ev = ev.Elem()
 	}
 
