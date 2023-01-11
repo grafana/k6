089cb08428dcc2d5d13f88658510385c3b9b19ef
İnanç Gümüş <inanc.gumus@grafana.com>
2023-01-11 13:33:08 +0300

    Convert screenshot.js to async/await

--- examples/screenshot.js
+++ examples/screenshot.js
@@ -6,18 +6,20 @@ export const options = {
   }
 }
 
-export default function() {
+export default async function() {
   const browser = chromium.launch({
     headless: __ENV.XK6_HEADLESS ? true : false,
   });
   const context = browser.newContext();
   const page = context.newPage();
-  page.goto('https://test.k6.io/').then(() => {
+  
+  try {
+    await page.goto('https://test.k6.io/');
     page.screenshot({ path: 'screenshot.png' });
     // TODO: Assert this somehow. Upload as CI artifact or just an external `ls`?
     // Maybe even do a fuzzy image comparison against a preset known good screenshot?
-  }).finally(() => {
+  } finally {
     page.close();
     browser.close();
-  });
+  }
 }
