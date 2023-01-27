bb627c0b16244fa41da3f2269941f70493c815e4
oleiade <theo@crevon.me>
2023-01-27 16:57:36 +0100

    Implement support for the sampling option

--- js/modules/k6/experimental/tracing/options.go
+++ js/modules/k6/experimental/tracing/options.go
@@ -3,19 +3,45 @@ package tracing
 import (
 	"errors"
 	"fmt"
+
+	"github.com/dop251/goja"
 )
 
 // options are the options that can be passed to the
 // tracing.instrumentHTTP() method.
 type options struct {
 	// Propagation is the propagation format to use for the tracer.
-	Propagator string `js:"propagator"`
+	Propagator string `json:"propagator"`
 
-	// Sampling is the sampling rate to use for the tracer.
-	Sampling *float64 `js:"sampling"`
+	// Sampling is the sampling rate to use for the
+	// tracer, expressed in percents within the
+	// bounds: 0.0 <= n <= 1.0.
+	Sampling float64 `json:"sampling"`
 
 	// Baggage is a map of baggage items to add to the tracer.
-	Baggage map[string]string `js:"baggage"`
+	Baggage map[string]string `json:"baggage"`
+}
+
+// defaultSamplingRate is the default sampling rate applied to options.
+const defaultSamplingRate float64 = 1.0
+
+// newOptions returns a new options object from the given goja.Value.
+//
+// Note that if the sampling field value is absent, or nullish, we'll
+// set it to the `defaultSamplingRate` value.
+func newOptions(rt *goja.Runtime, from goja.Value) (options, error) {
+	var opts options
+
+	if err := rt.ExportTo(from, &opts); err != nil {
+		return opts, fmt.Errorf("unable to parse options object; reason: %w", err)
+	}
+
+	fromSamplingValue := from.ToObject(rt).Get("sampling")
+	if fromSamplingValue == nil || isNullish(fromSamplingValue) {
+		opts.Sampling = defaultSamplingRate
+	}
+
+	return opts, nil
 }
 
 func (i *options) validate() error {
@@ -27,9 +53,8 @@ func (i *options) validate() error {
 		return fmt.Errorf("unknown propagator: %s", i.Propagator)
 	}
 
-	// TODO: implement sampling support
-	if i.Sampling != nil {
-		return errors.New("sampling is not yet supported")
+	if i.Sampling < 0.0 || i.Sampling > 1.0 {
+		return errors.New("sampling rate must be between 0.0 and 1.0")
 	}
 
 	// TODO: implement baggage support
