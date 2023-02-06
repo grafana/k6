grafana/k6

commit 2753196abee3c39dd633fd178e2406707f138d53

js: make certain only one copy of the exports is used per each module (#2773)
This is inline with ESM and works for source code modules, but not for
modules written in go as the code was never made to work with it.

This also lets (as the test suggests) change what `k6/http`.get is
across the VU just as it should be possible.

js/initcontext.go
@@ -45,13 +45,14 @@ type InitContext struct {
 	pwd         *url.URL
 
 	// Cache of loaded programs and files.
-	programs map[string]programWithSource
+	programs     map[string]programWithSource
+	exportsCache map[string]goja.Value
 
 	compatibilityMode lib.CompatibilityMode
 
 	logger logrus.FieldLogger
 
-	modules map[string]interface{}
+	moduleRegistry map[string]interface{}
 }
 
 // NewInitContext creates a new initcontext with the provided arguments
@@ -66,7 +67,8 @@ func NewInitContext(
 		programs:          make(map[string]programWithSource),
 		compatibilityMode: compatMode,
 		logger:            logger,
-		modules:           getJSModules(),
+		moduleRegistry:    getJSModules(),
+		exportsCache:      make(map[string]goja.Value),
 		moduleVUImpl: &moduleVUImpl{
 			ctx:     context.Background(),
 			runtime: rt,
@@ -92,14 +94,20 @@ func newBoundInitContext(base *InitContext, vuImpl *moduleVUImpl) *InitContext {
 
 		programs:          programs,
 		compatibilityMode: base.compatibilityMode,
+		exportsCache:      make(map[string]goja.Value),
 		logger:            base.logger,
-		modules:           base.modules,
+		moduleRegistry:    base.moduleRegistry,
 		moduleVUImpl:      vuImpl,
 	}
 }
 
 // Require is called when a module/file needs to be loaded by a script
-func (i *InitContext) Require(arg string) goja.Value {
+func (i *InitContext) Require(arg string) (export goja.Value) {
+	var ok bool
+	if export, ok = i.exportsCache[arg]; ok {
+		return export
+	}
+	defer func() { i.exportsCache[arg] = export }()
 	switch {
 	case arg == "k6", strings.HasPrefix(arg, "k6/"):
 		// Builtin or external modules ("k6", "k6/*", or "k6/x/*") are handled
@@ -144,7 +152,7 @@ func toESModuleExports(exp modules.Exports) interface{} {
 }
 
 func (i *InitContext) requireModule(name string) (goja.Value, error) {
-	mod, ok := i.modules[name]
+	mod, ok := i.moduleRegistry[name]
 	if !ok {
 		return nil, fmt.Errorf("unknown module: %s", name)
 	}

js/initcontext_test.go
@@ -687,3 +687,34 @@ export default function () {
 	// likely settings in the transpilers
 	require.Equal(t, "cool is cool\n\tat webpack:///./test1.ts:2:4(2)\n\tat r (webpack:///./test1.ts:5:4(3))\n\tat file:///script.js:4:2(4)\n\tat native\n", exception.String())
 }
+
+func TestImportModificationsAreConsistentBetweenFiles(t *testing.T) {
+	t.Parallel()
+	logger := testutils.NewLogger(t)
+	fs := afero.NewMemMapFs()
+	require.NoError(t, afero.WriteFile(fs, "/notk6.js", []byte(`export default {group}; function group() {}`), 0o644))
+	require.NoError(t, afero.WriteFile(fs, "/instrument.js", []byte(`
+    import k6 from "k6";
+    k6.newKey = 5;
+    k6.group = 3;
+
+    import notk6 from "./notk6.js";
+    notk6.group = 3;
+    notk6.newKey = 5;
+    `), 0o644))
+
+	b, err := getSimpleBundle(t, "/script.js", `
+    import k6 from "k6";
+    import notk6 from "./notk6.js";
+    import "./instrument.js";
+    if (k6.newKey != 5) { throw "k6.newKey is wrong "+ k6.newKey}
+    if (k6.group != 3) { throw "k6.group is wrong "+ k6.group}
+    if (notk6.newKey != 5) { throw "notk6.newKey is wrong "+ notk6.newKey}
+    if (notk6.group != 3) { throw "notk6.group is wrong "+ notk6.group}
+    export default () => { throw "this shouldn't be ran" }
+`, fs)
+	require.NoError(t, err, "bundle error")
+
+	_, err = b.Instantiate(logger, 0)
+	require.NoError(t, err)
+}

commit 63e2a5d4a9e6eedbacb279b0c1d7826adcb4e1e5

js: Cache scripts by absolute not relative paths
This fixes a regression introduced in 2753196ab where in all imports are
cached on the specifier that is provided for them. This is fine for k6
modules which happen to always have the same absolute names.

But source modules can have relative paths that resolve to different
absolute ones depending on where they are imported from.

Fixes #2902

js/initcontext.go
@@ -45,8 +45,9 @@ type InitContext struct {
 	pwd         *url.URL
 
 	// Cache of loaded programs and files.
-	programs     map[string]programWithSource
-	exportsCache map[string]goja.Value
+	programs map[string]programWithSource
+	// mege this and the above
+	k6ModulesCache map[string]goja.Value
 
 	compatibilityMode lib.CompatibilityMode
 
@@ -68,7 +69,7 @@ func NewInitContext(
 		compatibilityMode: compatMode,
 		logger:            logger,
 		moduleRegistry:    getJSModules(),
-		exportsCache:      make(map[string]goja.Value),
+		k6ModulesCache:    make(map[string]goja.Value),
 		moduleVUImpl: &moduleVUImpl{
 			// TODO: pass a real context as we did for https://github.com/grafana/k6/pull/2800,
 			// also see https://github.com/grafana/k6/issues/2804
@@ -96,7 +97,7 @@ func newBoundInitContext(base *InitContext, vuImpl *moduleVUImpl) *InitContext {
 
 		programs:          programs,
 		compatibilityMode: base.compatibilityMode,
-		exportsCache:      make(map[string]goja.Value),
+		k6ModulesCache:    make(map[string]goja.Value),
 		logger:            base.logger,
 		moduleRegistry:    base.moduleRegistry,
 		moduleVUImpl:      vuImpl,
@@ -105,13 +106,13 @@ func newBoundInitContext(base *InitContext, vuImpl *moduleVUImpl) *InitContext {
 
 // Require is called when a module/file needs to be loaded by a script
 func (i *InitContext) Require(arg string) (export goja.Value) {
-	var ok bool
-	if export, ok = i.exportsCache[arg]; ok {
-		return export
-	}
-	defer func() { i.exportsCache[arg] = export }()
 	switch {
 	case arg == "k6", strings.HasPrefix(arg, "k6/"):
+		var ok bool
+		if export, ok = i.k6ModulesCache[arg]; ok {
+			return export
+		}
+		defer func() { i.k6ModulesCache[arg] = export }()
 		// Builtin or external modules ("k6", "k6/*", or "k6/x/*") are handled
 		// specially, as they don't exist on the filesystem. This intentionally
 		// shadows attempts to name your own modules this.

js/initcontext_test.go
@@ -708,3 +708,26 @@ func TestImportModificationsAreConsistentBetweenFiles(t *testing.T) {
 	_, err = b.Instantiate(context.Background(), 0)
 	require.NoError(t, err)
 }
+
+func TestCacheAbsolutePathsNotRelative(t *testing.T) {
+	t.Parallel()
+	fs := afero.NewMemMapFs()
+	require.NoError(t, afero.WriteFile(fs, "/a/interesting.js", []byte(`export default "a.interesting"`), 0o644))
+	require.NoError(t, afero.WriteFile(fs, "/a/import.js", []byte(`export { default as default} from "./interesting.js"`), 0o644))
+
+	require.NoError(t, afero.WriteFile(fs, "/b/interesting.js", []byte(`export default "b.interesting"`), 0o644))
+	require.NoError(t, afero.WriteFile(fs, "/b/import.js", []byte(`export { default as default} from "./interesting.js"`), 0o644))
+
+	b, err := getSimpleBundle(t, "/script.js", `
+    import a from "/a/import.js"
+    import b from "/b/import.js"
+    if (a != "a.interesting") { throw `+"`"+`'a' has wrong value "${a}" should be "a.interesting"`+"`"+`}
+
+    if (b != "b.interesting") { throw `+"`"+`'b' has wrong value "${b}" should be "b.interesting"`+"`"+`}
+    export default () => { throw "this shouldn't be ran" }
+`, fs)
+	require.NoError(t, err, "bundle error")
+
+	_, err = b.Instantiate(context.Background(), 0)
+	require.NoError(t, err)
+}