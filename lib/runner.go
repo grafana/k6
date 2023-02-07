5d8fd12eb8943575827f414816c6929acb6e80ad
Ivan <2103732+codebien@users.noreply.github.com>
2023-02-07 12:58:10 +0100

    js: Migrate to ObservedMetric

--- lib/runner.go
+++ lib/runner.go
@@ -93,7 +93,7 @@ type UIState struct {
 
 // Summary contains all of the data the summary handler gets.
 type Summary struct {
-	Metrics         map[string]*metrics.Metric
+	Metrics         map[string]metrics.ObservedMetric
 	RootGroup       *Group
 	TestRunDuration time.Duration // TODO: use lib.ExecutionState-based interface instead?
 	NoColor         bool          // TODO: drop this when noColor is part of the (runtime) options
