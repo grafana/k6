338515f7fe5e42a711f855a0602ac3ee2ea21068
Azanul <azanulhaque@gmail.com>
2023-02-08 13:35:03 +0000

    test for rfc3339 nano ADD

--- output/csv/output_test.go
+++ output/csv/output_test.go
@@ -179,6 +179,27 @@ func TestSampleToRow(t *testing.T) {
 			ignoredTags: []string{"tag4", "tag6"},
 			timeFormat:  "unix_milli",
 		},
+		{
+			testname: "Two res tags, two ignored, with RFC3339Nano timestamp",
+			sample: &metrics.Sample{
+				TimeSeries: metrics.TimeSeries{
+					Metric: testMetric,
+					Tags: registry.RootTagSet().WithTagsFromMap(map[string]string{
+						"tag1": "val1",
+						"tag2": "val2",
+						"tag3": "val3",
+						"tag4": "val4",
+						"tag5": "val5",
+						"tag6": "val6",
+					}),
+				},
+				Time:  time.Unix(1562324644, 0),
+				Value: 1,
+			},
+			resTags:     []string{"tag1", "tag3"},
+			ignoredTags: []string{"tag4", "tag6"},
+			timeFormat:  "rfc3339_nano",
+		},
 	}
 
 	expected := []struct {
@@ -253,6 +274,19 @@ func TestSampleToRow(t *testing.T) {
 			baseRow: []string{
 				"my_metric",
 				"1562324644006",
+				"1.000000",
+				"val1",
+				"val3",
+			},
+			extraRow: []string{
+				"tag2=val2",
+				"tag5=val5",
+			},
+		},
+		{
+			baseRow: []string{
+				"my_metric",
+				time.Unix(1562324644, 0).Format(time.RFC3339Nano),
 				"1.000000",
 				"val1",
 				"val3",
