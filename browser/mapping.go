bcd9caa7e823e8bc9d2f0324f16407cef9ad546c
İnanç Gümüş <inanc.gumus@grafana.com>
2023-02-15 14:08:04 +0300

    Move moduleVU to modulevu.go
    Since we'll add more functionality to the type, let's move it to its own
    file to keep things tidier.

--- browser/mapping.go
+++ browser/mapping.go
@@ -1,7 +1,6 @@
 package browser
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/dop251/goja"
@@ -11,26 +10,7 @@ import (
 	"github.com/grafana/xk6-browser/k6ext"
 
 	k6common "go.k6.io/k6/js/common"
-	k6modules "go.k6.io/k6/js/modules"
 )
-
-// moduleVU carries module specific VU information.
-//
-// Currently, it is used to carry the VU object to the
-// inner objects and promises.
-type moduleVU struct {
-	k6modules.VU
-}
-
-func (vu moduleVU) Context() context.Context {
-	// promises and inner objects need the VU object to be
-	// able to use k6-core specific functionality.
-	//
-	// We should not cache the context (especially the init
-	// context from the vu that is received from k6 in
-	// NewModuleInstance).
-	return k6ext.WithVU(vu.VU.Context(), vu.VU)
-}
 
 // mapping is a type of mapping between our API (api/) and the JS
 // module. It acts like a bridge and allows adding wildcard methods
