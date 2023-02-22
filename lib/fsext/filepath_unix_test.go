80431e600dda3e5875dc74ae66337b4f182de00a
Oleg Bespalov <oleg.bespalov@grafana.com>
2023-02-22 18:07:31 +0100

    Restore the behaviour of the `filepath.Join` that was before go1.20

---
+++ lib/fsext/filepath_unix_test.go
@@ -0,0 +1,74 @@
+//go:build unix
+
+package fsext_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.k6.io/k6/lib/fsext"
+)
+
+func TestJoinFilePath(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		b string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "join root and some catalog",
+			args: args{
+				b: "/",
+				p: "test",
+			},
+			want: "/test",
+		},
+		{
+			name: "join root and some catalog with leading slash",
+			args: args{
+				b: "/",
+				p: "/test",
+			},
+			want: "/test",
+		},
+		{
+			name: "join root and some catalog with several leading slash",
+			args: args{
+				b: "/",
+				p: "//test",
+			},
+			want: "/test",
+		},
+		{
+			name: "join catalog and some other catalog",
+			args: args{
+				b: "/path/to",
+				p: "test",
+			},
+			want: "/path/to/test",
+		},
+		{
+			name: "join catalog and some other catalog with leading slash",
+			args: args{
+				b: "/path/to",
+				p: "/test",
+			},
+			want: "/path/to/test",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, fsext.JoinFilePath(tt.args.b, tt.args.p))
+		})
+	}
+}
