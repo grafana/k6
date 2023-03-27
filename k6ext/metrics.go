357610db2427a686513f506dc062cfb84e9506b7
ankur22 <ankur.agarwal@grafana.com>
2023-03-27 15:03:23 +0100

    Add the web vital metrics to the metrics registry
    This adds the six web vitals, as well as the three ratings for each.
    When a web vital metric is send from the JS code to k6-browser via
    the binding, it will need to extract the metric name and the rating.
    
    The rating can be good, needs-improvement or poor.

--- k6ext/metrics.go
+++ k6ext/metrics.go
@@ -1,6 +1,19 @@
 package k6ext
 
-import k6metrics "go.k6.io/k6/metrics"
+import (
+	"fmt"
+
+	k6metrics "go.k6.io/k6/metrics"
+)
+
+const (
+	webVitalFID  = "FID"
+	webVitalTTFB = "TTFB"
+	webVitalLCP  = "LCP"
+	webVitalCLS  = "CLS"
+	webVitalINP  = "INP"
+	webVitalFCP  = "FCP"
+)
 
 // CustomMetrics are the custom k6 metrics used by xk6-browser.
 type CustomMetrics struct {
@@ -9,11 +22,39 @@ type CustomMetrics struct {
 	BrowserFirstContentfulPaint *k6metrics.Metric
 	BrowserFirstMeaningfulPaint *k6metrics.Metric
 	BrowserLoaded               *k6metrics.Metric
+
+	WebVitals map[string]*k6metrics.Metric
 }
 
 // RegisterCustomMetrics creates and registers our custom metrics with the k6
 // VU Registry and returns our internal struct pointer.
 func RegisterCustomMetrics(registry *k6metrics.Registry) *CustomMetrics {
+	wvs := map[string]string{
+		webVitalFID:  "webvital_first_input_delay",
+		webVitalTTFB: "webvital_time_to_first_byte",
+		webVitalLCP:  "webvital_largest_content_paint",
+		webVitalCLS:  "webvital_cumulative_layout_shift",
+		webVitalINP:  "webvital_interaction_to_next_paint",
+		webVitalFCP:  "webvital_first_contentful_paint",
+	}
+	webVitals := make(map[string]*k6metrics.Metric)
+
+	for k, v := range wvs {
+		t := k6metrics.Time
+		if k == webVitalCLS {
+			t = k6metrics.Default
+		}
+
+		webVitals[k] = registry.MustNewMetric(v, k6metrics.Trend, t)
+
+		webVitals[ConcatWebVitalNameRating(k, "good")] = registry.MustNewMetric(
+			v+"_good", k6metrics.Counter)
+		webVitals[ConcatWebVitalNameRating(k, "needs-improvement")] = registry.MustNewMetric(
+			v+"_needs_improvement", k6metrics.Counter)
+		webVitals[ConcatWebVitalNameRating(k, "poor")] = registry.MustNewMetric(
+			v+"_poor", k6metrics.Counter)
+	}
+
 	return &CustomMetrics{
 		BrowserDOMContentLoaded: registry.MustNewMetric(
 			"browser_dom_content_loaded", k6metrics.Trend, k6metrics.Time),
@@ -25,5 +66,14 @@ func RegisterCustomMetrics(registry *k6metrics.Registry) *CustomMetrics {
 			"browser_first_meaningful_paint", k6metrics.Trend, k6metrics.Time),
 		BrowserLoaded: registry.MustNewMetric(
 			"browser_loaded", k6metrics.Trend, k6metrics.Time),
+		WebVitals: webVitals,
 	}
+}
+
+// ConcatWebVitalNameRating can be used
+// to create the correct metric key name
+// to retrieve the corresponding metric
+// from the registry.
+func ConcatWebVitalNameRating(name, rating string) string {
+	return fmt.Sprintf("%s:%s", name, rating)
 }
