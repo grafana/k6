3cd2fd56622ee39e3c37e8bad59382d664df3989
ankur22 <ankur.agarwal@grafana.com>
2023-05-23 10:17:19 +0100

    Add remoteRegistry in module
    We're moving the remote registry back into the module package since it
    is only ever used there.

--- module/registry.go
+++ module/registry.go
@@ -1,6 +1,13 @@
 package module
 
-import "sync"
+import (
+	"crypto/rand"
+	"math/big"
+	"strings"
+	"sync"
+
+	"github.com/grafana/xk6-browser/env"
+)
 
 // pidRegistry keeps track of the launched browser process IDs.
 type pidRegistry struct {
@@ -25,4 +32,61 @@ func (r *pidRegistry) Pids() []int {
 	copy(pids, r.ids)
 
 	return pids
+}
+
+// remoteRegistry contains the details of the remote web browsers.
+// At the moment it's the WS URLs.
+type remoteRegistry struct {
+	isRemote bool
+	wsURLs   []string
+}
+
+// newRemoteRegistry will create a new RemoteRegistry. This will
+// parse the K6_BROWSER_WS_URL env var to retrieve the defined
+// list of WS URLs.
+//
+// K6_BROWSER_WS_URL can be defined as a single WS URL or a
+// comma separated list of URLs.
+func newRemoteRegistry(envLookup env.LookupFunc) *remoteRegistry {
+	r := &remoteRegistry{}
+
+	wsURL, isRemote := envLookup("K6_BROWSER_WS_URL")
+	if !isRemote {
+		return r
+	}
+
+	if !strings.ContainsRune(wsURL, ',') {
+		r.isRemote = true
+		r.wsURLs = []string{wsURL}
+		return r
+	}
+
+	// If last parts element is a void string,
+	// because WS URL contained an ending comma,
+	// remove it
+	parts := strings.Split(wsURL, ",")
+	if parts[len(parts)-1] == "" {
+		parts = parts[:len(parts)-1]
+	}
+
+	r.isRemote = true
+	r.wsURLs = parts
+
+	return r
+}
+
+// isRemoteBrowser returns a WS URL and true when a WS URL is defined,
+// otherwise it returns an empty string and false. If more than one
+// WS URL was registered in newRemoteRegistry, a randomly chosen URL from
+// the list in a round-robin fashion is selected and returned.
+func (r *remoteRegistry) isRemoteBrowser() (string, bool) {
+	if !r.isRemote {
+		return "", false
+	}
+
+	// Choose a random WS URL from the provided list
+	i, _ := rand.Int(rand.Reader, big.NewInt(int64(len(r.wsURLs))))
+	wsURL := r.wsURLs[i.Int64()]
+
+	return wsURL, true
 }
--- module/registry_test.go
+++ module/registry_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/xk6-browser/env"
 )
 
 func TestPidRegistry(t *testing.T) {
@@ -27,4 +30,79 @@ func TestPidRegistry(t *testing.T) {
 	got := p.Pids()
 
 	assert.ElementsMatch(t, expected, got)
+}
+
+func TestIsRemoteBrowser(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name           string
+		envLookup      env.LookupFunc
+		expIsRemote    bool
+		expValidWSURLs []string
+	}{
+		{
+			name: "browser is not remote",
+			envLookup: func(key string) (string, bool) {
+				return "", false
+			},
+			expIsRemote: false,
+		},
+		{
+			name: "single WS URL",
+			envLookup: func(key string) (string, bool) {
+				return "WS_URL", true
+			},
+			expIsRemote:    true,
+			expValidWSURLs: []string{"WS_URL"},
+		},
+		{
+			name: "multiple WS URL",
+			envLookup: func(key string) (string, bool) {
+				return "WS_URL_1,WS_URL_2,WS_URL_3", true
+			},
+			expIsRemote:    true,
+			expValidWSURLs: []string{"WS_URL_1", "WS_URL_2", "WS_URL_3"},
+		},
+		{
+			name: "ending comma is handled",
+			envLookup: func(key string) (string, bool) {
+				return "WS_URL_1,WS_URL_2,", true
+			},
+			expIsRemote:    true,
+			expValidWSURLs: []string{"WS_URL_1", "WS_URL_2"},
+		},
+		{
+			name: "void string does not panic",
+			envLookup: func(key string) (string, bool) {
+				return "", true
+			},
+			expIsRemote:    true,
+			expValidWSURLs: []string{""},
+		},
+		{
+			name: "comma does not panic",
+			envLookup: func(key string) (string, bool) {
+				return ",", true
+			},
+			expIsRemote:    true,
+			expValidWSURLs: []string{""},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			rr := newRemoteRegistry(tc.envLookup)
+			wsURL, isRemote := rr.isRemoteBrowser()
+
+			require.Equal(t, tc.expIsRemote, isRemote)
+			if isRemote {
+				require.Contains(t, tc.expValidWSURLs, wsURL)
+			}
+		})
+	}
 }
