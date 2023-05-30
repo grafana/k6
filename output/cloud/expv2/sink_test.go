127efacc84e9516daac82a5027ecef342b973fce
Ivan <2103732+codebien@users.noreply.github.com>
2023-05-30 17:05:15 +0200

    Tests for add methods

--- output/cloud/expv2/sink_test.go
+++ output/cloud/expv2/sink_test.go
@@ -22,3 +22,55 @@ func TestNewSink(t *testing.T) {
 		assert.Equal(t, tc.exp, newMetricValue(tc.mt))
 	}
 }
+
+func TestCounterAdd(t *testing.T) {
+	c := counter{}
+	c.Add(2.3)
+	assert.Equal(t, 2.3, c.Sum)
+
+	c.Add(1.78)
+	assert.Equal(t, 4.08, c.Sum)
+}
+
+func TestGaugeAdd(t *testing.T) {
+	g := gauge{}
+	g.Add(28)
+
+	exp := gauge{
+		Last:   28,
+		Sum:    28,
+		Min:    28,
+		Max:    28,
+		Avg:    28,
+		Count:  1,
+		minSet: true,
+	}
+	assert.Equal(t, exp, g)
+
+	g.Add(8.28)
+	exp = gauge{
+		Last:   8.28,
+		Sum:    36.28,
+		Min:    8.28,
+		Max:    28,
+		Avg:    18.14,
+		Count:  2,
+		minSet: true,
+	}
+	assert.Equal(t, exp, g)
+}
+
+func TestRateAdd(t *testing.T) {
+	r := rate{}
+	r.Add(91)
+
+	exp := rate{
+		NonZeroCount: 1,
+		Total:        1,
+	}
+	assert.Equal(t, exp, r)
+
+	r.Add(0)
+	exp.Total = 2
+	assert.Equal(t, exp, r)
+}
