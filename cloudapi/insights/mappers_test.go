41cbe211b659b1274abe98025deab9a56ec922f0
Lukasz Gut <blinku.dev@gmail.com>
2023-06-01 13:32:52 +0200

    Fix mappers_test.go

--- cloudapi/insights/mappers_test.go
+++ cloudapi/insights/mappers_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.k6.io/k6/cloudapi/insights/proto/v1/ingester"
+	"go.k6.io/k6/cloudapi/insights/proto/v1/k6"
 )
 
 func Test_newBatchCreateRequestMetadatasRequest_CorrectlyMapsDomainTypeToProtoDefinition(t *testing.T) {
@@ -36,26 +37,26 @@ func Test_newBatchCreateRequestMetadatasRequest_CorrectlyMapsDomainTypeToProtoDe
 	// Then
 	expected := []*ingester.CreateRequestMetadataRequest{
 		{
-			RequestMetadata: &ingester.RequestMetadata{
+			RequestMetadata: &k6.RequestMetadata{
 				TraceID:           "test-trace-id-1",
 				StartTimeUnixNano: time.Unix(9, 0).UnixNano(),
 				EndTimeUnixNano:   time.Unix(10, 0).UnixNano(),
-				TestRunLabels:     &ingester.TestRunLabels{ID: 1337, Scenario: "test-scenario-1", Group: "test-group-1"},
-				ProtocolLabels: &ingester.RequestMetadata_HTTPLabels{
-					HTTPLabels: &ingester.HTTPLabels{
+				TestRunLabels:     &k6.TestRunLabels{ID: 1337, Scenario: "test-scenario-1", Group: "test-group-1"},
+				ProtocolLabels: &k6.RequestMetadata_HTTPLabels{
+					HTTPLabels: &k6.HTTPLabels{
 						Url: "test-url-1", Method: "test-method-1", StatusCode: 200,
 					},
 				},
 			},
 		},
 		{
-			RequestMetadata: &ingester.RequestMetadata{
+			RequestMetadata: &k6.RequestMetadata{
 				TraceID:           "test-trace-id-2",
 				StartTimeUnixNano: time.Unix(19, 0).UnixNano(),
 				EndTimeUnixNano:   time.Unix(20, 0).UnixNano(),
-				TestRunLabels:     &ingester.TestRunLabels{ID: 1337, Scenario: "test-scenario-2", Group: "test-group-2"},
-				ProtocolLabels: &ingester.RequestMetadata_HTTPLabels{
-					HTTPLabels: &ingester.HTTPLabels{
+				TestRunLabels:     &k6.TestRunLabels{ID: 1337, Scenario: "test-scenario-2", Group: "test-group-2"},
+				ProtocolLabels: &k6.RequestMetadata_HTTPLabels{
+					HTTPLabels: &k6.HTTPLabels{
 						Url: "test-url-2", Method: "test-method-2", StatusCode: 401,
 					},
 				},
