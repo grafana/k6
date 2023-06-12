1509fdec40ccb2a72183c0e626e9688c8dae682d
Lukasz Gut <blinku.dev@gmail.com>
2023-06-12 17:19:30 +0200

    Remove `gen.sh`

--- cloudapi/insights/proto/gen.go
+++ cloudapi/insights/proto/gen.go
@@ -2,4 +2,9 @@
 // by the k6 Insights.
 package proto
 
-//go:generate sh gen.sh
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./v1/common/common.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./v1/ingester/ingester.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./v1/k6/labels.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./v1/k6/request_metadata.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./v1/trace/labels.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./v1/trace/span.proto
