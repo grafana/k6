aa2b8c1dc04c4f5805962cc7df2a0ca626561f19
codebien <2103732+codebien@users.noreply.github.com>
2023-08-04 14:30:35 +0200

    Bump the k6 version

--- lib/consts/consts.go
+++ lib/consts/consts.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Version contains the current semantic version of k6.
-const Version = "0.45.0"
+const Version = "0.45.1"
 
 // VersionDetails can be set externally as part of the build process
 var VersionDetails = "" //nolint:gochecknoglobals
