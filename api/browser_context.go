9cde1cc9be28d5942c39dd939ad5d604acfe47fd
İnanç Gümüş <inanc.gumus@grafana.com>
2023-08-22 14:14:45 +0300

    Add api.Cookie type
    We can use this type in our code and tests and pass to clients. The
    network.Cookie contains an internal fields and we don't want to use it.
    
    This type provides type safety and representation. Instead of a map type
    which its fields are of the same type, not type-safe, and not useful or
    effective for representing a concept like a cookie.
    
    Declaring this in the api package. So we can have less verbosity in the
    rest of the codebase. The downside is the api package dependency.
    If we declared this in the common package, there would be circular
    dependency issues.

--- api/browser_context.go
+++ api/browser_context.go
@@ -36,3 +36,37 @@ type BrowserContext interface {
 	Unroute(url goja.Value, handler goja.Callable)
 	WaitForEvent(event string, optsOrPredicate goja.Value) any
 }
+
+// Cookie represents a browser cookie.
+//
+// https://datatracker.ietf.org/doc/html/rfc6265.
+type Cookie struct {
+	Name     string         `json:"name"`     // Cookie name.
+	Value    string         `json:"value"`    // Cookie value.
+	Domain   string         `json:"domain"`   // Cookie domain.
+	Path     string         `json:"path"`     // Cookie path.
+	Expires  int64          `json:"expires"`  // Cookie expiration date as the number of seconds since the UNIX epoch.
+	HTTPOnly bool           `json:"httpOnly"` // True if cookie is http-only.
+	Secure   bool           `json:"secure"`   // True if cookie is secure.
+	SameSite CookieSameSite `json:"sameSite"` // Cookie SameSite type.
+}
+
+// CookieSameSite represents the cookie's 'SameSite' status.
+//
+// https://tools.ietf.org/html/draft-west-first-party-cookies.
+type CookieSameSite string
+
+const (
+	// CookieSameSiteStrict sets the cookie to be sent only in a first-party
+	// context and not be sent along with requests initiated by third party
+	// websites.
+	CookieSameSiteStrict CookieSameSite = "Strict"
+
+	// CookieSameSiteLax sets the cookie to be sent along with "same-site"
+	// requests, and with "cross-site" top-level navigations.
+	CookieSameSiteLax CookieSameSite = "Lax"
+
+	// CookieSameSiteNone sets the cookie to be sent in all contexts, i.e
+	// potentially insecure third-party requests.
+	CookieSameSiteNone CookieSameSite = "None"
+)
