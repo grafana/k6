d91cecbce43c009acaad406cf6ba39aaed4d473c
codebien <2103732+codebien@users.noreply.github.com>
2023-10-03 17:32:15 +0200

    Drop the deprecated convert command

--- cmd/root.go
+++ cmd/root.go
@@ -57,7 +57,7 @@ func newRootCommand(gs *state.GlobalState) *rootCommand {
 	rootCmd.SetIn(gs.Stdin)
 
 	subCommands := []func(*state.GlobalState) *cobra.Command{
-		getCmdArchive, getCmdCloud, getCmdConvert, getCmdInspect,
+		getCmdArchive, getCmdCloud, getCmdInspect,
 		getCmdLogin, getCmdPause, getCmdResume, getCmdScale, getCmdRun,
 		getCmdStats, getCmdStatus, getCmdVersion,
 	}
