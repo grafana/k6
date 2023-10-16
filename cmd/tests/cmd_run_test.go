grafana/k6

commit 7b5aaaa0efc5511b52f01a2342121753352fa355

Fix js lint (#3393)

js/runner.go
@@ -44,6 +44,7 @@ var _ lib.Runner = &Runner{}
 //nolint:gochecknoglobals
 var nameToCertWarning sync.Once
 
+// Runner implements [lib.Runner] and is used to run js tests
 type Runner struct {
 	Bundle       *Bundle
 	preInitState *lib.TestPreInitState
@@ -110,6 +111,8 @@ func NewFromBundle(piState *lib.TestPreInitState, b *Bundle) (*Runner, error) {
 	return r, err
 }
 
+// MakeArchive creates an Archive of the runner. There should be a corresponding NewFromArchive() function
+// that will restore the runner from the archive.
 func (r *Runner) MakeArchive() *lib.Archive {
 	return r.Bundle.makeArchive()
 }
@@ -348,10 +351,12 @@ func (r *Runner) Teardown(ctx context.Context, out chan<- metrics.SampleContaine
 	return err
 }
 
+// GetDefaultGroup returns the default (root) Group.
 func (r *Runner) GetDefaultGroup() *lib.Group {
 	return r.defaultGroup
 }
 
+// GetOptions returns the currently calculated [lib.Options] for the given Runner.
 func (r *Runner) GetOptions() lib.Options {
 	return r.Bundle.Options
 }
@@ -371,7 +376,7 @@ func (r *Runner) HandleSummary(ctx context.Context, summary *lib.Summary) (map[s
 	defer close(out)
 
 	go func() { // discard all metrics
-		for range out {
+		for range out { //nolint:revive
 		}
 	}()
 
@@ -414,16 +419,8 @@ func (r *Runner) HandleSummary(ctx context.Context, summary *lib.Summary) (map[s
 	}
 	rawResult, _, _, err := vu.runFn(summaryCtx, false, handleSummaryWrapper, nil, wrapperArgs...)
 
-	// TODO: refactor the whole JS runner to avoid copy-pasting these complicated bits...
-	// deadline is reached so we have timeouted but this might've not been registered correctly
-	if deadline, ok := summaryCtx.Deadline(); ok && time.Now().After(deadline) {
-		// we could have an error that is not context.Canceled in which case we should return it instead
-		if err, ok := err.(*goja.InterruptedError); ok && rawResult != nil && err.Value() != context.Canceled {
-			// TODO: silence this error?
-			return nil, err
-		}
-		// otherwise we have timeouted
-		return nil, newTimeoutError(consts.HandleSummaryFn, r.getTimeoutFor(consts.HandleSummaryFn))
+	if deadlineError := r.checkDeadline(summaryCtx, consts.HandleSummaryFn, rawResult, err); deadlineError != nil {
+		return nil, err
 	}
 
 	if err != nil {
@@ -432,6 +429,23 @@ func (r *Runner) HandleSummary(ctx context.Context, summary *lib.Summary) (map[s
 	return getSummaryResult(rawResult)
 }
 
+func (r *Runner) checkDeadline(ctx context.Context, name string, result goja.Value, err error) error {
+	if deadline, ok := ctx.Deadline(); !(ok && time.Now().After(deadline)) {
+		return nil
+	}
+
+	// deadline is reached so we have timeouted but this might've not been registered correctly
+	// we could have an error that is not context.Canceled in which case we should return it instead
+	//nolint:errorlint
+	if err, ok := err.(*goja.InterruptedError); ok && result != nil && err.Value() != context.Canceled {
+		// TODO: silence this error?
+		return err
+	}
+	// otherwise we have timeouted
+	return newTimeoutError(name, r.getTimeoutFor(name))
+}
+
+// SetOptions sets the test Options to the provided data and makes necessary changes to the Runner.
 func (r *Runner) SetOptions(opts lib.Options) error {
 	r.Bundle.Options = opts
 	r.RPSLimit = nil
@@ -558,15 +572,8 @@ func (r *Runner) runPart(
 	vu.state.Group = group
 	v, _, _, err := vu.runFn(ctx, false, fn, nil, vu.Runtime.ToValue(arg))
 
-	// deadline is reached so we have timeouted but this might've not been registered correctly
-	if deadline, ok := ctx.Deadline(); ok && time.Now().After(deadline) {
-		// we could have an error that is not context.Canceled in which case we should return it instead
-		if err, ok := err.(*goja.InterruptedError); ok && v != nil && err.Value() != context.Canceled {
-			// TODO: silence this error?
-			return v, err
-		}
-		// otherwise we have timeouted
-		return v, newTimeoutError(name, r.getTimeoutFor(name))
+	if deadlineError := r.checkDeadline(ctx, name, v, err); deadlineError != nil {
+		return nil, err
 	}
 
 	return v, err
@@ -604,6 +611,7 @@ func (r *Runner) getTimeoutFor(stage string) time.Duration {
 	return d
 }
 
+// VU implements the [lib.VU] interface for the js [Runner].
 type VU struct {
 	BundleInstance
 
@@ -666,6 +674,7 @@ func (u *VU) Activate(params *lib.VUActivationParams) lib.ActiveVU {
 	for key, value := range params.Env {
 		env[key] = value
 	}
+	//nolint:errcheck,gosec // see https://github.com/grafana/k6/issues/1722#issuecomment-1761173634
 	u.Runtime.Set("__ENV", env)
 
 	opts := u.Runner.Bundle.Options
@@ -775,16 +784,7 @@ func (u *ActiveVU) RunOnce() error {
 		ScenarioName: u.scenarioName,
 	}
 
-	emitAndWaitEvent := func(evt *event.Event) {
-		waitDone := u.moduleVUImpl.events.local.Emit(evt)
-		waitCtx, waitCancel := context.WithTimeout(u.RunContext, 30*time.Minute)
-		defer waitCancel()
-		if werr := waitDone(waitCtx); werr != nil {
-			u.state.Logger.WithError(werr).Warn()
-		}
-	}
-
-	emitAndWaitEvent(&event.Event{Type: event.IterStart, Data: eventIterData})
+	u.emitAndWaitEvent(&event.Event{Type: event.IterStart, Data: eventIterData})
 
 	// Call the exported function.
 	_, isFullIteration, totalTime, err := u.runFn(ctx, true, fn, cancel, u.setupData)
@@ -799,7 +799,7 @@ func (u *ActiveVU) RunOnce() error {
 		eventIterData.Error = err
 	}
 
-	emitAndWaitEvent(&event.Event{Type: event.IterEnd, Data: eventIterData})
+	u.emitAndWaitEvent(&event.Event{Type: event.IterEnd, Data: eventIterData})
 
 	// If MinIterationDuration is specified and the iteration wasn't canceled
 	// and was less than it, sleep for the remainder
@@ -814,6 +814,15 @@ func (u *ActiveVU) RunOnce() error {
 	}
 
 	return err
+}
+
+func (u *ActiveVU) emitAndWaitEvent(evt *event.Event) {
+	waitDone := u.moduleVUImpl.events.local.Emit(evt)
+	waitCtx, waitCancel := context.WithTimeout(u.RunContext, 30*time.Minute)
+	defer waitCancel()
+	if werr := waitDone(waitCtx); werr != nil {
+		u.state.Logger.WithError(werr).Warn()
+	}
 }
 
 func (u *VU) getExported(name string) goja.Value {
@@ -866,7 +875,7 @@ func (u *VU) runFn(
 	endTime := time.Now()
 	var exception *goja.Exception
 	if errors.As(err, &exception) {
-		err = &scriptException{inner: exception}
+		err = &scriptExceptionError{inner: exception}
 	}
 
 	if u.Runner.Bundle.Options.NoVUConnectionReuse.Bool {
@@ -897,7 +906,7 @@ func (u *ActiveVU) incrIteration() {
 	}
 }
 
-type scriptException struct {
+type scriptExceptionError struct {
 	inner *goja.Exception
 }
 
@@ -906,37 +915,29 @@ var _ interface {
 	errext.HasExitCode
 	errext.HasHint
 	errext.HasAbortReason
-} = &scriptException{}
+} = &scriptExceptionError{}
 
-func (s *scriptException) Error() string {
+func (s *scriptExceptionError) Error() string {
 	// this calls String instead of error so that by default if it's printed to print the stacktrace
 	return s.inner.String()
 }
 
-func (s *scriptException) StackTrace() string {
+func (s *scriptExceptionError) StackTrace() string {
 	return s.inner.String()
 }
 
-func (s *scriptException) Unwrap() error {
+func (s *scriptExceptionError) Unwrap() error {
 	return s.inner
 }
 
-func (s *scriptException) Hint() string {
+func (s *scriptExceptionError) Hint() string {
 	return "script exception"
 }
 
-func (s *scriptException) AbortReason() errext.AbortReason {
+func (s *scriptExceptionError) AbortReason() errext.AbortReason {
 	return errext.AbortedByScriptError
 }
 
-func (s *scriptException) ExitCode() exitcodes.ExitCode {
+func (s *scriptExceptionError) ExitCode() exitcodes.ExitCode {
 	return exitcodes.ScriptException
-}
-
-func copyStringMap(m map[string]string) map[string]string {
-	clone := make(map[string]string, len(m))
-	for ktag, vtag := range m {
-		clone[ktag] = vtag
-	}
-	return clone
 }

js/bundle.go
@@ -333,7 +333,7 @@ func (b *Bundle) instantiate(vuImpl *moduleVUImpl, vuID uint64) (*goja.Object, e
 	if err != nil {
 		var exception *goja.Exception
 		if errors.As(err, &exception) {
-			err = &scriptException{inner: exception}
+			err = &scriptExceptionError{inner: exception}
 		}
 		return nil, err
 	}

js/bundle_test.go
@@ -89,7 +89,7 @@ func TestNewBundle(t *testing.T) {
 	t.Run("Error", func(t *testing.T) {
 		t.Parallel()
 		_, err := getSimpleBundle(t, "/script.js", `throw new Error("aaaa");`)
-		exception := new(scriptException)
+		exception := new(scriptExceptionError)
 		require.ErrorAs(t, err, &exception)
 		require.EqualError(t, err, "Error: aaaa\n\tat file:///script.js:2:7(3)\n")
 	})
@@ -453,7 +453,7 @@ func TestNewBundle(t *testing.T) {
 			require.Len(t, entries, 1)
 			assert.Equal(t, logrus.WarnLevel, entries[0].Level)
 			assert.Contains(t, entries[0].Message, "There were unknown fields")
-			assert.Contains(t, entries[0].Data["error"].(error).Error(), "unknown field \"something\"")
+			assert.Contains(t, entries[0].Data["error"].(error).Error(), "unknown field \"something\"") //nolint:forcetypeassert
 		})
 	})
 }
@@ -579,6 +579,7 @@ func TestNewBundleFromArchive(t *testing.T) {
 }
 
 func TestOpen(t *testing.T) {
+	t.Parallel()
 	testCases := [...]struct {
 		name           string
 		openPath       string
@@ -667,7 +668,7 @@ func TestOpen(t *testing.T) {
 			if isWindows {
 				fs = fsext.NewTrimFilePathSeparatorFs(fs)
 			}
-			return fs, prefix, func() { require.NoError(t, os.RemoveAll(prefix)) }
+			return fs, prefix, func() { require.NoError(t, os.RemoveAll(prefix)) } //nolint:forbidigo
 		},
 	}
 
@@ -691,7 +692,7 @@ func TestOpen(t *testing.T) {
 						openPath = filepath.Join(prefix, openPath)
 					}
 					if isWindows {
-						openPath = strings.Replace(openPath, `\`, `\\`, -1)
+						openPath = strings.ReplaceAll(openPath, `\`, `\\`)
 					}
 					pwd := tCase.pwd
 					if pwd == "" {
@@ -727,8 +728,8 @@ func TestOpen(t *testing.T) {
 				t.Run(tCase.name, testFunc)
 				if isWindows {
 					// windowsify the testcase
-					tCase.openPath = strings.Replace(tCase.openPath, `/`, `\`, -1)
-					tCase.pwd = strings.Replace(tCase.pwd, `/`, `\`, -1)
+					tCase.openPath = strings.ReplaceAll(tCase.openPath, `/`, `\`)
+					tCase.pwd = strings.ReplaceAll(tCase.pwd, `/`, `\`)
 					t.Run(tCase.name+" with windows slash", testFunc)
 				}
 			}
@@ -856,7 +857,7 @@ func TestBundleNotSharable(t *testing.T) {
 				bi, err := b.Instantiate(context.Background(), uint64(i))
 				require.NoError(t, err)
 				for j := 0; j < iters; j++ {
-					bi.Runtime.Set("__ITER", j)
+					require.NoError(t, bi.Runtime.Set("__ITER", j))
 					_, err := bi.getCallableExport(consts.DefaultFn)(goja.Undefined())
 					require.NoError(t, err)
 				}

js/console.go
@@ -21,7 +21,8 @@ func newConsole(logger logrus.FieldLogger) *console {
 
 // Creates a console logger with its output set to the file at the provided `filepath`.
 func newFileConsole(filepath string, formatter logrus.Formatter, level logrus.Level) (*console, error) {
-	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0o644) //nolint:gosec
+	//nolint:gosec,forbidigo // see https://github.com/grafana/k6/issues/2565
+	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0o644)
 	if err != nil {
 		return nil, err
 	}

js/console_test.go
@@ -83,7 +83,14 @@ func getSimpleRunner(tb testing.TB, filename, data string, opts ...interface{}) 
 }
 
 // TODO: remove the need for this function, see https://github.com/grafana/k6/issues/2968
-func extractLogger(fl logrus.FieldLogger) *logrus.Logger {
+//
+//nolint:forbidigo
+func extractLogger(vu lib.ActiveVU) *logrus.Logger {
+	vuSpecific, ok := vu.(*ActiveVU)
+	if !ok {
+		panic("lib.ActiveVU can't be caset to *ActiveVU")
+	}
+	fl := vuSpecific.Console.logger
 	switch e := fl.(type) {
 	case *logrus.Entry:
 		return e.Logger
@@ -217,7 +224,7 @@ func TestConsoleLog(t *testing.T) {
 
 			vu := initVU.Activate(&lib.VUActivationParams{RunContext: ctx})
 
-			logger := extractLogger(vu.(*ActiveVU).Console.logger)
+			logger := extractLogger(vu)
 
 			logger.Out = io.Discard
 			logger.Level = logrus.DebugLevel
@@ -275,7 +282,7 @@ func TestConsoleLevels(t *testing.T) {
 
 					vu := initVU.Activate(&lib.VUActivationParams{RunContext: ctx})
 
-					logger := extractLogger(vu.(*ActiveVU).Console.logger)
+					logger := extractLogger(vu)
 
 					logger.Out = io.Discard
 					logger.Level = logrus.DebugLevel
@@ -334,26 +341,19 @@ func TestFileConsole(t *testing.T) {
 						msg, deleteFile := msg, deleteFile
 						t.Run(msg, func(t *testing.T) {
 							t.Parallel()
-							f, err := os.CreateTemp("", "") //nolint:forbidigo
-							if err != nil {
-								t.Fatalf("Couldn't create temporary file for testing: %s", err)
-							}
+							f, err := os.CreateTemp("", "") //nolint:forbidigo // fix with https://github.com/grafana/k6/issues/2565
+							require.NoError(t, err)
 							logFilename := f.Name()
-							defer os.Remove(logFilename)
+							defer os.Remove(logFilename) //nolint:errcheck,forbidigo // fix with https://github.com/grafana/k6/issues/2565
 							// close it as we will want to reopen it and maybe remove it
 							if deleteFile {
-								f.Close()
-								if err := os.Remove(logFilename); err != nil {
-									t.Fatalf("Couldn't remove tempfile: %s", err)
-								}
+								require.NoError(t, f.Close())
+								require.NoError(t, os.Remove(logFilename)) //nolint:forbidigo // fix with https://github.com/grafana/k6/issues/2565
 							} else {
 								// TODO: handle case where the string was no written in full ?
 								_, err = f.WriteString(preExistingText)
-								_ = f.Close()
-								if err != nil {
-									t.Fatalf("Error while writing text to preexisting logfile: %s", err)
-								}
-
+								assert.NoError(t, f.Close())
+								require.NoError(t, err)
 							}
 							r, err := getSimpleRunner(t, "/script",
 								fmt.Sprintf(
@@ -375,7 +375,7 @@ func TestFileConsole(t *testing.T) {
 							require.NoError(t, err)
 
 							vu := initVU.Activate(&lib.VUActivationParams{RunContext: ctx})
-							logger := extractLogger(vu.(*ActiveVU).Console.logger)
+							logger := extractLogger(vu)
 
 							logger.Level = logrus.DebugLevel
 							hook := logtest.NewLocal(logger)
@@ -384,7 +384,7 @@ func TestFileConsole(t *testing.T) {
 							require.NoError(t, err)
 
 							// Test if the file was created.
-							_, err = os.Stat(logFilename)
+							_, err = os.Stat(logFilename) //nolint:forbidigo // fix with https://github.com/grafana/k6/issues/2565
 							require.NoError(t, err)
 
 							entry := hook.LastEntry()
@@ -403,7 +403,7 @@ func TestFileConsole(t *testing.T) {
 							entryStr, err := entry.String()
 							require.NoError(t, err)
 
-							f, err = os.Open(logFilename) //nolint:gosec
+							f, err = os.Open(logFilename) //nolint:forbidigo,gosec // fix with https://github.com/grafana/k6/issues/2565
 							require.NoError(t, err)
 
 							fileContent, err := io.ReadAll(f)

js/empty_iterations_bench_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.k6.io/k6/lib"
-	"go.k6.io/k6/metrics"
 )
 
 func BenchmarkEmptyIteration(b *testing.B) {
@@ -16,12 +15,9 @@ func BenchmarkEmptyIteration(b *testing.B) {
 	r, err := getSimpleRunner(b, "/script.js", `exports.default = function() { }`)
 	require.NoError(b, err)
 
-	ch := make(chan metrics.SampleContainer, 100)
+	ch := newDevNullSampleChannel()
 	defer close(ch)
-	go func() { // read the channel so it doesn't block
-		for range ch {
-		}
-	}()
+
 	initVU, err := r.NewVU(context.Background(), 1, 1, ch)
 	require.NoError(b, err)
 	ctx, cancel := context.WithCancel(context.Background())

js/http_bench_test.go
@@ -39,12 +39,8 @@ func BenchmarkHTTPRequests(b *testing.B) {
 	})
 	require.NoError(b, err)
 
-	ch := make(chan metrics.SampleContainer, 100)
+	ch := newDevNullSampleChannel()
 	defer close(ch)
-	go func() { // read the channel so it doesn't block
-		for range ch {
-		}
-	}()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	initVU, err := r.NewVU(ctx, 1, 1, ch)
@@ -77,12 +73,8 @@ func BenchmarkHTTPRequestsBase(b *testing.B) {
 	})
 	require.NoError(b, err)
 
-	ch := make(chan metrics.SampleContainer, 100)
+	ch := newDevNullSampleChannel()
 	defer close(ch)
-	go func() { // read the channel so it doesn't block
-		for range ch {
-		}
-	}()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	initVU, err := r.NewVU(ctx, 1, 1, ch)

js/initcontext_test.go
@@ -241,7 +241,7 @@ func TestInitContextOpen(t *testing.T) {
 		{[]byte("hello world!"), "ascii", 12},
 		{[]byte("?((¯°·._.• ţ€$ţɨɲǥ µɲɨȼ๏ď€ΣSЫ ɨɲ Ќ6 •._.·°¯))؟•"), "utf", 47},
 		{[]byte{0o44, 226, 130, 172}, "utf-8", 2}, // $€
-		//{[]byte{00, 36, 32, 127}, "utf-16", 2},   // $€
+		// {[]byte{00, 36, 32, 127}, "utf-16", 2},   // $€
 	}
 	for _, tc := range testCases {
 		tc := tc

commit 7e4a71c094d48953cc56cf15b1034244d152df2f

js: Return the new deadline error when running code
This was mistakenly broken in 7b5aaaa0

js/runner.go
@@ -420,7 +420,7 @@ func (r *Runner) HandleSummary(ctx context.Context, summary *lib.Summary) (map[s
 	rawResult, _, _, err := vu.runFn(summaryCtx, false, handleSummaryWrapper, nil, wrapperArgs...)
 
 	if deadlineError := r.checkDeadline(summaryCtx, consts.HandleSummaryFn, rawResult, err); deadlineError != nil {
-		return nil, err
+		return nil, deadlineError
 	}
 
 	if err != nil {
@@ -573,7 +573,7 @@ func (r *Runner) runPart(
 	v, _, _, err := vu.runFn(ctx, false, fn, nil, vu.Runtime.ToValue(arg))
 
 	if deadlineError := r.checkDeadline(ctx, name, v, err); deadlineError != nil {
-		return nil, err
+		return nil, deadlineError
 	}
 
 	return v, err

cmd/tests/cmd_run_test.go
@@ -2223,3 +2223,35 @@ func TestBrowserPermissions(t *testing.T) {
 		})
 	}
 }
+
+func TestSetupTimeout(t *testing.T) {
+	t.Parallel()
+	ts := NewGlobalTestState(t)
+	ts.ExpectedExitCode = int(exitcodes.SetupTimeout)
+	ts.CmdArgs = []string{"k6", "run", "-"}
+	ts.Stdin = bytes.NewBufferString(`
+		import { sleep } from 'k6';
+
+		export const options = {
+			setupTimeout: '1s',
+		};
+
+		export function setup() { sleep(100000); };
+		export default function() {}
+	`)
+
+	start := time.Now()
+	cmd.ExecuteWithGlobalState(ts.GlobalState)
+	elapsed := time.Since(start)
+	assert.Greater(t, elapsed, 1*time.Second, "expected more time to have passed because of setupTimeout")
+	assert.Less(
+		t, elapsed, 2*time.Second,
+		"expected less time to have passed because setupTimeout ",
+	)
+
+	stdout := ts.Stdout.String()
+	t.Log(stdout)
+	stderr := ts.Stderr.String()
+	t.Log(stderr)
+	assert.Contains(t, stderr, "setup() execution timed out after 1 seconds")
+}