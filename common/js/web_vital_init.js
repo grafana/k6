edd80db1b8b941bb431f4198bec5f274ecfbb8ea
ka3de <danijs12@hotmail.com>
2023-10-24 08:48:14 +0200

    Add spanID to web_vital_init.js
    To be able to associate a web vital to the PageNavigation span, we need
    to inject the span's id that was previously set in the page in
    onFrameNavigated to the WV event so that the measurement can
    be correctly linked to the page navigation span.

--- common/js/web_vital_init.js
+++ common/js/web_vital_init.js
@@ -8,6 +8,12 @@ function print(metric) {
     numEntries: metric.entries.length,
     navigationType: metric.navigationType,
     url: window.location.href,
+    // To be able to associate a Web Vital measurement to the PageNavigation
+    // span, we need to collect the span ID that was previously set in the
+    // page after the navigation, and pass it back to k6 browser included in
+    // the WV event so the measurement can be correctly linked to the page
+    // navigation span
+    spanID: window.k6SpanId,
   }
   window.k6browserSendWebVitalMetric(JSON.stringify(m))
 }
