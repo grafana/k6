3113d9361d984a9fc34a7c8bb0a3da2104601afa
İnanç Gümüş <inanc.gumus@grafana.com>
2023-10-26 09:42:20 +0300

    Refactor NetworkManager.onLoadingFinished
    This is a prior to fixing the bug #1072.

--- common/network_manager.go
+++ common/network_manager.go
@@ -373,54 +373,53 @@ func (m *NetworkManager) onLoadingFailed(event *network.EventLoadingFailed) {
 }
 
 func (m *NetworkManager) onLoadingFinished(event *network.EventLoadingFinished) {
-	req := m.requestFromID(event.RequestID)
-	if req == nil {
-		// Handling of iframe document request starting in parent session and ending up in iframe session.
-		if m.parent != nil {
-			reqFromParent := m.parent.requestFromID(event.RequestID)
+	rid := event.RequestID
+	req := m.requestForOnLoadingFinished(rid)
 
-			// Main requests have matching loaderID and requestID.
-			if reqFromParent != nil && reqFromParent.getDocumentID() == event.RequestID.String() {
-				m.reqsMu.Lock()
-				m.reqIDToRequest[event.RequestID] = reqFromParent
-				m.reqsMu.Unlock()
-				m.parent.deleteRequestByID(event.RequestID)
-				req = reqFromParent
-			} else {
-				return
-			}
-		} else {
-			return
-		}
+	// the request was not created yet.
+	if req == nil {
+		return
 	}
+
 	req.responseEndTiming = float64(event.Timestamp.Time().Unix()-req.timestamp.Unix()) * 1000
 	m.deleteRequestByID(event.RequestID)
 	m.frameManager.requestFinished(req)
 
 	// Skip data and blob URLs when emitting metrics, since they're internal to the browser.
-	if isInternalURL(req.url) {
-		return
-	}
-
-	emitResponseMetrics := func() {
+	if !isInternalURL(req.url) {
 		req.responseMu.RLock()
 		m.emitResponseMetrics(req.response, req)
 		req.responseMu.RUnlock()
 	}
-	if req.allowInterception {
-		// When request interception is enabled, we need to process requestPaused messages
-		// from CDP in order to get the response for the request. However, we can't process
-		// them until the request is unblocked. Since we're blocking the NetworkManager
-		// goroutine here, we need to spawn a new goroutine to allow the requestPaused
-		// messages to be processed by the NetworkManager.
-		//
-		// This happens when the main page request redirection before it finishes loading.
-		// So the new redirect request will be blocked until the main page finishes loading.
-		// The main page will wait forever since its subrequest is blocked.
-		go emitResponseMetrics()
-		return
+	m.deleteRequestByID(rid)
+	m.frameManager.requestFinished(req)
+}
+
+// requestForOnLoadingFinished returns the request for the given request ID.
+func (m *NetworkManager) requestForOnLoadingFinished(rid network.RequestID) *Request {
+	r := m.requestFromID(rid)
+
+	// Immediately return if the request is not found.
+	if r != nil {
+		return r
 	}
-	emitResponseMetrics()
+
+	// Handle IFrame document requests starting in one session and ending up in another.
+	if m.parent == nil {
+		return nil
+	}
+	// Main requests have matching loaderID and requestID.
+	pr := m.parent.requestFromID(rid)
+	if pr == nil || pr.getDocumentID() != rid.String() {
+		return nil
+	}
+	// Switch the request to the parent request.
+	m.reqsMu.Lock()
+	m.reqIDToRequest[rid] = pr
+	m.reqsMu.Unlock()
+	m.parent.deleteRequestByID(rid)
+
+	return pr
 }
 
 func isInternalURL(u *url.URL) bool {
