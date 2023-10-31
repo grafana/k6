f37d5830eca8a569b9a0359f36c0a1bb0ecb623a
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-31 02:15:17 +0100

    Adding comments to Update grpc_reflection.js

--- examples/grpc_reflection.js
+++ examples/grpc_reflection.js
@@ -1,18 +1,35 @@
+// Import the necessary k6 gRPC module and the k6 check module.
+
 import grpc from 'k6/net/grpc';
 import {check} from "k6";
 
+// Create a new gRPC client.
+
 let client = new grpc.Client();
 
+// Define the default k6 script.
+
 export default () => {
-	client.connect("127.0.0.1:10000", {plaintext: true, reflect: true})
-	const response = client.invoke("main.FeatureExplorer/GetFeature", {
-		latitude: 410248224,
-		longitude: -747127767
-	})
+    // Connect to the gRPC server at "127.0.0.1:10000" using plaintext communication.
+    // The "reflect" option allows the server to send the request back to the client, which can be useful for debugging.
 
-	check(response, {"status is OK": (r) => r && r.status === grpc.StatusOK});
-	console.log(JSON.stringify(response.message))
+    client.connect("127.0.0.1:10000", { plaintext: true, reflect: true });
 
-	client.close()
+    // Make a request to the "main.FeatureExplorer/GetFeature" gRPC method with specific latitude and longitude values.
+
+    const response = client.invoke("main.FeatureExplorer/GetFeature", {
+        latitude: 410248224,
+        longitude: -747127767
+    });
+
+    // Check the response for an "OK" status using the gRPC status code.
+
+    check(response, { "status is OK": (r) => r && r.status === grpc.StatusOK });
+
+    // Log the response message as a JSON string.
+
+    console.log(JSON.stringify(response.message));
+
+    // Close the gRPC client connection.
+    client.close();
 }
-
