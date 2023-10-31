e18fe1853ba5c7eb4c358785a760ec7df84bcb45
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-31 02:09:05 +0100

    Adding comments to Update grpc.js

--- examples/grpc.js
+++ examples/grpc.js
@@ -1,21 +1,36 @@
+// Import gRPC and k6 check module for use in the script.
+
 import grpc from 'k6/net/grpc';
 import { check } from "k6";
 
-let client = new grpc.Client();
-client.load([], "./grpc_server/route_guide.proto")
+// Create a new gRPC client and load the protocol buffer definition.
 
+let client = new grpc.Client();
+client.load([], "./grpc_server/route_guide.proto");
+
+// Define the default k6 script.
 
 export default () => {
-    client.connect("127.0.0.1:10000", { plaintext: true })
+    // Connect to the gRPC server at 127.0.0.1:10000 using plaintext.
+
+    client.connect("127.0.0.1:10000", { plaintext: true });
+
+    // Invoke the "main.FeatureExplorer/GetFeature" method with specific latitude and longitude values.
 
     const response = client.invoke("main.FeatureExplorer/GetFeature", {
         latitude: 410248224,
         longitude: -747127767
-    })
+    });
+
+    // Check the response for an "OK" status using the gRPC status code.
 
     check(response, { "status is OK": (r) => r && r.status === grpc.StatusOK });
-    console.log(JSON.stringify(response.message))
 
-    client.close()
+    // Log the response message as a JSON string.
+
+    console.log(JSON.stringify(response.message));
+
+    // Close the gRPC client connection.
+    client.close();
 }
 
