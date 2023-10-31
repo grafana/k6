15507218e3c1e44ec1a46fabae14906c38ff2088
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-31 01:58:54 +0100

    Added comments to Update graphql.js
    Each comment helps to explain how the script:
    
    - Imports necessary k6 modules and define the GitHub Access Token.
    - Sends a GraphQL query to fetch the first issue in the "grafana/k6" repository.
    - Sets headers for authorization and content type.
    - Checks the response status and log results.
    - Parses the response and log issue details.
    - Sends a GraphQL mutation to add a 'HOORAY' reaction to the issue.
    - Includes a sleep interval for pacing in load testing.

--- examples/graphql.js
+++ examples/graphql.js
@@ -1,10 +1,14 @@
+// Import necessary modules from k6
 import http from "k6/http";
 import { sleep } from "k6";
 
+// Replace with your GitHub Personal Access Token
 let accessToken = "YOUR_GITHUB_ACCESS_TOKEN";
 
+// Define the default k6 function
 export default function() {
 
+  // GraphQL query to find the first issue in the "grafana/k6" repository
   let query = `
     query FindFirstIssue {
       repository(owner:"grafana", name:"k6") {
@@ -20,22 +24,33 @@ export default function() {
       }
     }`;
 
+  // Set request headers, including the authorization token
   let headers = {
     'Authorization': `Bearer ${accessToken}`,
     "Content-Type": "application/json"
   };
 
+  // Send a POST request to the GitHub GraphQL API
   let res = http.post("https://api.github.com/graphql",
     JSON.stringify({ query: query }),
     {headers: headers}
   );
 
+  // Check if the response status is 200 (OK)
   if (res.status === 200) {
+    // Log the response body
     console.log(JSON.stringify(res.body));
+    
+    // Parse the JSON response
     let body = JSON.parse(res.body);
+    
+    // Extract the first issue information
     let issue = body.data.repository.issues.edges[0].node;
+    
+    // Log the issue details
     console.log(issue.id, issue.number, issue.title);
 
+    // GraphQL mutation to add a reaction ("HOORAY") to the issue
     let mutation = `
       mutation AddReactionToIssue {
         addReaction(input:{subjectId:"${issue.id}",content:HOORAY}) {
@@ -48,10 +63,13 @@ export default function() {
         }
     }`;
 
+    // Send a POST request to the GitHub GraphQL API to add a reaction
     res = http.post("https://api.github.com/graphql",
       JSON.stringify({query: mutation}),
       {headers: headers}
     );
   }
+
+  // Sleep for 0.3 seconds to simulate user pacing
   sleep(0.3);
 }
