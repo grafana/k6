ba5db6bc323e181c5bf956af06e22c6c1e6c1558
ka3de <danijs12@hotmail.com>
2023-11-06 12:44:43 +0100

    Add methods to parse tracer provider configuration

--- lib/trace/otel.go
+++ lib/trace/otel.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	stdurl "net/url"
+	"strings"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
@@ -13,12 +15,22 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	"go.opentelemetry.io/otel/trace"
+
+	"go.k6.io/k6/lib/strvals"
 )
 
 const serviceName = "k6"
 
-// ErrInvalidProto indicates that the defined exporter protocol is not valid.
-var ErrInvalidProto = errors.New("invalid protocol")
+var (
+	// ErrInvalidTracesOutput indicates that the defined traces output is not valid.
+	ErrInvalidTracesOutput = errors.New("invalid traces output")
+	// ErrInvalidProto indicates that the defined exporter protocol is not valid.
+	ErrInvalidProto = errors.New("invalid protocol")
+	// ErrInvalidURLScheme indicates that the defined exporter URL scheme is not valid.
+	ErrInvalidURLScheme = errors.New("invalid URL scheme")
+	// ErrInvalidGRPCWithURLPath indicates that an exporter using gRPC protocol does not support URL path.
+	ErrInvalidGRPCWithURLPath = errors.New("grpc protocol does not support URL path")
+)
 
 type (
 	tracerProvShutdownFunc func(ctx context.Context) error
@@ -133,4 +145,106 @@ func NewNoopTracerProvider() *TracerProvider {
 // After Shutdown is called, all methods are no-ops.
 func (tp *TracerProvider) Shutdown(ctx context.Context) error {
 	return tp.shutdown(ctx)
+}
+
+// TracerProviderFromConfigLine initializes a new TracerProvider based on the configuration
+// specified through input line.
+//
+// Supported format is: otel[=<endpoint>:<port>,<other opts>]
+// Where endpoint and port default to: 127.0.0.1:4317
+// And other opts accept:
+//   - proto: http or grpc (default).
+//   - header.<header_name>
+//
+// Example: otel=127.0.0.1:4318/v1/traces,proto=http,header.Authorization=token ***
+func TracerProviderFromConfigLine(ctx context.Context, line string) (*TracerProvider, error) {
+	params, err := tracerProviderParamsFromConfigLine(line)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewTracerProvider(ctx, params)
+}
+
+func tracerProviderParamsFromConfigLine(line string) (tracerProviderParams, error) {
+	params := defaultTracerProviderParams()
+
+	if line == "otel" {
+		// Use default params
+		return params, nil
+	}
+
+	parts := strings.SplitN(line, "=", 2)
+	if parts[0] != "otel" {
+		return params, fmt.Errorf("%w %q", ErrInvalidTracesOutput, parts[0])
+	}
+
+	tokens, err := strvals.Parse(line)
+	if err != nil {
+		return params, fmt.Errorf("error while parsing otel configuration %w", err)
+	}
+
+	for _, token := range tokens {
+		key := token.Key
+		value := token.Value
+
+		var err error
+		switch key {
+		case "otel":
+			err = params.parseURL(value)
+			if err != nil {
+				return params, fmt.Errorf("couldn't parse the otel URL: %w", err)
+			}
+		case "proto":
+			err = params.parseProto(value)
+			if err != nil {
+				return params, fmt.Errorf("couldn't parse the otel proto: %w", err)
+			}
+		default:
+			if strings.HasPrefix(key, "header.") {
+				params.parseHeader(key, value)
+				continue
+			}
+			return params, fmt.Errorf("unknown otel config key %s", key)
+		}
+	}
+
+	if params.proto == "grpc" && params.urlPath != "" {
+		return params, ErrInvalidGRPCWithURLPath
+	}
+
+	return params, nil
+}
+
+func (p *tracerProviderParams) parseURL(url string) error {
+	u, err := stdurl.Parse(url)
+	if err != nil {
+		return err
+	}
+
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return fmt.Errorf("%w: %q", ErrInvalidURLScheme, u.Scheme)
+	}
+
+	p.proto = "http"
+	p.endpoint = u.Host
+	p.urlPath = u.Path
+	p.insecure = u.Scheme == "http"
+
+	return nil
+}
+
+func (p *tracerProviderParams) parseProto(proto string) error {
+	if proto != "http" && proto != "grpc" {
+		return fmt.Errorf("%w: %q", ErrInvalidProto, proto)
+	}
+
+	p.proto = proto
+
+	return nil
+}
+
+func (p *tracerProviderParams) parseHeader(header, value string) {
+	headerName := strings.TrimPrefix(header, "header.")
+	p.headers[headerName] = value
 }
