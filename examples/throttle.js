e37d96fd4cbfaefd0783782adc88b4818f095e29
ankur22 <ankur.agarwal@grafana.com>
2023-11-09 16:54:54 +0000

    Update the throttle example
    This update adds the use of throttleCPU into the example test script.

--- examples/throttle.js
+++ examples/throttle.js
@@ -11,19 +11,30 @@ export const options = {
       },
       exec: 'normal',
     },
-    throttled: {
+    networkThrottled: {
       executor: 'shared-iterations',
       options: {
         browser: {
             type: 'chromium',
         },
       },
-      exec: 'throttled',
+      exec: 'networkThrottled',
+    },
+    cpuThrottled: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+            type: 'chromium',
+        },
+      },
+      exec: 'cpuThrottled',
     },
   },
   thresholds: {
     'browser_http_req_duration{scenario:normal}': ['p(99)<500'],
-    'browser_http_req_duration{scenario:throttled}': ['p(99)<1500'],
+    'browser_http_req_duration{scenario:networkThrottled}': ['p(99)<1500'],
+    'iteration_duration{scenario:normal}': ['p(99)<4000'],
+    'iteration_duration{scenario:cpuThrottled}': ['p(99)<10000'],
   },
 }
 
@@ -38,7 +49,7 @@ export async function normal() {
   }
 }
 
-export async function throttled() {
+export async function networkThrottled() {
   const context = browser.newContext();
   const page = context.newPage();
 
@@ -48,6 +59,19 @@ export async function throttled() {
       download: 250,
       upload: 250,
     });
+
+    await page.goto('https://test.k6.io/', { waitUntil: 'networkidle' });
+  } finally {
+    page.close();
+  }
+}
+
+export async function cpuThrottled() {
+  const context = browser.newContext();
+  const page = context.newPage();
+
+  try {
+    page.throttleCPU({ rate: 4 });
 
     await page.goto('https://test.k6.io/', { waitUntil: 'networkidle' });
   } finally {
