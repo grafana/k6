9971f1eb26ceb3efd8e7ca93097cd7f25ff2cb72
oleiade <theo@crevon.me>
2023-11-10 10:29:24 +0100

    Add fsext.Abs helper function

--- lib/fsext/filepath.go
+++ lib/fsext/filepath.go
@@ -15,3 +15,31 @@ import (
 func JoinFilePath(b, p string) string {
 	return filepath.Join(b, filepath.Clean("/"+p))
 }
+
+// Abs returns an absolute representation of path.
+//
+// this implementation allows k6 to handle absolute paths starting from
+// the current drive on windows like `\users\noname\...`. It makes it easier
+// to test and is needed for archive execution under windows (it always consider '/...' as an
+// absolute path).
+//
+// If the path is not absolute it will be joined with root
+// to turn it into an absolute path. The root path is assumed
+// to be a directory.
+//
+// Because k6 does not interact with the OS itself, but with
+// its own virtual file system, it needs to be able to resolve
+// the root path of the file system. In most cases this would be
+// the bundle or init environment's working directory.
+func Abs(root, path string) string {
+	if path[0] != '/' && path[0] != '\\' && !filepath.IsAbs(path) {
+		path = filepath.Join(root, path)
+	}
+	path = filepath.Clean(path)
+
+	if path[0:1] != FilePathSeparator {
+		path = FilePathSeparator + path
+	}
+
+	return path
+}
