27ff27944994f54c3f2f74c628f949935d3f3596
ankur22 <ankur.agarwal@grafana.com>
2023-11-10 17:01:54 +0000

    Move NetworkProfile type to new file
    This type now feels like it should live closer to where the predefined
    network profiles have been defined.

--- common/network_manager.go
+++ common/network_manager.go
@@ -28,27 +28,6 @@ import (
 	"github.com/dop251/goja"
 )
 
-// NetworkProfile is used in ThrottleNetwork.
-type NetworkProfile struct {
-	// Minimum latency from request sent to response headers received (ms).
-	Latency float64
-
-	// Maximal aggregated download throughput (bytes/sec). -1 disables download throttling.
-	Download float64
-
-	// Maximal aggregated upload throughput (bytes/sec). -1 disables upload throttling.
-	Upload float64
-}
-
-// NewNetworkProfile creates a non-throttled network profile.
-func NewNetworkProfile() NetworkProfile {
-	return NetworkProfile{
-		Latency:  0,
-		Download: -1,
-		Upload:   -1,
-	}
-}
-
 // Credentials holds HTTP authentication credentials.
 type Credentials struct {
 	Username string `js:"username"`
--- common/network_profile.go
+++ common/network_profile.go
@@ -1,5 +1,26 @@
 package common
 
+// NetworkProfile is used in ThrottleNetwork.
+type NetworkProfile struct {
+	// Minimum latency from request sent to response headers received (ms).
+	Latency float64
+
+	// Maximal aggregated download throughput (bytes/sec). -1 disables download throttling.
+	Download float64
+
+	// Maximal aggregated upload throughput (bytes/sec). -1 disables upload throttling.
+	Upload float64
+}
+
+// NewNetworkProfile creates a non-throttled network profile.
+func NewNetworkProfile() NetworkProfile {
+	return NetworkProfile{
+		Latency:  0,
+		Download: -1,
+		Upload:   -1,
+	}
+}
+
 // GetNetworkProfiles returns NetworkProfiles which are ready to be used to
 // throttle the network with page.throttleNetwork.
 func GetNetworkProfiles() map[string]NetworkProfile {
