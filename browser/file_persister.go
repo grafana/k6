79945afad9f57ced470dc32f6834709ba5b1e81d
ankur22 <ankur.agarwal@grafana.com>
2024-02-09 12:01:32 +0000

    Add presignedURLConfig
    This type helps encapsulate and helps document what the parsed types
    and usages are from the K6_BROWSER_SCREENSHOTS_OUTPUT env var.

--- browser/file_persister.go
+++ browser/file_persister.go
@@ -10,6 +10,12 @@ import (
 	"github.com/grafana/xk6-browser/storage"
 )
 
+type presignedURLConfig struct {
+	getterURL string
+	headers   map[string]string
+	basePath  string
+}
+
 // newFilePersister will return either a persister that persists file to the local
 // disk or uploads the files to a remote location. This decision depends on whether
 // the K6_BROWSER_SCREENSHOTS_OUTPUT env var is setup with the correct configs.
@@ -19,31 +25,31 @@ func newFilePersister(envLookup env.LookupFunc) (filePersister, error) {
 		return &storage.LocalFilePersister{}, nil
 	}
 
-	u, b, h, err := parseEnvVar(envVar)
+	popts, err := parseEnvVar(envVar)
 	if err != nil {
 		return nil, fmt.Errorf("parsing %s: %w", env.ScreenshotsOutput, err)
 	}
 
-	return storage.NewRemoteFilePersister(u, h, b), nil
+	return storage.NewRemoteFilePersister(popts.getterURL, popts.headers, popts.basePath), nil
 }
 
 // parseEnvVar will parse a value such as:
 // url=https://127.0.0.1/,basePath=/screenshots,header.1=a,header.2=b
 // and return them.
-func parseEnvVar(envVarValue string) (string, string, map[string]string, error) {
+//
+//nolint:cyclop
+func parseEnvVar(envVarValue string) (presignedURLConfig, error) {
 	ss := strings.Split(envVarValue, ",")
 
-	var (
-		getterURL string
-		basePath  string
-		headers   = make(map[string]string)
-	)
+	presignedURL := presignedURLConfig{
+		headers: make(map[string]string),
+	}
 	for _, s := range ss {
 		// The key value pair should be of the form key=value, so split
 		// on '=' to retrieve the key and value separately.
 		kv := strings.Split(s, "=")
 		if len(kv) != 2 {
-			return "", "", nil, fmt.Errorf("format of value must be k=v, received %q", s)
+			return presignedURLConfig{}, fmt.Errorf("format of value must be k=v, received %q", s)
 		}
 
 		k := kv[0]
@@ -59,14 +65,14 @@ func parseEnvVar(envVarValue string) (string, string, map[string]string, error) 
 
 			hh := strings.Split(k, ".")
 			if len(hh) != 2 {
-				return "", "", nil, fmt.Errorf("format of header must be header.k=v, received %q", s)
+				return presignedURLConfig{}, fmt.Errorf("format of header must be header.k=v, received %q", s)
 			}
 
 			k = hh[0]
 			hk = hh[1]
 
 			if hk == "" {
-				return "", "", nil, fmt.Errorf("empty header key, received %q", s)
+				return presignedURLConfig{}, fmt.Errorf("empty header key, received %q", s)
 			}
 		}
 
@@ -74,21 +80,21 @@ func parseEnvVar(envVarValue string) (string, string, map[string]string, error) 
 		case "url":
 			u, err := url.ParseRequestURI(v)
 			if err != nil && u.Scheme != "" && u.Host != "" {
-				return "", "", nil, fmt.Errorf("invalid url %q", s)
+				return presignedURLConfig{}, fmt.Errorf("invalid url %q", s)
 			}
-			getterURL = v
+			presignedURL.getterURL = v
 		case "basePath":
-			basePath = v
+			presignedURL.basePath = v
 		case "header":
-			headers[hk] = hv
+			presignedURL.headers[hk] = hv
 		default:
-			return "", "", nil, fmt.Errorf("invalid option %q", k)
+			return presignedURLConfig{}, fmt.Errorf("invalid option %q", k)
 		}
 	}
 
-	if getterURL == "" {
-		return "", "", nil, errors.New("missing required url")
+	if presignedURL.getterURL == "" {
+		return presignedURLConfig{}, errors.New("missing required url")
 	}
 
-	return getterURL, basePath, headers, nil
+	return presignedURL, nil
 }
