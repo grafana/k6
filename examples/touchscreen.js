dbfd8cf3f9a4257d3f2c2190ee50e523aa1d8467
İnanç Gümüş <inanc.gumus@grafana.com>
2024-04-18 14:51:12 +0300

    Fix the touchscreen example
    This was printing out the warning (before the fix):
    WARN[0002] failed to hide page: evaluating JS: Cannot find context with specified id  category="Page:Close" elapsed="0 ms" source=browser
    
    The reason is "tapping" causes a page navigation and we're trying to
    close the page while the navigation is in-progress.

--- examples/touchscreen.js
+++ examples/touchscreen.js
@@ -17,11 +17,16 @@ export default async function () {
   const page = browser.newPage();
 
   await page.goto('https://test.k6.io/', { waitUntil: 'networkidle' });
-  
+
   // Obtain ElementHandle for news link and navigate to it
   // by tapping in the 'a' element's bounding box
   const newsLinkBox = page.$('a[href="/news.php"]').boundingBox();
   page.touchscreen.tap(newsLinkBox.x + newsLinkBox.width / 2, newsLinkBox.y);
-    
-  await page.close();
+
+  // Wait until the navigation is done before closing the page.
+  // Otherwise, there will be a race condition between the page closing
+  // and the navigation.
+  await page.waitForNavigation({ waitUntil: 'networkidle' });
+
+  page.close();
 }
