fa15adc7acee6452733d18dc560086c5e05ebe23
İnanç Gümüş <inanc.gumus@grafana.com>
2024-05-08 13:00:29 +0300

    Async keyboard

--- browser/keyboard_mapping.go
+++ browser/keyboard_mapping.go
@@ -1,13 +1,38 @@
 package browser
 
-import "github.com/grafana/xk6-browser/common"
+import (
+	"github.com/dop251/goja"
 
-func mapKeyboard(_ moduleVU, kb *common.Keyboard) mapping {
+	"github.com/grafana/xk6-browser/common"
+	"github.com/grafana/xk6-browser/k6ext"
+)
+
+func mapKeyboard(vu moduleVU, kb *common.Keyboard) mapping {
 	return mapping{
-		"up":         kb.Up,
-		"down":       kb.Down,
-		"press":      kb.Press,
-		"type":       kb.Type,
-		"insertText": kb.InsertText,
+		"down": func(key string) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return nil, kb.Down(key) //nolint:wrapcheck
+			})
+		},
+		"up": func(key string) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return nil, kb.Up(key) //nolint:wrapcheck
+			})
+		},
+		"press": func(key string, opts goja.Value) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return nil, kb.Press(key, opts) //nolint:wrapcheck
+			})
+		},
+		"type": func(text string, opts goja.Value) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return nil, kb.Type(text, opts) //nolint:wrapcheck
+			})
+		},
+		"insertText": func(text string) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return nil, kb.InsertText(text) //nolint:wrapcheck
+			})
+		},
 	}
 }
