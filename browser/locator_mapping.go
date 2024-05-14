247ea91d9c5e9ea76d6a6f32d5db119fd77b9088
İnanç Gümüş <inanc.gumus@grafana.com>
2024-05-14 14:28:11 +0300

    Async Locator.Uncheck

--- browser/locator_mapping.go
+++ browser/locator_mapping.go
@@ -41,7 +41,11 @@ func mapLocator(vu moduleVU, lo *common.Locator) mapping { //nolint:funlen
 				return nil, lo.Check(opts) //nolint:wrapcheck
 			})
 		},
-		"uncheck":      lo.Uncheck,
+		"uncheck": func(opts goja.Value) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return nil, lo.Uncheck(opts) //nolint:wrapcheck
+			})
+		},
 		"isChecked":    lo.IsChecked,
 		"isEditable":   lo.IsEditable,
 		"isEnabled":    lo.IsEnabled,
