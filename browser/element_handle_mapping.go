d56e91fa7da7ab2ccb055919ead3c56e83dcb454
İnanç Gümüş <inanc.gumus@grafana.com>
2024-05-20 15:57:26 +0300

    Async elementhandle.queryAll

--- browser/element_handle_mapping.go
+++ browser/element_handle_mapping.go
@@ -227,17 +227,19 @@ func mapElementHandle(vu moduleVU, eh *common.ElementHandle) mapping { //nolint:
 			return ehm, nil
 		})
 	}
-	maps["$$"] = func(selector string) ([]mapping, error) {
-		ehs, err := eh.QueryAll(selector)
-		if err != nil {
-			return nil, err //nolint:wrapcheck
-		}
-		var mehs []mapping
-		for _, eh := range ehs {
-			ehm := mapElementHandle(vu, eh)
-			mehs = append(mehs, ehm)
-		}
-		return mehs, nil
+	maps["$$"] = func(selector string) *goja.Promise {
+		return k6ext.Promise(vu.Context(), func() (any, error) {
+			ehs, err := eh.QueryAll(selector)
+			if err != nil {
+				return nil, err //nolint:wrapcheck
+			}
+			var mehs []mapping
+			for _, eh := range ehs {
+				ehm := mapElementHandle(vu, eh)
+				mehs = append(mehs, ehm)
+			}
+			return mehs, nil
+		})
 	}
 
 	jsHandleMap := mapJSHandle(vu, eh)
