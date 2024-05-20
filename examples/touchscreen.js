5bad2e39ae4c680b29f88fb077f6b4439461a395
İnanç Gümüş <inanc.gumus@grafana.com>
2024-05-20 16:06:02 +0300

    Async touchscreen example

--- examples/touchscreen.js
+++ examples/touchscreen.js
@@ -16,17 +16,19 @@ export const options = {
 export default async function () {
   const page = await browser.newPage();
 
-  await page.goto('https://test.k6.io/', { waitUntil: 'networkidle' });
+  await page.goto("https://test.k6.io/", { waitUntil: "networkidle" });
 
   // Obtain ElementHandle for news link and navigate to it
   // by tapping in the 'a' element's bounding box
-  const newsLinkBox = page.$('a[href="/news.php"]').boundingBox();
-  await page.touchscreen.tap(newsLinkBox.x + newsLinkBox.width / 2, newsLinkBox.y);
+  const newsLinkBox = await page.$('a[href="/news.php"]').boundingBox();
 
   // Wait until the navigation is done before closing the page.
   // Otherwise, there will be a race condition between the page closing
   // and the navigation.
-  await page.waitForNavigation({ waitUntil: 'networkidle' });
+  await Promise.all([
+    page.waitForNavigation(),
+    page.touchscreen.tap(newsLinkBox.x + newsLinkBox.width / 2, newsLinkBox.y),
+  ]);
 
   page.close();
 }
