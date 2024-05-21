f9dff3d2a2b3cd65ab9b2915304eaa4b62a32b71
ankur22 <ankur.agarwal@grafana.com>
2024-05-21 16:07:35 +0100

    Update request.allHeaders to async

--- browser/request_mapping.go
+++ browser/request_mapping.go
@@ -4,13 +4,18 @@ import (
 	"github.com/dop251/goja"
 
 	"github.com/grafana/xk6-browser/common"
+	"github.com/grafana/xk6-browser/k6ext"
 )
 
 // mapRequest to the JS module.
 func mapRequest(vu moduleVU, r *common.Request) mapping {
 	rt := vu.Runtime()
 	maps := mapping{
-		"allHeaders": r.AllHeaders,
+		"allHeaders": func() *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return r.AllHeaders(), nil
+			})
+		},
 		"frame": func() *goja.Object {
 			mf := mapFrame(vu, r.Frame())
 			return rt.ToValue(mf).ToObject(rt)
