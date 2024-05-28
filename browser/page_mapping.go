310e79ccf8a9c645ea8f8346ccc6a45c4cf6a5d3
İnanç Gümüş <inanc.gumus@grafana.com>
2024-05-28 14:15:52 +0300

    Async page.inputValue

--- browser/page_mapping.go
+++ browser/page_mapping.go
@@ -147,7 +147,11 @@ func mapPage(vu moduleVU, p *common.Page) mapping { //nolint:gocognit,cyclop
 				return p.InnerText(selector, opts) //nolint:wrapcheck
 			})
 		},
-		"inputValue": p.InputValue,
+		"inputValue": func(selector string, opts goja.Value) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return p.InputValue(selector, opts) //nolint:wrapcheck
+			})
+		},
 		"isChecked":  p.IsChecked,
 		"isClosed":   p.IsClosed,
 		"isDisabled": p.IsDisabled,
