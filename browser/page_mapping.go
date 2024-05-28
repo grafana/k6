13593c9c3a43f1494156e0ccda5b12637635cf2f
İnanç Gümüş <inanc.gumus@grafana.com>
2024-05-28 14:12:51 +0300

    Async page.focus

--- browser/page_mapping.go
+++ browser/page_mapping.go
@@ -95,7 +95,11 @@ func mapPage(vu moduleVU, p *common.Page) mapping { //nolint:gocognit,cyclop
 				return nil, p.Fill(selector, value, opts) //nolint:wrapcheck
 			})
 		},
-		"focus": p.Focus,
+		"focus": func(selector string, opts goja.Value) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return nil, p.Focus(selector, opts) //nolint:wrapcheck
+			})
+		},
 		"frames": func() *goja.Object {
 			var (
 				mfrs []mapping
