821346e09ebce177332e165838272d12e3da78b7
İnanç Gümüş <inanc.gumus@grafana.com>
2024-06-04 16:00:33 +0300

    Async Frame.isHidden

--- browser/frame_mapping.go
+++ browser/frame_mapping.go
@@ -154,7 +154,11 @@ func mapFrame(vu moduleVU, f *common.Frame) mapping { //nolint:gocognit,cyclop
 				return f.IsEnabled(selector, opts) //nolint:wrapcheck
 			})
 		},
-		"isHidden":  f.IsHidden,
+		"isHidden": func(selector string, opts goja.Value) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (any, error) {
+				return f.IsHidden(selector, opts) //nolint:wrapcheck
+			})
+		},
 		"isVisible": f.IsVisible,
 		"locator": func(selector string, opts goja.Value) *goja.Object {
 			ml := mapLocator(vu, f.Locator(selector, opts))
