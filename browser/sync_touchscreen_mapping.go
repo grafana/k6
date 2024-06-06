7c0eca89ac763d683330f7c1e8512c9d910d6e1a
İnanç Gümüş <inanc.gumus@grafana.com>
2024-06-06 16:10:17 +0300

    Add syncMapTouchscreen

--- browser/sync_touchscreen_mapping.go
+++ browser/sync_touchscreen_mapping.go
@@ -1,10 +1,19 @@
 package browser
 
 import (
+	"github.com/dop251/goja"
+
 	"github.com/grafana/xk6-browser/common"
+	"github.com/grafana/xk6-browser/k6ext"
 )
 
 // syncMapTouchscreen is like mapTouchscreen but returns synchronous functions.
-func syncMapTouchscreen(_ moduleVU, _ *common.Touchscreen) mapping {
-	return nil
+func syncMapTouchscreen(vu moduleVU, ts *common.Touchscreen) mapping {
+	return mapping{
+		"tap": func(x float64, y float64) *goja.Promise {
+			return k6ext.Promise(vu.Context(), func() (result any, reason error) {
+				return nil, ts.Tap(x, y) //nolint:wrapcheck
+			})
+		},
+	}
 }
