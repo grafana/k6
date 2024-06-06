3b681e657fbfa59481fd687c28976cf8c78de7de
Joan López de la Franca Beltran <joanjan14@gmail.com>
2024-06-06 23:01:51 +0200

    Replace goja => sobek

--- js/modules/k6/experimental/streams/module_test.go
+++ js/modules/k6/experimental/streams/module_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/dop251/goja"
+	"github.com/grafana/sobek"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.k6.io/k6/js/modulestest"
@@ -18,11 +18,11 @@ func TestNewReadableStreamForReader(t *testing.T) {
 
 	// We initialize the runtime, with the ReadableStream(rs) accessible in JS.
 	r := modulestest.NewRuntime(t)
-	rs := NewReadableStreamForReader(r.VU, bytes.NewReader([]byte(exp)))
+	rs := NewReadableStreamFromReader(r.VU, bytes.NewReader([]byte(exp)))
 	require.NoError(t, r.VU.Runtime().Set("rs", rs))
 
 	// Then, we run some JS code that reads from the ReadableStream(rs).
-	var ret goja.Value
+	var ret sobek.Value
 	err := r.EventLoop.Start(func() (err error) {
 		ret, err = r.VU.Runtime().RunString(`(async () => {
   const reader = rs.getReader();
@@ -35,7 +35,7 @@ func TestNewReadableStreamForReader(t *testing.T) {
 
 	// Finally, we expect the returned promise to resolve
 	// to the expected value (the one we streamed).
-	p, ok := ret.Export().(*goja.Promise)
+	p, ok := ret.Export().(*sobek.Promise)
 	require.True(t, ok)
 	assert.Equal(t, exp, p.Result().String())
 }
