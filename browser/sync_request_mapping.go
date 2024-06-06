0c6c7d55d6e0b3ab40c067e5a24539924f38b995
İnanç Gümüş <inanc.gumus@grafana.com>
2024-06-06 16:16:17 +0300

    Add syncMapRequest

--- browser/sync_request_mapping.go
+++ browser/sync_request_mapping.go
@@ -5,6 +5,23 @@ import (
 )
 
 // syncMapRequest is like mapRequest but returns synchronous functions.
-func syncMapRequest(_ moduleVU, _ *common.Request) mapping {
-	return nil
+func syncMapRequest(vu moduleVU, r *common.Request) mapping {
+	maps := mapping{
+		"allHeaders":          r.AllHeaders,
+		"frame":               func() mapping { return syncMapFrame(vu, r.Frame()) },
+		"headerValue":         r.HeaderValue,
+		"headers":             r.Headers,
+		"headersArray":        r.HeadersArray,
+		"isNavigationRequest": r.IsNavigationRequest,
+		"method":              r.Method,
+		"postData":            r.PostData,
+		"postDataBuffer":      r.PostDataBuffer,
+		"resourceType":        r.ResourceType,
+		"response":            func() mapping { return syncMapResponse(vu, r.Response()) },
+		"size":                r.Size,
+		"timing":              r.Timing,
+		"url":                 r.URL,
+	}
+
+	return maps
 }
