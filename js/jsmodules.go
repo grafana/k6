5f36e9b7a8dd35deb380dd5ead57599a230dfd37
Mihail Stoykov <312246+mstoykov@users.noreply.github.com>
2024-09-10 12:59:40 +0300

    Stop supporting k6/experimental/timers (#3838)

--- js/jsmodules.go
+++ js/jsmodules.go
@@ -42,10 +42,8 @@ func getInternalJSModules() map[string]interface{} {
 		"k6/experimental/streams":    streams.New(),
 		"k6/experimental/webcrypto":  webcrypto.New(),
 		"k6/experimental/websockets": &expws.RootModule{},
-		"k6/experimental/timers": newWarnExperimentalModule(timers.New(),
-			"The exports of `k6/experimental/timers` are globally available, so no need to import them."+
-				" The module will be removed after September 23rd, 2024 (v0.54.0). Ensure your scripts are migrated by then."+
-				" There are no API changes, so this is a drop-in replacement and is also available under `k6/timers`."),
+		"k6/experimental/timers": newRemovedModule(
+			"k6/experimental/timers has been graduated, please use k6/timers instead."),
 		"k6/experimental/tracing": newWarnExperimentalModule(tracing.New(),
 			"k6/experimental/tracing is now deprecated. All of its functionality is available as pure javascript module."+
 				" More info available at the docs:"+
