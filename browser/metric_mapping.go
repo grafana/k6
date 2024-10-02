fcd1ea33189fb7d65d272ed0bd21215d0ab9033a
ankur22 <ankur.agarwal@grafana.com>
2024-10-02 12:29:56 +0100

    Add a grouping func to sobek runtime
    We want to use the regex engine in the sobek runtime which should be
    the same or similar to other JS regex engines. This function will be
    used to work with the user defined regex to group metrics with url
    tags.

--- browser/metric_mapping.go
+++ browser/metric_mapping.go
@@ -1,11 +1,29 @@
 package browser
 
 import (
+	"fmt"
+
 	"github.com/grafana/xk6-browser/common"
 )
 
 // mapMetric to the JS module.
 func mapMetric(vu moduleVU, cm *common.ExportedMetric) (mapping, error) {
+	rt := vu.Runtime()
+
+	// We're setting up the function in the Sobek context that will be reused
+	// for this VU.
+	_, err := rt.RunString(`
+	function _k6BrowserURLGroupingTest(pattern, url) {
+		let r = pattern;
+		if (typeof pattern === 'string') {
+			r = new RegExp(pattern);
+		}
+		return r.test(url);
+	}`)
+	if err != nil {
+		return nil, fmt.Errorf("evaluating url grouping: %w", err)
+	}
+
 	return mapping{
 		"groupURLTag": func(urls common.URLGroups) error {
 			return cm.GroupURLTag(urls)
