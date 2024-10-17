280656f5aa074d8bec5dbb88074d0e5eca79a789
ankur22 <ankur.agarwal@grafana.com>
2024-10-17 11:20:35 +0100

    Rename the variable from patterns to matches

--- common/page.go
+++ common/page.go
@@ -414,16 +414,16 @@ type K6BrowserCheckRegEx func(pattern, url string) (bool, error)
 
 // Tag will find the first match given the URLTagPatterns and the URL from
 // the metric tag and update the name field.
-func (e *MetricEvent) Tag(matchesRegex K6BrowserCheckRegEx, patterns TagMatches) error {
-	for _, o := range patterns.Matches {
-		name := strings.TrimSpace(patterns.TagName)
+func (e *MetricEvent) Tag(matchesRegex K6BrowserCheckRegEx, matches TagMatches) error {
+	for _, m := range matches.Matches {
+		name := strings.TrimSpace(matches.TagName)
 		if name == "" {
-			return fmt.Errorf("name %q is invalid", patterns.TagName)
+			return fmt.Errorf("name %q is invalid", matches.TagName)
 		}
 
 		// matchesRegex is a function that will perform the regex test in the Sobek
 		// runtime.
-		matched, err := matchesRegex(o.URLRegEx, e.url)
+		matched, err := matchesRegex(m.URLRegEx, e.url)
 		if err != nil {
 			return err
 		}
