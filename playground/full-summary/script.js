e088fabb88efd14ec80c0677e2765cf3bd442130
Joan López de la Franca Beltran <joanjan14@gmail.com>
2024-10-30 11:53:06 +0100

    Push multi-scenario script example
    Co-authored-by: oleiade <theo@crevon.me>

---
+++ playground/full-summary/script.js
@@ -0,0 +1,36 @@
+export {apiTest} from './api.js';
+export {browserTest} from './browser.js';
+export {grpcTest} from './grpc.js';
+export {wsTest} from './ws.js';
+
+export const options = {
+	thresholds: {
+		'http_reqs{group: ::auth}': ['count>1'],
+		'http_reqs{scenario: api}': ['count>1'],
+	},
+	scenarios: {
+		api: {
+			executor: 'per-vu-iterations',
+			vus: 1,
+			iterations: 1,
+			exec: 'apiTest',
+		},
+		browser: {
+			executor: 'shared-iterations',
+			options: {
+				browser: {
+					type: 'chromium',
+				},
+			},
+			exec: 'browserTest',
+		},
+		grpc: {
+			executor: 'shared-iterations',
+			exec: 'grpcTest',
+		},
+		ws: {
+			executor: 'shared-iterations',
+			exec: 'wsTest',
+		},
+	},
+}
