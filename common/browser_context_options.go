a5ba0c4d7dd91b87c5bdf1b90114750dc5979b49
İnanç Gümüş <inanc.gumus@grafana.com>
2024-11-06 21:14:35 -0500

    Move Geolocation below
    The main type here should BrowserContextOptions.

--- common/browser_context_options.go
+++ common/browser_context_options.go
@@ -1,29 +1,6 @@
 package common
 
-import (
-	"fmt"
-)
-
-// Geolocation represents a geolocation.
-type Geolocation struct {
-	Latitude  float64 `js:"latitude"`
-	Longitude float64 `js:"longitude"`
-	Accuracy  float64 `js:"accuracy"`
-}
-
-// Validate validates the [Geolocation].
-func (g *Geolocation) Validate() error {
-	if g.Longitude < -180 || g.Longitude > 180 {
-		return fmt.Errorf(`invalid longitude "%.2f": precondition -180 <= LONGITUDE <= 180 failed`, g.Longitude)
-	}
-	if g.Latitude < -90 || g.Latitude > 90 {
-		return fmt.Errorf(`invalid latitude "%.2f": precondition -90 <= LATITUDE <= 90 failed`, g.Latitude)
-	}
-	if g.Accuracy < 0 {
-		return fmt.Errorf(`invalid accuracy "%.2f": precondition 0 <= ACCURACY failed`, g.Accuracy)
-	}
-	return nil
-}
+import "fmt"
 
 // BrowserContextOptions stores browser context options.
 type BrowserContextOptions struct {
@@ -63,6 +40,27 @@ func DefaultBrowserContextOptions() *BrowserContextOptions {
 		Screen:            Screen{Width: DefaultScreenWidth, Height: DefaultScreenHeight},
 		Viewport:          Viewport{Width: DefaultScreenWidth, Height: DefaultScreenHeight},
 	}
+}
+
+// Geolocation represents a geolocation.
+type Geolocation struct {
+	Latitude  float64 `js:"latitude"`
+	Longitude float64 `js:"longitude"`
+	Accuracy  float64 `js:"accuracy"`
+}
+
+// Validate validates the [Geolocation].
+func (g *Geolocation) Validate() error {
+	if g.Longitude < -180 || g.Longitude > 180 {
+		return fmt.Errorf(`invalid longitude "%.2f": precondition -180 <= LONGITUDE <= 180 failed`, g.Longitude)
+	}
+	if g.Latitude < -90 || g.Latitude > 90 {
+		return fmt.Errorf(`invalid latitude "%.2f": precondition -90 <= LATITUDE <= 90 failed`, g.Latitude)
+	}
+	if g.Accuracy < 0 {
+		return fmt.Errorf(`invalid accuracy "%.2f": precondition 0 <= ACCURACY failed`, g.Accuracy)
+	}
+	return nil
 }
 
 // GrantPermissionsOptions is used by BrowserContext.GrantPermissions.
