4a2e38f31eecabf4ee9c4013741b3acabcd2dd44
Joan López de la Franca Beltran <5459617+joanlopez@users.noreply.github.com>
2024-11-18 16:03:53 +0100

    Report whether k6 runs on CI (#4063)
    * Report whether k6 runs on CI
    
    Co-authored-by: Inanc Gumus <m@inanc.io>
    
    * Rename and relocate the 'is_ci' usage attribute
    
    ---------
    
    Co-authored-by: Inanc Gumus <m@inanc.io>

--- cmd/report_test.go
+++ cmd/report_test.go
@@ -18,6 +18,7 @@ import (
 
 func TestCreateReport(t *testing.T) {
 	t.Parallel()
+
 	logger := testutils.NewLogger(t)
 	opts, err := executor.DeriveScenariosFromShortcuts(lib.Options{
 		VUs:        null.IntFrom(10),
@@ -25,25 +26,82 @@ func TestCreateReport(t *testing.T) {
 	}, logger)
 	require.NoError(t, err)
 
-	s, err := execution.NewScheduler(&lib.TestRunState{
-		TestPreInitState: &lib.TestPreInitState{
-			Logger: logger,
-		},
-		Options: opts,
-	}, local.NewController())
-	require.NoError(t, err)
-	s.GetState().ModInitializedVUsCount(6)
-	s.GetState().AddFullIterations(uint64(opts.Iterations.Int64))
-	s.GetState().MarkStarted()
-	time.Sleep(10 * time.Millisecond)
-	s.GetState().MarkEnded()
+	initSchedulerWithEnv := func(lookupEnv func(string) (string, bool)) (*execution.Scheduler, error) {
+		return execution.NewScheduler(&lib.TestRunState{
+			TestPreInitState: &lib.TestPreInitState{
+				Logger:    logger,
+				LookupEnv: lookupEnv,
+			},
+			Options: opts,
+		}, local.NewController())
+	}
 
-	m := createReport(usage.New(), s)
-	require.NoError(t, err)
+	t.Run("default (no env)", func(t *testing.T) {
+		t.Parallel()
 
-	assert.Equal(t, consts.Version, m["k6_version"])
-	assert.EqualValues(t, map[string]int{"shared-iterations": 1}, m["executors"])
-	assert.EqualValues(t, 6, m["vus_max"])
-	assert.EqualValues(t, 170, m["iterations"])
-	assert.NotEqual(t, "0s", m["duration"])
+		s, err := initSchedulerWithEnv(func(_ string) (val string, ok bool) {
+			return "", false
+		})
+		require.NoError(t, err)
+
+		s.GetState().ModInitializedVUsCount(6)
+		s.GetState().AddFullIterations(uint64(opts.Iterations.Int64))
+		s.GetState().MarkStarted()
+		time.Sleep(10 * time.Millisecond)
+		s.GetState().MarkEnded()
+
+		m := createReport(usage.New(), s)
+		require.NoError(t, err)
+
+		assert.Equal(t, consts.Version, m["k6_version"])
+		assert.EqualValues(t, map[string]int{"shared-iterations": 1}, m["executors"])
+		assert.EqualValues(t, 6, m["vus_max"])
+		assert.EqualValues(t, 170, m["iterations"])
+		assert.NotEqual(t, "0s", m["duration"])
+		assert.EqualValues(t, false, m["is_ci"])
+	})
+
+	t.Run("CI=false", func(t *testing.T) {
+		t.Parallel()
+
+		s, err := initSchedulerWithEnv(func(envVar string) (val string, ok bool) {
+			if envVar == "CI" {
+				return "false", true
+			}
+			return "", false
+		})
+		require.NoError(t, err)
+
+		m := createReport(usage.New(), s)
+		require.NoError(t, err)
+
+		assert.Equal(t, consts.Version, m["k6_version"])
+		assert.EqualValues(t, map[string]int{"shared-iterations": 1}, m["executors"])
+		assert.EqualValues(t, 0, m["vus_max"])
+		assert.EqualValues(t, 0, m["iterations"])
+		assert.Equal(t, "0s", m["duration"])
+		assert.EqualValues(t, false, m["is_ci"])
+	})
+
+	t.Run("CI=true", func(t *testing.T) {
+		t.Parallel()
+
+		s, err := initSchedulerWithEnv(func(envVar string) (val string, ok bool) {
+			if envVar == "CI" {
+				return "true", true
+			}
+			return "", false
+		})
+		require.NoError(t, err)
+
+		m := createReport(usage.New(), s)
+		require.NoError(t, err)
+
+		assert.Equal(t, consts.Version, m["k6_version"])
+		assert.EqualValues(t, map[string]int{"shared-iterations": 1}, m["executors"])
+		assert.EqualValues(t, 0, m["vus_max"])
+		assert.EqualValues(t, 0, m["iterations"])
+		assert.Equal(t, "0s", m["duration"])
+		assert.EqualValues(t, true, m["is_ci"])
+	})
 }
