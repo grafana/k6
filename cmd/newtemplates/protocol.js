beda4b1c7d95e85c218aa6e9c55100a1e6e27ee2
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2025-01-03 14:27:28 +0100

    Use Authorization header instead of User-ID
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- cmd/newtemplates/protocol.js
+++ cmd/newtemplates/protocol.js
@@ -42,7 +42,7 @@ export default function() {
   let res = http.post(BASE_URL + "/api/pizza", JSON.stringify(restrictions), {
     headers: {
       'Content-Type': 'application/json',
-      'X-User-ID': 23423,
+      'Authorization': 'token abcdef0123456789',
     },
   });
 
