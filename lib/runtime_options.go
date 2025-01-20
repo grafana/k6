728fc1343311dd3f4f595a783bc3b2316e7d3ef1
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-01-20 12:12:52 +0100

    fix lll linter issue

--- lib/runtime_options.go
+++ lib/runtime_options.go
@@ -11,7 +11,9 @@ import (
 //
 // There are several CLIs called enumer, and several versions of them.
 // The generated code can be produced exactly as follows:
-// go run github.com/alvaroloes/enumer@v1.0.0 -type=CompatibilityMode -transform=snake -trimprefix CompatibilityMode -output compatibility_mode_gen.go
+//
+//	go run github.com/alvaroloes/enumer@v1.0.0 -type=CompatibilityMode -transform=snake \
+//	       -trimprefix CompatibilityMode -output compatibility_mode_gen.go
 //
 //go:generate enumer -type=CompatibilityMode -transform=snake -trimprefix CompatibilityMode -output compatibility_mode_gen.go
 type CompatibilityMode uint8
