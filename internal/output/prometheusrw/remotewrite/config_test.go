23daaeacbdb6947c709b3784b1efc2e0e4de5c05
codebien <2103732+codebien@users.noreply.github.com>
2025-01-27 18:29:41 +0100

    output/promrw: Move one package up then drop the useless pkg package

--- internal/output/prometheusrw/remotewrite/config_test.go
+++ internal/output/prometheusrw/remotewrite/config_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/grafana/xk6-output-prometheus-remote/pkg/remote"
+	"go.k6.io/k6/internal/output/prometheusrw/remote"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.k6.io/k6/lib/types"
