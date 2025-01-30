0f91d527c22f16a71c29a676d4878e38969ca852
codebien <2103732+codebien@users.noreply.github.com>
2025-01-30 12:42:37 +0100

    Changed the default config file path

--- cmd/state/state.go
+++ cmd/state/state.go
@@ -35,12 +35,13 @@ const defaultConfigFileName = "config.json"
 type GlobalState struct {
 	Ctx context.Context
 
-	FS         fsext.Fs
-	Getwd      func() (string, error)
-	BinaryName string
-	CmdArgs    []string
-	Env        map[string]string
-	Events     *event.System
+	FS              fsext.Fs
+	Getwd           func() (string, error)
+	UserOSConfigDir string
+	BinaryName      string
+	CmdArgs         []string
+	Env             map[string]string
+	Events          *event.System
 
 	DefaultFlags, Flags GlobalFlags
 
@@ -106,23 +107,24 @@ func NewGlobalState(ctx context.Context) *GlobalState {
 	defaultFlags := GetDefaultFlags(confDir)
 
 	return &GlobalState{
-		Ctx:          ctx,
-		FS:           fsext.NewOsFs(),
-		Getwd:        os.Getwd,
-		BinaryName:   filepath.Base(binary),
-		CmdArgs:      os.Args,
-		Env:          env,
-		Events:       event.NewEventSystem(100, logger),
-		DefaultFlags: defaultFlags,
-		Flags:        getFlags(defaultFlags, env),
-		OutMutex:     outMutex,
-		Stdout:       stdout,
-		Stderr:       stderr,
-		Stdin:        os.Stdin,
-		OSExit:       os.Exit,
-		SignalNotify: signal.Notify,
-		SignalStop:   signal.Stop,
-		Logger:       logger,
+		Ctx:             ctx,
+		FS:              fsext.NewOsFs(),
+		Getwd:           os.Getwd,
+		UserOSConfigDir: confDir,
+		BinaryName:      filepath.Base(binary),
+		CmdArgs:         os.Args,
+		Env:             env,
+		Events:          event.NewEventSystem(100, logger),
+		DefaultFlags:    defaultFlags,
+		Flags:           getFlags(defaultFlags, env),
+		OutMutex:        outMutex,
+		Stdout:          stdout,
+		Stderr:          stderr,
+		Stdin:           os.Stdin,
+		OSExit:          os.Exit,
+		SignalNotify:    signal.Notify,
+		SignalStop:      signal.Stop,
+		Logger:          logger,
 		FallbackLogger: &logrus.Logger{ // we may modify the other one
 			Out:       stderr,
 			Formatter: new(logrus.TextFormatter), // no fancy formatting here
@@ -149,7 +151,7 @@ func GetDefaultFlags(homeDir string) GlobalFlags {
 	return GlobalFlags{
 		Address:          "localhost:6565",
 		ProfilingEnabled: false,
-		ConfigFilePath:   filepath.Join(homeDir, "loadimpact", "k6", defaultConfigFileName),
+		ConfigFilePath:   filepath.Join(homeDir, "k6", defaultConfigFileName),
 		LogOutput:        "stderr",
 	}
 }
