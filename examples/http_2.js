6d835404410d19ebeaeca37f9b841cad3313eb97
Federico Tedin <federicotedin@gmail.com>
2025-02-06 11:24:36 -0300

    Replace legacy testing services from README and examples

--- examples/http_2.js
+++ examples/http_2.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { check } from "k6";
 
 export default function () {
-  check(http.get("https://test-api.k6.io/"), {
+  check(http.get("https://quickpizza.grafana.com"), {
     "status is 200": (r) => r.status == 200,
     "protocol is HTTP/2": (r) => r.proto == "HTTP/2.0",
   });
