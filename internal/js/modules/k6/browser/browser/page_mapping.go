109d86572b35c7d147d566c50e1ae6744ac19264
Mihail Stoykov <M.Stoikov@gmail.com>
2025-02-10 11:31:51 +0200

    browser: reduce races in evaluate*
    Possibly fixing #4085

--- internal/js/modules/k6/browser/browser/page_mapping.go
+++ internal/js/modules/k6/browser/browser/page_mapping.go
@@ -87,21 +87,24 @@ func mapPage(vu moduleVU, p *common.Page) mapping { //nolint:gocognit,cyclop
 			if sobekEmptyString(pageFunc) {
 				return nil, fmt.Errorf("evaluate requires a page function")
 			}
+			funcString := pageFunc.String()
+			gopts := exportArgs(gargs)
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				// TODO(@mstoykov): don't use sobek Values in a separate goroutine
-				return p.Evaluate(pageFunc.String(), exportArgs(gargs)...)
+				return p.Evaluate(funcString, gopts...)
 			}), nil
 		},
 		"evaluateHandle": func(pageFunc sobek.Value, gargs ...sobek.Value) (*sobek.Promise, error) {
 			if sobekEmptyString(pageFunc) {
 				return nil, fmt.Errorf("evaluateHandle requires a page function")
 			}
+			funcString := pageFunc.String()
+			gopts := exportArgs(gargs)
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				// TODO(@mstoykov): don't use sobek Values in a separate goroutine
-				jsh, err := p.EvaluateHandle(pageFunc.String(), exportArgs(gargs)...)
+				jsh, err := p.EvaluateHandle(funcString, gopts...)
 				if err != nil {
 					return nil, err //nolint:wrapcheck
 				}
+				// TODO(@mstoykov): don't use sobek Values in a separate goroutine - this uses the runtime in a lot of the cases
 				return mapJSHandle(vu, jsh), nil
 			}), nil
 		},
