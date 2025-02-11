29b80002c3a6a7bacafc344bee786bbac9b19555
codebien <2103732+codebien@users.noreply.github.com>
2025-02-11 15:04:19 +0100

    Move version to internal/build

--- lib/consts/consts.go
+++ lib/consts/consts.go
@@ -1,5 +1,10 @@
 // Package consts houses some constants needed across k6
 package consts
 
+import "go.k6.io/k6/version"
+
 // Version contains the current semantic version of k6.
-const Version = "0.57.0"
+//
+// Deprecated: alias to support the legacy versioning API. Use the new version package,
+// it will be removed as soon as the external services stop to depend on it.
+const Version = version.SemVer
