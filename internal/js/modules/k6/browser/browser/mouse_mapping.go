41a357175d2ca36b6d4ff1275fec7a288de0c0bf
İnanç Gümüş <inanc.gumus@grafana.com>
2025-02-18 17:23:12 -0500

    browser: Fix mouse sobek mappings

--- internal/js/modules/k6/browser/browser/mouse_mapping.go
+++ internal/js/modules/k6/browser/browser/mouse_mapping.go
@@ -1,6 +1,8 @@
 package browser
 
 import (
+	"fmt"
+
 	"github.com/grafana/sobek"
 
 	"go.k6.io/k6/internal/js/modules/k6/browser/common"
@@ -9,35 +11,50 @@ import (
 
 func mapMouse(vu moduleVU, m *common.Mouse) mapping {
 	return mapping{
-		"click": func(x float64, y float64, opts sobek.Value) *sobek.Promise {
+		"click": func(x float64, y float64, opts sobek.Value) (*sobek.Promise, error) {
+			popts := common.NewMouseClickOptions()
+			if err := popts.Parse(vu.Context(), opts); err != nil {
+				return nil, fmt.Errorf("parsing mouse click options: %w", err)
+			}
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				// TODO(@mstoykov): don't use sobek Values in a separate goroutine
-				return nil, m.Click(x, y, opts) //nolint:wrapcheck
-			})
+				return nil, m.Click(x, y, popts) //nolint:wrapcheck
+			}), nil
 		},
-		"dblClick": func(x float64, y float64, opts sobek.Value) *sobek.Promise {
+		"dblClick": func(x float64, y float64, opts sobek.Value) (*sobek.Promise, error) {
+			popts := common.NewMouseDblClickOptions()
+			if err := popts.Parse(vu.Context(), opts); err != nil {
+				return nil, fmt.Errorf("parsing double click options: %w", err)
+			}
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				// TODO(@mstoykov): don't use sobek Values in a separate goroutine
-				return nil, m.DblClick(x, y, opts) //nolint:wrapcheck
-			})
+				return nil, m.DblClick(x, y, popts) //nolint:wrapcheck
+			}), nil
 		},
-		"down": func(opts sobek.Value) *sobek.Promise {
+		"down": func(opts sobek.Value) (*sobek.Promise, error) {
+			popts := common.NewMouseDownUpOptions()
+			if err := popts.Parse(vu.Context(), opts); err != nil {
+				return nil, fmt.Errorf("parsing mouse down options: %w", err)
+			}
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				// TODO(@mstoykov): don't use sobek Values in a separate goroutine
-				return nil, m.Down(opts) //nolint:wrapcheck
-			})
+				return nil, m.Down(popts) //nolint:wrapcheck
+			}), nil
 		},
-		"up": func(opts sobek.Value) *sobek.Promise {
+		"up": func(opts sobek.Value) (*sobek.Promise, error) {
+			popts := common.NewMouseDownUpOptions()
+			if err := popts.Parse(vu.Context(), opts); err != nil {
+				return nil, fmt.Errorf("parsing mouse down options: %w", err)
+			}
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				// TODO(@mstoykov): don't use sobek Values in a separate goroutine
-				return nil, m.Up(opts) //nolint:wrapcheck
-			})
+				return nil, m.Up(popts) //nolint:wrapcheck
+			}), nil
 		},
-		"move": func(x float64, y float64, opts sobek.Value) *sobek.Promise {
+		"move": func(x float64, y float64, opts sobek.Value) (*sobek.Promise, error) {
+			popts := common.NewMouseMoveOptions()
+			if err := popts.Parse(vu.Context(), opts); err != nil {
+				return nil, fmt.Errorf("parsing mouse move options: %w", err)
+			}
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				// TODO(@mstoykov): don't use sobek Values in a separate goroutine
-				return nil, m.Move(x, y, opts) //nolint:wrapcheck
-			})
+				return nil, m.Move(x, y, popts) //nolint:wrapcheck
+			}), nil
 		},
 	}
 }
