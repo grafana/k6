cede84e0f5ac5793ddaef5c887927ccb2a9462ab
Pablo Chacin <pablochacin@gmail.com>
2025-04-03 21:29:01 +0200

    remove redundant ExecuteWithGlobalState method
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -127,15 +127,7 @@ func (c *rootCommand) execute() {
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute(gs *state.GlobalState) {
-	newRootCommand(gs).execute()
-}
-
-// ExecuteWithGlobalState runs the root command with an existing GlobalState.
-// This is needed by integration tests, and we don't want to modify the
-// Execute() signature to avoid breaking k6 extensions.
-func ExecuteWithGlobalState(gs *state.GlobalState) {
 	newRootCommand(gs).execute()
 }
 
