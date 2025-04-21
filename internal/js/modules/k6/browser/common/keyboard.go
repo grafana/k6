26d9f43b34516c6b5ad167cfa8883ec8a86e8513
Mihail Stoykov <M.Stoikov@gmail.com>
2025-04-21 13:15:30 +0300

    browser: safeguard against pressedKeys being updated concurrently

--- internal/js/modules/k6/browser/common/keyboard.go
+++ internal/js/modules/k6/browser/common/keyboard.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"runtime"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
@@ -31,10 +32,11 @@ type Keyboard struct {
 	ctx     context.Context
 	session session
 
-	modifiers   int64              // like shift, alt, ctrl, ...
-	pressedKeys map[int64]struct{} // tracks keys through down() and up()
-	layoutName  string             // us by default
-	layout      keyboardlayout.KeyboardLayout
+	modifiers     int64              // like shift, alt, ctrl, ...
+	pressedKeys   map[int64]struct{} // tracks keys through down() and up()
+	pressedKeysMu sync.Mutex
+	layoutName    string // us by default
+	layout        keyboardlayout.KeyboardLayout
 }
 
 // NewKeyboard returns a new keyboard with a "us" layout.
@@ -106,8 +108,11 @@ func (k *Keyboard) down(key string) error {
 	keyDef := k.keyDefinitionFromKey(keyInput)
 	k.modifiers |= k.modifierBitFromKeyName(keyDef.Key)
 	text := keyDef.Text
+
+	k.pressedKeysMu.Lock()
 	_, autoRepeat := k.pressedKeys[keyDef.KeyCode]
 	k.pressedKeys[keyDef.KeyCode] = struct{}{}
+	k.pressedKeysMu.Unlock()
 
 	keyType := input.KeyDown
 	if text == "" {
@@ -141,7 +146,9 @@ func (k *Keyboard) up(key string) error {
 
 	keyDef := k.keyDefinitionFromKey(keyInput)
 	k.modifiers &= ^k.modifierBitFromKeyName(keyDef.Key)
+	k.pressedKeysMu.Lock()
 	delete(k.pressedKeys, keyDef.KeyCode)
+	k.pressedKeysMu.Unlock()
 
 	action := input.DispatchKeyEvent(input.KeyUp).
 		WithModifiers(input.Modifier(k.modifiers)).
