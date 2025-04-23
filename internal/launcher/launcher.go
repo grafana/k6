9872be4d87a662143831a84dc42eec6ed2aecda5
codebien <2103732+codebien@users.noreply.github.com>
2025-04-23 12:28:30 +0200

    Merge provision file

--- internal/launcher/launcher.go
+++ internal/launcher/launcher.go
@@ -2,12 +2,15 @@
 package launcher
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/Masterminds/semver/v3"
 	"github.com/grafana/k6deps"
+	"github.com/grafana/k6provider"
 	"go.k6.io/k6/cmd/state"
 	"go.k6.io/k6/internal/build"
 	k6Cmd "go.k6.io/k6/internal/cmd"
@@ -177,4 +180,40 @@ func customBuildRequired(baseK6Version string, deps k6deps.Dependencies) bool {
 
 	// if the current version satisfies the constrains, binary provisioning is not required
 	return !k6Dependency.Constraints.Check(k6Ver)
+}
+
+// k6buildProvision returns the path to a k6 binary that satisfies the dependencies and the list of versions it provides
+func k6buildProvision(gs *state.GlobalState, deps k6deps.Dependencies) (commandExecutor, error) {
+	config := k6provider.Config{
+		BuildServiceURL:  gs.Flags.BuildServiceURL,
+		BuildServiceAuth: extractToken(gs),
+	}
+
+	if config.BuildServiceAuth == "" {
+		return nil, errors.New("k6 cloud token is required when Binary provisioning feature is enabled." +
+			" Set K6_CLOUD_TOKEN environment variable or execute the `k6 cloud login` command")
+	}
+
+	provider, err := k6provider.NewProvider(config)
+	if err != nil {
+		return nil, err
+	}
+
+	binary, err := provider.GetBinary(gs.Ctx, deps)
+	if err != nil {
+		return nil, err
+	}
+
+	gs.Logger.
+		Info("A new k6 binary has been provisioned with version(s): ", formatDependencies(binary.Dependencies))
+
+	return &customBinary{binary.Path}, nil
+}
+
+func formatDependencies(deps map[string]string) string {
+	buffer := &bytes.Buffer{}
+	for dep, version := range deps {
+		buffer.WriteString(fmt.Sprintf("%s:%s ", dep, version))
+	}
+	return strings.Trim(buffer.String(), " ")
 }
