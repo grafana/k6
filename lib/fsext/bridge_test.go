b7755d0c279939ab15f6f34cc3d777ff6c12b18e
codebien <2103732+codebien@users.noreply.github.com>
2025-06-06 16:03:53 +0200

    Bridge between afero and io/fs.FS

---
+++ lib/fsext/bridge_test.go
@@ -0,0 +1,29 @@
+package fsext
+
+import (
+	"io"
+	"io/fs"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBridgeOpen(t *testing.T) {
+	testfs := afero.NewMemMapFs()
+	require.NoError(t, WriteFile(testfs, "abasicpath/onetwo.txt", []byte(`test123`), 0o644))
+
+	bridge := &IOFSBridge{FSExt: testfs}
+
+	// it ensure Bridge implements io/fs.FS
+	goiofs := fs.FS(bridge)
+	f, err := goiofs.Open("abasicpath/onetwo.txt")
+	require.NoError(t, err)
+	require.NotNil(t, f)
+
+	content, err := io.ReadAll(f)
+	require.NoError(t, err)
+
+	assert.Equal(t, "test123", string(content))
+}
