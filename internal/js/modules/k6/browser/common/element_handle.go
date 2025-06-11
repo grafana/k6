0837a4b61a2c4047cb8c5a8585122b83194815a5
ankur22 <ankur.agarwal@grafana.com>
2025-06-11 13:11:53 +0100

    Add a workaround for getting frame width height
    This works with the utility context which might help avoid the NPDs we
    see when working with the current approach -- cdppage.GetLayoutMetrics.

--- internal/js/modules/k6/browser/common/element_handle.go
+++ internal/js/modules/k6/browser/common/element_handle.go
@@ -180,6 +180,45 @@ func (h *ElementHandle) clickablePoint() (*Position, error) {
 	return filterQuads(layoutViewport.ClientWidth, layoutViewport.ClientHeight, quads)
 }
 
+// We are evaluating the inner width and height of the current frame that the
+// element is in in the utility context. Calculating this in the main context
+// causes NPD errors when working with the CDP API cdppage.GetLayoutMetrics.
+//
+// It returns the width and height of the current frame.
+func (h *ElementHandle) evaluateInnerWidthHeight() (int64, int64, error) {
+	h.logger.Debugf("ElementHandle:evaluateInnerWidthHeight", "fid:%s furl:%q", h.frame.ID(), h.frame.URL())
+
+	js := `() => ({ width: innerWidth, height: innerHeight })`
+
+	h.frame.waitForExecutionContext(utilityWorld)
+
+	eopts := evalOptions{
+		forceCallable: true,
+		returnByValue: true,
+	}
+	v, err := h.frame.evaluate(h.ctx, utilityWorld, eopts, js)
+	if err != nil {
+		return 0, 0, fmt.Errorf("getting inner width and height: %w", err)
+	}
+
+	m, ok := v.(map[string]any)
+	if !ok {
+		return 0, 0, fmt.Errorf("unexpected value %v when getting inner width and height", v)
+	}
+
+	width, ok := m["width"].(float64)
+	if !ok {
+		return 0, 0, fmt.Errorf("unexpected value %v when getting inner width", m["width"])
+	}
+
+	height, ok := m["height"].(float64)
+	if !ok {
+		return 0, 0, fmt.Errorf("unexpected value %v when getting inner height", m["height"])
+	}
+
+	return int64(width), int64(height), nil
+}
+
 func filterQuads(viewportWidth, viewportHeight int64, quads []dom.Quad) (*Position, error) {
 	var filteredQuads []dom.Quad
 	for _, q := range quads {
