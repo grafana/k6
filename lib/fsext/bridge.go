4918704af0d16839e56be8824ee0c652601e6387
Pablo Chacin <pablochacin@gmail.com>
2025-06-19 15:59:01 +0200

    add fsext bridge
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

---
+++ lib/fsext/bridge.go
@@ -0,0 +1,27 @@
+package fsext
+
+import (
+	"fmt"
+	"io/fs"
+)
+
+// IOFSBridge allows an afero.Fs to implement the Go standard library io/fs.FS.
+type IOFSBridge struct {
+	FSExt Fs
+}
+
+// NewIOFSBridge returns an IOFSBridge from a Fs
+func NewIOFSBridge(fs Fs) fs.FS {
+	return &IOFSBridge{
+		FSExt: fs,
+	}
+}
+
+// Open implements fs.Fs Open
+func (b *IOFSBridge) Open(name string) (fs.File, error) {
+	f, err := b.FSExt.Open(name)
+	if err != nil {
+		return nil, fmt.Errorf("opening file %w", err)
+	}
+	return f, nil
+}
