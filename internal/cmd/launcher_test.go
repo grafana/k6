0c911ee42735bdb5d714568eac60f4b1316e8fb6
Pablo Chacin <pablochacin@gmail.com>
2025-07-16 14:08:02 +0200

    add missing import
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- internal/cmd/launcher_test.go
+++ internal/cmd/launcher_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/grafana/k6deps"
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
