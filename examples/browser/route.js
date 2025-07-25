2f8a0b0cf75572ab1112abc298e20784fcd8ee79
AgnesToulet <35176601+AgnesToulet@users.noreply.github.com>
2025-07-25 15:40:48 +0200

    improve example script consistency with others

--- examples/browser/route.js
+++ examples/browser/route.js
@@ -1,12 +1,12 @@
-import { browser } from "k6/browser";
+import { browser } from 'k6/browser';
 
 export const options = {
   scenarios: {
     ui: {
-      executor: "shared-iterations",
+      executor: 'shared-iterations',
       options: {
         browser: {
-          type: "chromium",
+          type: 'chromium',
         },
       },
     },
@@ -16,28 +16,30 @@ export const options = {
 export default async function () {
   const page = await browser.newPage();
 
-  await page.route(
-    "https://quickpizza.grafana.com/api/tools",
-    function (route) {
+  try {
+    await page.route(
+      'https://quickpizza.grafana.com/api/tools',
+      function (route) {
+        route.abort();
+      }
+    );
+
+    await page.route(/(\.png$)|(\.jpg$)/, function (route) {
       route.abort();
-    }
-  );
+    });
 
-  await page.route(/(\.png$)|(\.jpg$)/, function (route) {
-    route.abort();
-  });
+    page.on('request', function (request) {
+      console.log('on request', 'url', request.url());
+    });
 
-  page.on("request", function (request) {
-    console.log("on request", "url", request.url());
-  });
+    page.on('response', function (response) {
+      console.log('on response', 'url', response.url());
+    });
 
-  page.on("response", function (response) {
-    console.log("on response", "url", response.url());
-  });
-
-  await page.goto("https://quickpizza.grafana.com/", {
-    waitUntil: "networkidle",
-  });
-
-  await page.close();
+    await page.goto('https://quickpizza.grafana.com/', {
+      waitUntil: 'networkidle',
+    });
+  } finally {
+    await page.close();
+  }
 }
