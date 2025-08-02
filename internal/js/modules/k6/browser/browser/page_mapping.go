ed6125f7b83c71a57fe94cf0cdfb4ed77e30dd1c
HasithDeAlwis <hasithde24@gmail.com>
2025-08-02 13:57:41 -0400

    feat(browser/waitForResponse): add javascript mappings

--- internal/js/modules/k6/browser/browser/page_mapping.go
+++ internal/js/modules/k6/browser/browser/page_mapping.go
@@ -577,6 +577,31 @@ func mapPage(vu moduleVU, p *common.Page) mapping { //nolint:gocognit,cyclop
 		"waitForURL": func(url sobek.Value, opts sobek.Value) (*sobek.Promise, error) {
 			return waitForURLBodyImpl(vu, p, url, opts)
 		},
+		"waitForResponse": func(url sobek.Value, opts sobek.Value) (*sobek.Promise, error) {
+			popts, err := parseWaitForResponseOptions(vu.Context(), opts, p.Timeout())
+			if err != nil {
+				return nil, fmt.Errorf("parsing waitForResponse options: %w", err)
+			}
+
+			var val string
+			switch url.ExportType() {
+			case reflect.TypeOf(string("")):
+				val = fmt.Sprintf("'%s'", url.String()) // Strings require quotes
+			default: // JS Regex, CSS, numbers or booleans
+				val = url.String() // No quotes
+			}
+
+			// Inject JS regex checker for URL pattern matching
+			ctx := vu.Context()
+			jsRegexChecker, err := injectRegexMatcherScript(ctx, vu, p.TargetID())
+			if err != nil {
+				return nil, err
+			}
+
+			return k6ext.Promise(ctx, func() (result any, reason error) {
+				return p.WaitForResponse(val, popts, jsRegexChecker)
+			}), nil
+		},
 		"workers": func() *sobek.Object {
 			var mws []mapping
 			for _, w := range p.Workers() {
