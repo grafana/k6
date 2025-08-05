997c8bb785756b7ea0588ffabd069308795e21b8
HasithDeAlwis <hasithde24@gmail.com>
2025-08-04 20:12:07 -0400

    test(examples/browser): update tests for `page.on` and `waitForResponse`

--- examples/browser/route.js
+++ examples/browser/route.js
@@ -1,4 +1,5 @@
 import { browser } from 'k6/browser';
+import { check } from 'k6';
 
 export const options = {
   scenarios: {
@@ -36,8 +37,32 @@ export default async function () {
       console.log('on response', 'url', response.url());
     });
 
+    // Test waitForResponse alongside page.on('response')
+    const responsePromise = page.waitForResponse('https://quickpizza.grafana.com/');
+
     await page.goto('https://quickpizza.grafana.com/', {
       waitUntil: 'networkidle',
+    });
+
+    const response = await responsePromise;
+
+    // Check that the main page loaded successfully
+    check(response, {
+      'main page status is 200': (r) => r.status() === 200,
+      'main page URL is correct': (r) => r.url() === 'https://quickpizza.grafana.com/',
+    });
+
+    // Test waitForResponse with user interaction
+    const pizzaResponsePromise = page.waitForResponse('https://quickpizza.grafana.com/api/pizza');
+
+    await page.getByRole('button', { name: /pizza/i }).click();
+
+    const pizzaResponse = await pizzaResponsePromise;
+
+    // Check that the pizza API call was successful
+    check(pizzaResponse, {
+      'pizza API status is 200': (r) => r.status() === 200,
+      'pizza API URL is correct': (r) => r.url() === 'https://quickpizza.grafana.com/api/pizza',
     });
   } finally {
     await page.close();
