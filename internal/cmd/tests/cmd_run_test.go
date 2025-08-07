5e238dbb4fa4e538dd11db15c0acbf21136b5ad2
Pablo Chacin <pablochacin@gmail.com>
2025-08-07 14:07:49 +0200

    reintroduce binary provisioning disable test
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- internal/cmd/tests/cmd_run_test.go
+++ internal/cmd/tests/cmd_run_test.go
@@ -2423,6 +2423,35 @@ func TestTypeScriptSupport(t *testing.T) {
 	`
 
 	ts := NewGlobalTestState(t)
+	require.NoError(t, fsext.WriteFile(ts.FS, filepath.Join(ts.Cwd, "test.ts"), []byte(mainScript), 0o644))
+	require.NoError(t, fsext.WriteFile(ts.FS, filepath.Join(ts.Cwd, "bar.ts"), []byte(depScript), 0o644))
+
+	ts.CmdArgs = []string{"k6", "run", "--quiet", "test.ts"}
+
+	cmd.ExecuteWithGlobalState(ts.GlobalState)
+
+	stderr := ts.Stderr.String()
+	t.Log(stderr)
+	assert.Contains(t, stderr, `something 42`)
+}
+
+func TestTypeScriptSupportWithoutBinaryProvisioning(t *testing.T) {
+	t.Parallel()
+	depScript := `
+		export default function(): number {
+			let p: number = 42;
+			return p;
+		}
+	`
+	mainScript := `
+		import bar from "./bar.ts";
+		let s: string = "something";
+		export default function() {
+			console.log(s, bar());
+		};
+	`
+
+	ts := NewGlobalTestState(t)
 	ts.Flags.BinaryProvisioning = false
 	require.NoError(t, fsext.WriteFile(ts.FS, filepath.Join(ts.Cwd, "test.ts"), []byte(mainScript), 0o644))
 	require.NoError(t, fsext.WriteFile(ts.FS, filepath.Join(ts.Cwd, "bar.ts"), []byte(depScript), 0o644))
