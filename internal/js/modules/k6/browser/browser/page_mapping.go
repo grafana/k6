a7738509135ee377ad20718431ca3269a37166bd
Mihail Stoykov <M.Stoikov@gmail.com>
2025-08-08 15:36:09 +0300

    fix: panic in waitBy* without parameters - fixes #5022

--- internal/js/modules/k6/browser/browser/page_mapping.go
+++ internal/js/modules/k6/browser/browser/page_mapping.go
@@ -173,36 +173,54 @@ func mapPage(vu moduleVU, p *common.Page) mapping { //nolint:gocognit,cyclop
 			return rt.ToValue(ml).ToObject(rt), nil
 		},
 		"getByAltText": func(alt sobek.Value, opts sobek.Value) (*sobek.Object, error) {
+			if k6common.IsNullish(alt) {
+				return nil, errors.New("missing required argument 'altText'")
+			}
 			palt, popts := parseGetByBaseOptions(vu.Context(), alt, false, opts)
 
 			ml := mapLocator(vu, p.GetByAltText(palt, popts))
 			return rt.ToValue(ml).ToObject(rt), nil
 		},
 		"getByLabel": func(label sobek.Value, opts sobek.Value) (*sobek.Object, error) {
+			if k6common.IsNullish(label) {
+				return nil, errors.New("missing required argument 'label'")
+			}
 			plabel, popts := parseGetByBaseOptions(vu.Context(), label, true, opts)
 
 			ml := mapLocator(vu, p.GetByLabel(plabel, popts))
 			return rt.ToValue(ml).ToObject(rt), nil
 		},
 		"getByPlaceholder": func(placeholder sobek.Value, opts sobek.Value) (*sobek.Object, error) {
+			if k6common.IsNullish(placeholder) {
+				return nil, errors.New("missing required argument 'placeholder'")
+			}
 			pplaceholder, popts := parseGetByBaseOptions(vu.Context(), placeholder, false, opts)
 
 			ml := mapLocator(vu, p.GetByPlaceholder(pplaceholder, popts))
 			return rt.ToValue(ml).ToObject(rt), nil
 		},
 		"getByTitle": func(title sobek.Value, opts sobek.Value) (*sobek.Object, error) {
+			if k6common.IsNullish(title) {
+				return nil, errors.New("missing required argument 'title'")
+			}
 			ptitle, popts := parseGetByBaseOptions(vu.Context(), title, false, opts)
 
 			ml := mapLocator(vu, p.GetByTitle(ptitle, popts))
 			return rt.ToValue(ml).ToObject(rt), nil
 		},
 		"getByTestId": func(testID sobek.Value) (*sobek.Object, error) {
+			if k6common.IsNullish(testID) {
+				return nil, errors.New("missing required argument 'testId'")
+			}
 			ptestID := parseStringOrRegex(testID, false)
 
 			ml := mapLocator(vu, p.GetByTestID(ptestID))
 			return rt.ToValue(ml).ToObject(rt), nil
 		},
 		"getByText": func(text sobek.Value, opts sobek.Value) (*sobek.Object, error) {
+			if k6common.IsNullish(text) {
+				return nil, errors.New("missing required argument 'text'")
+			}
 			ptext, popts := parseGetByBaseOptions(vu.Context(), text, true, opts)
 
 			ml := mapLocator(vu, p.GetByText(ptext, popts))
