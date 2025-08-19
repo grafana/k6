0889cf021d171db6d73ffabf077c3d2d03885f30
ankur22 <ankur.agarwal@grafana.com>
2025-08-19 18:06:25 +0100

    Add framelocator.locator in its mapping file

--- internal/js/modules/k6/browser/browser/frame_locator_mapping.go
+++ internal/js/modules/k6/browser/browser/frame_locator_mapping.go
@@ -1,10 +1,17 @@
 package browser
 
 import (
+	"github.com/grafana/sobek"
 	"go.k6.io/k6/internal/js/modules/k6/browser/common"
 )
 
 // mapFrameLocator API to the JS module.
 func mapFrameLocator(vu moduleVU, fl *common.FrameLocator) mapping {
-	return mapping{}
+	rt := vu.Runtime()
+	return mapping{
+		"locator": func(selector string) *sobek.Object {
+			ml := mapLocator(vu, fl.Locator(selector))
+			return rt.ToValue(ml).ToObject(rt)
+		},
+	}
 }
