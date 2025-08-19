5f7972db71ff5577fdd5a20228564d9ba54b696e
ankur22 <ankur.agarwal@grafana.com>
2025-08-19 17:08:40 +0100

    Add FrameLocator mapper

---
+++ internal/js/modules/k6/browser/browser/frame_locator_mapping.go
@@ -0,0 +1,10 @@
+package browser
+
+import (
+	"go.k6.io/k6/internal/js/modules/k6/browser/common"
+)
+
+// mapFrameLocator API to the JS module.
+func mapFrameLocator(vu moduleVU, fl *common.FrameLocator) mapping {
+	return mapping{}
+}
