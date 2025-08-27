5a89fb91e1afc011f4574e85fc9d8d075db98b9f
ankur22 <ankur.agarwal@grafana.com>
2025-08-27 15:10:46 +0100

    Update the elementHandle.boundingBox to rtn null
    We should catch the element not visible error and return a null here
    now.

--- internal/js/modules/k6/browser/browser/element_handle_mapping.go
+++ internal/js/modules/k6/browser/browser/element_handle_mapping.go
@@ -2,6 +2,7 @@ package browser
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/grafana/sobek"
 
@@ -15,7 +16,13 @@ func mapElementHandle(vu moduleVU, eh *common.ElementHandle) mapping { //nolint:
 	maps := mapping{
 		"boundingBox": func() *sobek.Promise {
 			return k6ext.Promise(vu.Context(), func() (any, error) {
-				return eh.BoundingBox(), nil
+				box, err := eh.BoundingBox()
+				// We want to avoid errors when an element is not visible and instead
+				// opt to return a nil rectangle -- this matches Playwright's behaviour.
+				if err != nil && strings.Contains(err.Error(), "check if element is visible") {
+					return nil, nil
+				}
+				return box, err
 			})
 		},
 		"check": func(opts sobek.Value) (*sobek.Promise, error) {
