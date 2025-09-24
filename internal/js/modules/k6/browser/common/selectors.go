ea8241f4cfde622f792d9323fbd5501ddec38a03
Elmir Iskhakov <elmir.iskhakov1337@gmail.com>
2025-09-24 20:41:36 +0500

    fix: improve selector parsing

--- internal/js/modules/k6/browser/common/selectors.go
+++ internal/js/modules/k6/browser/common/selectors.go
@@ -71,72 +71,39 @@ func (s *Selector) appendPart(p *SelectorPart, capture bool) error {
 //
 //nolint:cyclop,funlen
 func (s *Selector) parse() error {
-	parsePart := func(selector string, start, index int) (*SelectorPart, bool) {
-		part := strings.TrimSpace(selector[start:index])
+	parsePart := func(part string) (*SelectorPart, bool) {
+		part = strings.TrimSpace(part)
+		if part == "" {
+			return nil, false
+		}
+
 		eqIndex := strings.Index(part, "=")
 		var name, body string
 
 		switch {
-		// Using TrimQuote prevents issues with selectors, allowing us to
-		// handle quoted text with internal selectors, such as:
-		//
-		//       page.getByRole("listitem >> internal:has-text='Product 2'")
-		//
-		// We'd receive the following error because of the quotes:
-		//
-		//       Uncaught (in promise) getting text content of
-		//       "internal:role=listitem >> internal:has-text='Product 2'
-		//       >> nth=0": SyntaxError: Unexpected token ''', "'Product 2'"
-		//       is not valid JSON
-		//
-		// Selectors, such as, internal:has-text, by their nature, are
-		// exposed to users, even if we don't document their use.
-		case strings.HasPrefix(part, "internal:has-text="):
-			name = "internal:has-text"
-			body = TrimQuotes(part[eqIndex+1:])
-		case strings.HasPrefix(part, "internal:has-not-text="):
-			name = "internal:has-not-text"
-			body = TrimQuotes(part[eqIndex+1:])
-		case strings.HasPrefix(part, "nth="):
-			name = "nth"
+		case eqIndex != -1 && reQueryEngine.MatchString(strings.TrimSpace(part[:eqIndex])):
+			name = strings.TrimSpace(part[:eqIndex])
 			body = part[eqIndex+1:]
-		case eqIndex != -1 && reQueryEngine.Match([]byte(strings.TrimSpace(part[0:eqIndex]))):
-			name = strings.TrimSpace(part[0:eqIndex])
-			body = part[eqIndex+1:]
+
 		case len(part) > 1 && part[0] == '"' && part[len(part)-1] == '"':
 			name = "text"
 			body = part
-		case isQuotedText(part):
+
+		case len(part) > 1 && part[0] == '\'' && part[len(part)-1] == '\'':
 			name = "text"
 			body = part
-		case reXPathSelector.Match([]byte(part)) || strings.HasPrefix(part, ".."):
-			// If selector starts with '//' or '//' prefixed with multiple opening
-			// parenthesis, consider xpath. @see https://github.com/microsoft/playwright/issues/817
-			// If selector starts with '..', consider xpath as well.
+
+		case reXPathSelector.MatchString(part) || strings.HasPrefix(part, ".."):
 			name = "xpath"
 			body = part
-		case strings.HasPrefix(part, "internal:role="):
-			name = "internal:role"
-			body = part
-		case strings.HasPrefix(part, "internal:attr="):
-			name = "internal:attr"
-			body = part
-		case strings.HasPrefix(part, "internal:label="):
-			name = "internal:label"
-			body = part
-		case strings.HasPrefix(part, "internal:text="):
-			name = "internal:text"
-			body = part
-		case strings.HasPrefix(part, "internal:control="):
-			name = "internal:control"
-			body = part[eqIndex+1:]
+
 		default:
 			name = "css"
 			body = part
 		}
 
 		capture := false
-		if name[0] == '*' {
+		if strings.HasPrefix(name, "*") {
 			capture = true
 			name = name[1:]
 		}
@@ -144,34 +111,46 @@ func (s *Selector) parse() error {
 		return &SelectorPart{Name: name, Body: body}, capture
 	}
 
-	if !strings.Contains(s.Selector, ">>") {
-		part, capture := parsePart(s.Selector, 0, len(s.Selector))
-		err := s.appendPart(part, capture)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-
 	start := 0
 	index := 0
-	var quote byte
+	var quote rune
+	selector := s.Selector
 
-	for index < len(s.Selector) {
-		c := s.Selector[index]
+	appendPart := func(start, end int) error {
+		part := selector[start:end]
+		p, capture := parsePart(part)
+		if p == nil {
+			return nil
+		}
+		return s.appendPart(p, capture)
+	}
+
+	if !strings.Contains(selector, ">>") {
+		return appendPart(0, len(selector))
+	}
+
+	shouldIgnoreTextSelectorQuote := func(start, end int) bool {
+		prefix := selector[start:end]
+		if match := regexp.MustCompile(`^\s*text\s*=(.*)$`).FindStringSubmatch(prefix); match != nil && match[1] != "" {
+			return true
+		}
+		return false
+	}
+
+	for index < len(selector) {
+		c := rune(selector[index])
+
 		switch {
-		case c == '\\' && index+1 < len(s.Selector):
+		case c == '\\' && index+1 < len(selector):
 			index += 2
 		case c == quote:
-			quote = byte(0)
+			quote = 0
 			index++
-		case quote == 0 && (c == '"' || c == '\'' || c == '`'):
+		case quote == 0 && (c == '"' || c == '\'' || c == '`') && !shouldIgnoreTextSelectorQuote(start, index):
 			quote = c
 			index++
-		case quote == 0 && c == '>' && s.Selector[index+1] == '>':
-			part, capture := parsePart(s.Selector, start, index)
-			err := s.appendPart(part, capture)
-			if err != nil {
+		case quote == 0 && c == '>' && index+1 < len(selector) && selector[index+1] == '>':
+			if err := appendPart(start, index); err != nil {
 				return err
 			}
 			index += 2
@@ -181,10 +160,5 @@ func (s *Selector) parse() error {
 		}
 	}
 
-	part, capture := parsePart(s.Selector, start, index)
-	err := s.appendPart(part, capture)
-	if err != nil {
-		return err
-	}
-	return nil
+	return appendPart(start, index)
 }
