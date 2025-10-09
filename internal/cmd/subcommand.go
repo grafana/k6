cbb1850bc7ad0bf9a5ffdff8b57af53dc42530fe
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-10-09 08:12:02 +0200

    docs: improve comments for subcommand extension

--- internal/cmd/subcommand.go
+++ internal/cmd/subcommand.go
@@ -10,6 +10,8 @@ import (
 	"go.k6.io/k6/subcommand"
 )
 
+// extensionSubcommands returns an iterator over all registered subcommand extensions
+// that are not already defined in the given slice of commands.
 func extensionSubcommands(gs *state.GlobalState, defined []*cobra.Command) iter.Seq[*cobra.Command] {
 	already := make(map[string]struct{}, len(defined))
 	for _, cmd := range defined {
@@ -33,6 +35,7 @@ func extensionSubcommands(gs *state.GlobalState, defined []*cobra.Command) iter.
 	}
 }
 
+// getCmdForExtension gets a *cobra.Command for the given subcommand extension.
 func getCmdForExtension(extension *ext.Extension, gs *state.GlobalState) *cobra.Command {
 	ctor, ok := extension.Module.(subcommand.Constructor)
 	if !ok {
@@ -41,6 +44,8 @@ func getCmdForExtension(extension *ext.Extension, gs *state.GlobalState) *cobra.
 	}
 
 	cmd := ctor(gs)
+
+	// Validate that the command's name matches the extension name.
 	if cmd.Name() != extension.Name {
 		gs.Logger.WithFields(logrus.Fields{"name": extension.Name, "path": extension.Path}).
 			Fatalf("subcommand's command name (%s) does not match the extension name (%s)", cmd.Name(), extension.Name)
