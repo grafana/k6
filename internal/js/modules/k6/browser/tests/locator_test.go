17ef5e4700d908915a9c4f00ff55befeb96c5ba0
rajan2345 <rajanrkt2345@gmail.com>
2025-10-13 22:23:48 +0530

    fix: Remove redundant comment as per review feedback

--- internal/js/modules/k6/browser/tests/locator_test.go
+++ internal/js/modules/k6/browser/tests/locator_test.go
@@ -333,7 +333,6 @@ func TestLocator(t *testing.T) {
 				require.Equal(t, "some text", value)
 			},
 		},
-
 		{
 			"SelectOption", func(tb *testBrowser, p *common.Page) {
 				l := p.Locator("#selectElement", nil)
