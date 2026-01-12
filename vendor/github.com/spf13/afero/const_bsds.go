63b983bb52e1397c6f30a91114d88b6025ba68d4
Adam Munawar Rahman <amrahman@ibm.com>
2026-01-12 16:41:16 -0500

    vendor: add z/OS build flags to vendored afero
    Add zos build tags to vendored spf13/afero files to enable building
    on z/OS USS. This mirrors the upstream fix in spf13/afero#384 which
    was merged in v1.15.0, but the vendored version (v1.1.2) predates it.
    
    - const_bsds.go: add zos to include list (uses syscall.EBADF)
    - const_win_unix.go: add !zos to exclude list (syscall.EBADFD unavailable)
    
    Fixes build error:
      vendor/github.com/spf13/afero/const_win_unix.go:25:23: undefined: syscall.EBADFD
    
    Follow-up to #2888 and #2892. Closes #5547

--- vendor/github.com/spf13/afero/const_bsds.go
+++ vendor/github.com/spf13/afero/const_bsds.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build darwin openbsd freebsd netbsd dragonfly
+// +build darwin openbsd freebsd netbsd dragonfly zos
 
 package afero
 
