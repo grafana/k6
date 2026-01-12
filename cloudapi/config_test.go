ab7d68ec4fe98bae54b3449df3fa7097391a0f9a
AgnesToulet <35176601+AgnesToulet@users.noreply.github.com>
2026-01-12 12:49:41 +0100

    fix config_test.go

--- cloudapi/config_test.go
+++ cloudapi/config_test.go
@@ -29,6 +29,7 @@ func TestConfigApply(t *testing.T) {
 		ProjectID:             null.NewInt(1, true),
 		Name:                  null.NewString("Name", true),
 		Host:                  null.NewString("Host", true),
+		Hostv6:                null.NewString("Hostv6", true),
 		Timeout:               types.NewNullDuration(5*time.Second, true),
 		LogsTailURL:           null.NewString("LogsTailURL", true),
 		PushRefID:             null.NewString("PushRefID", true),
