a1ef23440fe4848ca07312b4aa29bbd40782d3f5
ankur22 <ankur.agarwal@grafana.com>
2026-01-22 16:24:31 +0000

    Fix headers method in request and response

--- internal/js/modules/k6/browser/common/http.go
+++ internal/js/modules/k6/browser/common/http.go
@@ -381,7 +381,7 @@ func (r *Request) HeaderValue(name string) (string, bool) {
 func (r *Request) Headers() map[string]string {
 	headers := make(map[string]string)
 	for n, v := range r.headers {
-		headers[n] = strings.Join(v, ",")
+		headers[strings.ToLower(n)] = strings.Join(v, ",")
 	}
 	return headers
 }
@@ -739,7 +739,7 @@ func (r *Response) FromServiceWorker() bool {
 func (r *Response) Headers() map[string]string {
 	headers := make(map[string]string)
 	for n, v := range r.headers {
-		headers[n] = strings.Join(v, ",")
+		headers[strings.ToLower(n)] = strings.Join(v, ",")
 	}
 	return headers
 }
