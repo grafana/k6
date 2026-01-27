bc74e0bb92eff948191b0cf457d95d94f3be9808
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2026-01-27 17:03:00 +0300

    [fix] Fix linter

--- lib/netext/httpext/error_codes.go
+++ lib/netext/httpext/error_codes.go
@@ -29,7 +29,7 @@ const (
 	invalidURLErrorCode       errCode = 1020
 	requestTimeoutErrorCode   errCode = 1050
 	// DNS errors
-	defaultDNSErrorCode      errCode = 1100
+	defaultDNSErrorCode         errCode = 1100
 	dnsNoSuchHostErrorCode      errCode = 1101
 	blackListedIPErrorCode      errCode = 1110
 	blockedHostnameErrorCode    errCode = 1111
@@ -74,11 +74,11 @@ const (
 	tcpDialRefusedErrorCodeMsg  = "dial: connection refused"
 	tcpBrokenPipeErrorCodeMsg   = "%s: broken pipe"
 	netUnknownErrnoErrorCodeMsg = "%s: unknown errno `%d` on %s with message `%s`"
-	dnsNoSuchHostErrorCodeMsg      = "lookup: no such host"
-	blackListedIPErrorCodeMsg      = "ip is blacklisted"
-	blockedHostnameErrorMsg        = "hostname is blocked"
-	notAllowedHostnameErrorMsg     = "hostname is not allowed"
-	http2GoAwayErrorCodeMsg        = "http2: received GoAway with http2 ErrCode %s"
+	dnsNoSuchHostErrorCodeMsg   = "lookup: no such host"
+	blackListedIPErrorCodeMsg   = "ip is blacklisted"
+	blockedHostnameErrorMsg     = "hostname is blocked"
+	notAllowedHostnameErrorMsg  = "hostname is not allowed"
+	http2GoAwayErrorCodeMsg     = "http2: received GoAway with http2 ErrCode %s"
 	http2StreamErrorCodeMsg     = "http2: stream error with http2 ErrCode %s"
 	http2ConnectionErrorCodeMsg = "http2: connection error with http2 ErrCode %s"
 	x509HostnameErrorCodeMsg    = "x509: certificate doesn't match hostname"
