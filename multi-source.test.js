adb2e9026e97af457759fc3ed3e6a1048d3775d6
Vicente Ortega Torres <vicente.ortega@grafana.com>
2026-03-12 11:39:54 +0100

    [fix] Remove test files

--- multi-source.test.js
+++
@@ -1,9 +0,0 @@
-import secrets from "k6/secrets";
-
-export default async () => {
-  const my_secret = await secrets.get("cool");
-  console.log(my_secret == "cool secret");
-  const anothersource = await secrets.source("another")
-  console.log(await anothersource.get("cool") == "cool secret");
-  console.log(await anothersource.get("cool") == "not cool secret");
-}
