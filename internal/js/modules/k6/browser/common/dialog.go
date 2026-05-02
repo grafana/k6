066caba4933dbeef747027c9b02ad9ab6c7f6f3c
José Henrique <jose.henrique@umdev.cloud>
2026-05-02 18:27:56 -0300

    feat(browser): add Dialog type with Accept and Dismiss methods

---
+++ internal/js/modules/k6/browser/common/dialog.go
@@ -0,0 +1,40 @@
+package common
+
+import (
+	"context"
+
+	"github.com/chromedp/cdproto/cdp"
+	cdppage "github.com/chromedp/cdproto/page"
+)
+
+// Dialog represents a JavaScript dialog (alert, confirm, prompt).
+type Dialog struct {
+	ctx     context.Context
+	session session
+	handled bool
+}
+
+func newDialog(ctx context.Context, s session) *Dialog {
+	return &Dialog{
+		ctx:     ctx,
+		session: s,
+	}
+}
+
+// Accept accepts the dialog.
+func (d *Dialog) Accept() error {
+	err := cdppage.HandleJavaScriptDialog(true).Do(cdp.WithExecutor(d.ctx, d.session))
+	if err == nil {
+		d.handled = true
+	}
+	return err
+}
+
+// Dismiss dismisses the dialog.
+func (d *Dialog) Dismiss() error {
+	err := cdppage.HandleJavaScriptDialog(false).Do(cdp.WithExecutor(d.ctx, d.session))
+	if err == nil {
+		d.handled = true
+	}
+	return err
+}
