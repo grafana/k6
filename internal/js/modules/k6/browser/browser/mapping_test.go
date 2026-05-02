94e361c2e818b0e6b99318eac3a187d94b39d675
José Henrique <jose.henrique@umdev.cloud>
2026-05-02 18:53:17 -0300

    feat(browser): map dialog event to JS module with accept and dismiss

--- internal/js/modules/k6/browser/browser/mapping_test.go
+++ internal/js/modules/k6/browser/browser/mapping_test.go
@@ -434,6 +434,14 @@ func TestMappings(t *testing.T) {
 				})
 			},
 		},
+		"dialog": {
+			apiInterface: (*dialogAPI)(nil),
+			mapp: func() mapping {
+				return mapDialog(moduleVU{VU: vu}, common.PageEvent{
+					Dialog: &common.Dialog{},
+				})
+			},
+		},
 		"mapTouchscreen": {
 			apiInterface: (*touchscreenAPI)(nil),
 			mapp: func() mapping {
@@ -861,4 +869,10 @@ type mouseAPI interface {
 // workerAPI is the interface of a web worker.
 type workerAPI interface {
 	URL() string
+}
+
+// dialogAPI is the interface of a JavaScript dialog.
+type dialogAPI interface {
+	Accept() error
+	Dismiss() error
 }
