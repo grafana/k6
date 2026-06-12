05e76011bab2c9c8eeb375876f6c5e673720f5e5
José Henrique <jose.henrique@umdev.cloud>
2026-06-12 12:27:48 -0300

    feat(browser): add type, message, defaultValue to Dialog; accept promptText

--- internal/js/modules/k6/browser/common/dialog.go
+++ internal/js/modules/k6/browser/common/dialog.go
@@ -7,33 +7,44 @@ import (
 	cdppage "github.com/chromedp/cdproto/page"
 )
 
-// Dialog represents a JavaScript dialog (alert, confirm, prompt).
 type Dialog struct {
-	ctx     context.Context
-	session session
-	handled bool
+	ctx          context.Context
+	session      session
+	handled      bool
+	dialogType   string
+	message      string
+	defaultValue string
 }
 
-func newDialog(ctx context.Context, s session) *Dialog {
+func newDialog(ctx context.Context, s session, event *cdppage.EventJavascriptDialogOpening) *Dialog {
 	return &Dialog{
-		ctx:     ctx,
-		session: s,
+		ctx:          ctx,
+		session:      s,
+		dialogType:   event.Type.String(),
+		message:      event.Message,
+		defaultValue: event.DefaultPrompt,
 	}
 }
 
-// Accept accepts the dialog.
-func (d *Dialog) Accept() error {
+func (d *Dialog) Type() string         { return d.dialogType }
+func (d *Dialog) Message() string      { return d.message }
+func (d *Dialog) DefaultValue() string { return d.defaultValue }
+
+func (d *Dialog) Accept(promptText ...string) error {
 	if d.handled {
 		return nil
 	}
-	err := cdppage.HandleJavaScriptDialog(true).Do(cdp.WithExecutor(d.ctx, d.session))
+	action := cdppage.HandleJavaScriptDialog(true)
+	if len(promptText) > 0 {
+		action = action.WithPromptText(promptText[0])
+	}
+	err := action.Do(cdp.WithExecutor(d.ctx, d.session))
 	if err == nil {
 		d.handled = true
 	}
 	return err
 }
 
-// Dismiss dismisses the dialog.
 func (d *Dialog) Dismiss() error {
 	if d.handled {
 		return nil
